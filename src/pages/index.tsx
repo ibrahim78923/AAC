2e0919952ef207d36b96f7415be3926b8879516e
zahir <zahir.abbas@ceative.co.uk>
2023-09-24 23:17:39 -0700

    #feat(): added theme implementataion and making routes of super admin module and remove button sx (#0000)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import SuperAdminLayout from '@/layouts/SuperAdminLayout/SuperAdminLayout';
-import { Button, Theme, Typography } from '@mui/material';
+import { useTheme } from '@emotion/react';
+import { Button, Typography } from '@mui/material';
 
 export default function Home() {
+  const theme = useTheme();
   return (
     <div>
       <Typography variant="h1">h1 (Air Apple Cart)</Typography>
@@ -14,18 +16,13 @@ export default function Home() {
 
       <Typography variant="subtitle2">Subtitle 2 (Air Apple Cart)</Typography>
 
-      <Typography variant="body1">Body 1 (Air Apple Cart)</Typography>
+      <Typography variant="body1" sx={{ color: theme?.palette?.primary?.main }}>
+        Body 1 (Air Apple Cart)
+      </Typography>
 
       <Typography variant="body2">Body 2 (Air Apple Cart)</Typography>
 
-      <Button
-        variant="contained"
-        sx={(theme: Theme) => ({
-          bgcolor: theme.palette.primary.main,
-        })}
-      >
-        Add Button
-      </Button>
+      <Button variant="contained">Add Button</Button>
       <Button> Cancel </Button>
     </div>
   );
