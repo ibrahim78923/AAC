4df2d142284b886d40e0262fc1cb4df9e168da60
Ahmer <muhammadahmer.akram@ceative.co.uk>
2023-09-26 21:44:27 +0500

    feat: Created following common components: Form Providee, Auto Complete, Radio Group, Checkbox, Multi Checkbox. (#AASSS-832)

---
+++ src/components/ReactHookForm/index.tsx
@@ -0,0 +1,5 @@
+export { default as FormProvider } from './FormProvider';
+export { default as RHFAutocomplete } from './RHFAutocomplete';
+export { default as RHFRadioGroup } from './RHFRadioGroup';
+export { default as RHFCheckbox } from './RHFCheckbox';
+export { default as RHFMultiCheckbox } from './RHFMultiCheckbox';
