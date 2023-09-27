676cf0b9f4c4a975e7102956fc6bae0bc54ce08f
nabeel ahmed <developer_nabeel.ahmed-at-555645084058>
2023-09-27 15:40:29 +0500

    feat(hat): changes in searchable select => create custom dropdown functionalities   . (#AACSA-8)

--- src/components/SearchableSelect/index.tsx
+++ src/components/SearchableSelect/index.tsx
@@ -1,69 +1,88 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Box from '@mui/material/Box';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
-import Popover from '@mui/material/Popover';
+import { Typography } from '@mui/material';
 
 const SearchableSelect = () => {
-  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
-    null,
-  );
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
+  const [isSearchableSelect, setIsSearchableSelect] = useState(false);
+
+  const textFieldRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        textFieldRef.current &&
+        !textFieldRef.current.contains(event.target as Node)
+      ) {
+        setIsSearchableSelect(false);
+      }
+    };
+    window.addEventListener('click', handleClickOutside);
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  const handleSearchFieldClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
   };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? 'searchable-select' : undefined;
-
   return (
     <Box
       sx={{
         width: '100%',
+        position: 'relative',
       }}
     >
       <TextField
         id="outlined-basic"
         label=""
         variant="outlined"
-        aria-describedby={id}
         placeholder="Select"
-        onClick={handleClick}
+        onClick={() => setIsSearchableSelect(true)}
         sx={{
           width: '100%',
-        }}
-      />
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        sx={{
-          '& .css-60ssnn-MuiPaper-root-MuiPopover-paper': {
-            width: { lg: '440px', md: '400px', sm: '100%', xs: '100%' },
+          height: '44px',
+          '& .css-s5zijf-MuiInputBase-root-MuiOutlinedInput-root': {
+            borderRadius: '8px',
+            border: ' #E5E7EB',
           },
         }}
-      >
-        <TextField
-          id="outlined-basic"
-          label=""
-          variant="outlined"
-          placeholder="Select"
+        ref={textFieldRef}
+      />
+
+      {isSearchableSelect && (
+        <Box
           sx={{
-            width: { lg: '440px', md: '440px', sm: '100%', xs: '100%' },
+            width: '100%',
+            position: 'absolute',
+            left: '0',
+            top: '58px',
+            borderRadius: '8px',
+            border: '1px solid #F3F4F6',
+            background: '#fff',
+            boxShadow:
+              '0px 4px 6px -2px rgba(16, 24, 40, 0.03), 0px 12px 16px -4px rgba(16, 24, 40, 0.08)',
           }}
-        />
-        <MenuItem value={10}>test</MenuItem>
-        <MenuItem value={10}>test</MenuItem>
-        <MenuItem value={10}>test</MenuItem>
-      </Popover>
+        >
+          <TextField
+            id="search-field"
+            label=""
+            variant="outlined"
+            placeholder="Select"
+            onClick={handleSearchFieldClick}
+            sx={{
+              width: { lg: '440px', md: '440px', sm: '100%', xs: '100%' },
+              borderRadius: '8px',
+            }}
+          />
+          <MenuItem value={10}>
+            <Typography variant="h6" sx={{ color: 'red' }}>
+              h6 (Air Apple Cart)
+            </Typography>
+          </MenuItem>
+        </Box>
+      )}
     </Box>
   );
 };
