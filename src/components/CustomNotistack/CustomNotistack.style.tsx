18f5e79cfe9ebbb31164ec73bfb9c5fb8e6eb5c2
Nouman <nouman.ishtiaq@ceative.co.uk>
2023-09-28 15:07:50 +0500

    fix(hat): implementing some fixes in the code of the common componenets for the evening shift (#000)

--- src/components/CustomNotistack/CustomNotistack.style.tsx
+++ src/components/CustomNotistack/CustomNotistack.style.tsx
@@ -1,11 +1,11 @@
-export const variantColors = {
+export const variantColors: any = {
   success: '#374151',
   error: '#374151',
   warning: '#374151',
   info: '#374151',
 };
 
-export const variantBackgroundColors = {
+export const variantBackgroundColors: any = {
   success: '#E4F0E7',
   error: '#FFE6E6',
   warning: '#FFF6D9',
