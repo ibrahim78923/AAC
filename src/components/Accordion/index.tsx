36ec4759448f447c19f642e55683a8c80893267a
Muhammad Fahad <muhammad.fahad@ceative.co.uk>
2023-10-03 20:12:03 +0500

    feat(): Implemented Table in Tickets Tasks and fix bugs in common components.(#679)

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,10 +1,13 @@
-import { AccordionItem } from './AccordionItem.type';
+import { AccordionItemI } from './AccordionItem.type';
 import { AccordionSummary, Collapse } from '@mui/material';
 import { ArrowUpIcon, ArrowDownIcon } from '@/assets/icons';
 import { UseAccordionItem } from './UseAccordionItem';
 import { styles } from './AccordionItem.style';
 
-const AccordionItem: React.FC<AccordionItem> = ({ sectionTitle, children }) => {
+const AccordionItem: React.FC<AccordionItemI> = ({
+  sectionTitle,
+  children,
+}) => {
   const { handleAccordionClick, isActive } = UseAccordionItem();
   const styleArr = styles(isActive);
   return (
