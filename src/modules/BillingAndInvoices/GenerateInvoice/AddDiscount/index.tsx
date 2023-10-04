f4f45a88cbf8499142a9c7933e4f77ff14232c31
zahir <zahir.abbas@ceative.co.uk>
2023-10-04 03:02:30 -0700

    #feat(): added generate invoice info and theme card border radius (#AACSA-103)

--- src/modules/BillingAndInvoices/GenerateInvoice/AddDiscount/index.tsx
+++ src/modules/BillingAndInvoices/GenerateInvoice/AddDiscount/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@mui/material';
+import { Card, Grid, Typography } from '@mui/material';
 
 const AddDiscount = () => {
   return (
@@ -6,13 +6,28 @@ const AddDiscount = () => {
       <Grid item xs={12}>
         <Typography variant="h4">Add Discount</Typography>
       </Grid>
-
-      <Grid item xs={6}>
-        <Typography variant="body1">3 Additional Users (£ 15/user)</Typography>
-      </Grid>
-      <Grid item xs={6}>
-        <Typography variant="overline">£ 45</Typography>
-      </Grid>
+      <Card sx={{ width: '100%' }}>
+        <Grid container style={{ padding: '24px' }}>
+          <Grid item xs={6}>
+            <Typography variant="body1">
+              3 Additional Users (£ 15/user)
+            </Typography>
+            <Typography variant="body1">
+              4GB Additional Storage (£ 15/1GB)
+            </Typography>
+            <Typography variant="body1">Sub Total</Typography>
+            <Typography variant="body1">Discount(10%)</Typography>
+            <Typography variant="body1">Tax (Vat 20%)</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="subtitle2">£45</Typography>
+            <Typography variant="subtitle2">£60</Typography>
+            <Typography variant="subtitle2">£145</Typography>
+            <Typography variant="subtitle2">-£10</Typography>
+            <Typography variant="subtitle2">£60</Typography>
+          </Grid>
+        </Grid>
+      </Card>
     </Grid>
   );
 };
