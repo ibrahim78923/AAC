3c202056ed8665bc6f9e3dc18fa1a7584767df31
nouman <nouman.ishtiaq@ceative.co.uk>
2023-10-06 11:56:50 +0000

    Squashed commit of the following
    commit ff4fcb3e 
    Author: Ali Haider <ali.r4565081@gmail.com> 
    Date: Fri Oct 06 2023 16:47:54 GMT+0500 (Pakistan Standard Time) 
    
        feat(hat): add a table into contracts section of assets (#32)

---
+++ src/modules/Assets/Contracts/Contracts.mock.tsx
@@ -0,0 +1,293 @@
+import { Checkbox } from '@mui/material';
+
+export const data: any = [
+  {
+    id: 1,
+    ContractName: 'Microsoft license',
+    Type: 'Software license',
+    Status: 'Active',
+    RenewalStatus: '---',
+    ContractNumber: 'CNTR-3',
+    Vendor: 'Microsoft',
+    ExpiryDate: '26 Oct, 2023',
+  },
+  {
+    id: 2,
+    ContractName: 'Dell Contract',
+    Type: 'Lease',
+    Status: 'Active',
+    RenewalStatus: 'Extended',
+    ContractNumber: 'CNTR-3',
+    Vendor: 'Microsoft',
+    ExpiryDate: '26 Oct, 2023',
+  },
+  {
+    id: 3,
+    ContractName: 'Apple License',
+    Type: 'Warranty',
+    Status: 'Terminated',
+    RenewalStatus: '---',
+    ContractNumber: 'CNTR-3',
+    Vendor: 'Microsoft',
+    ExpiryDate: '26 Oct, 2023',
+  },
+  {
+    id: 4,
+    ContractName: 'Dell license',
+    Type: 'Warranty',
+    Status: 'Draft',
+    RenewalStatus: '---',
+    ContractNumber: 'CNTR-3',
+    Vendor: 'Microsoft',
+    ExpiryDate: '26 Oct, 2023',
+  },
+];
+export const columns = (
+  meetingsData: any,
+  setMeetingsData: any,
+  meetingsMainData: any,
+): any => [
+  {
+    accessorFn: (row: any) => row.id,
+    id: 'id',
+    cell: (info: any) => (
+      <Checkbox
+        checked={
+          !!meetingsData.find((item: any) => item.id === info.getValue())
+        }
+        onChange={(e: any) => {
+          e.target.checked
+            ? setMeetingsData([
+                ...meetingsData,
+                meetingsMainData.find(
+                  (item: any) => item.id === info.getValue(),
+                ),
+              ])
+            : setMeetingsData(
+                meetingsData.filter((item: any) => {
+                  return item.id !== info.getValue();
+                }),
+              );
+        }}
+        color="primary"
+        name={info.getValue()}
+      />
+    ),
+    header: (
+      <Checkbox
+        checked={meetingsData.length === meetingsMainData.length}
+        onChange={(e: any) => {
+          e.target.checked
+            ? setMeetingsData([...meetingsMainData])
+            : setMeetingsData([]);
+        }}
+        color="primary"
+        name="id"
+      />
+    ),
+    isSortable: false,
+  },
+  {
+    accessorFn: (row: any) => row.ContractName,
+    id: 'Contract Name',
+    isSortable: false,
+    header: (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 800,
+          fontSize: '14px',
+          color: '#1F305D',
+        }}
+      >
+        Contract Name
+      </span>
+    ),
+    cell: (info: any) => (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 500,
+          fontSize: '14px',
+          color: '#0AADC7',
+        }}
+      >
+        {info.getValue()}
+      </span>
+    ),
+  },
+  {
+    accessorFn: (row: any) => row.Type,
+    id: 'Type',
+    header: (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 800,
+          fontSize: '14px',
+          color: '#1F305D',
+        }}
+      >
+        Type
+      </span>
+    ),
+    isSortable: false,
+    cell: (info: any) => (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 500,
+          fontSize: '14px',
+          color: '#6B7280',
+        }}
+      >
+        {info.getValue()}
+      </span>
+    ),
+  },
+  {
+    accessorFn: (row: any) => row.Status,
+    id: 'Status',
+    isSortable: false,
+    header: (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 800,
+          fontSize: '14px',
+          color: '#1F305D',
+        }}
+      >
+        Status
+      </span>
+    ),
+    cell: (info: any) => (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 500,
+          fontSize: '14px',
+          color: '#6B7280',
+        }}
+      >
+        {info.getValue()}
+      </span>
+    ),
+  },
+  {
+    accessorFn: (row: any) => row.RenewalStatus,
+    id: 'Renewal Status',
+    isSortable: false,
+    header: (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 800,
+          fontSize: '14px',
+          color: '#1F305D',
+        }}
+      >
+        Renewal Status
+      </span>
+    ),
+    cell: (info: any) => (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 500,
+          fontSize: '14px',
+          color: '#6B7280',
+        }}
+      >
+        {info.getValue()}
+      </span>
+    ),
+  },
+  {
+    accessorFn: (row: any) => row.ContractNumber,
+    id: 'Contract Number',
+    isSortable: false,
+    header: (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 800,
+          fontSize: '14px',
+          color: '#1F305D',
+        }}
+      >
+        Contract Number
+      </span>
+    ),
+    cell: (info: any) => (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 500,
+          fontSize: '14px',
+          color: '#6B7280',
+        }}
+      >
+        {info.getValue()}
+      </span>
+    ),
+  },
+  {
+    accessorFn: (row: any) => row.Impact,
+    id: 'Vendor',
+    isSortable: false,
+    header: (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 800,
+          fontSize: '14px',
+          color: '#1F305D',
+        }}
+      >
+        Vendor
+      </span>
+    ),
+    cell: (info: any) => (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 500,
+          fontSize: '14px',
+          color: '#6B7280',
+        }}
+      >
+        {info.getValue()}
+      </span>
+    ),
+  },
+  {
+    accessorFn: (row: any) => row.ExpiryDate,
+    id: 'Expiry Date',
+    isSortable: false,
+    header: (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 800,
+          fontSize: '14px',
+          color: '#1F305D',
+        }}
+      >
+        Expiry Date
+      </span>
+    ),
+    cell: (info: any) => (
+      <span
+        style={{
+          lineHeight: '18px',
+          fontWeight: 500,
+          fontSize: '14px',
+          color: '#6B7280',
+        }}
+      >
+        {info.getValue()}
+      </span>
+    ),
+  },
+];
