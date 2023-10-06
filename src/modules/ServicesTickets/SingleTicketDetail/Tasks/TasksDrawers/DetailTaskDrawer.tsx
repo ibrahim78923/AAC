0714189f83f6c09a5dd1db9897ce26a8bf45c70c
Muhammad Fahad <muhammad.fahad@ceative.co.uk>
2023-10-06 22:34:36 +0500

    feat(): worked on detail drawer button for status.(#680)

--- src/modules/ServicesTickets/SingleTicketDetail/Tasks/TasksDrawers/DetailTaskDrawer.tsx
+++ src/modules/ServicesTickets/SingleTicketDetail/Tasks/TasksDrawers/DetailTaskDrawer.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image';
-import { Grid, Typography, TextField } from '@mui/material';
+import {
+  Grid,
+  Typography,
+  TextField,
+  Select,
+  Popover,
+  MenuItem,
+} from '@mui/material';
 import { uuid } from 'uuidv4';
 import { useForm } from 'react-hook-form';
 import CommonDrawer from '@/components/CommonDrawer';
 import { FormProvider } from '@/components/ReactHookForm';
-import { drawerDetail } from '../Tasks.mock';
+import { drawerDetail, tasksTableData } from '../Tasks.mock';
 import { DetailTaskDrawerI } from '../Tasks.interface';
 import { taskStyles } from '../TicketTasks.styles';
 
@@ -28,6 +35,69 @@ export const DetailTaskDrawer: React.FC<DetailTaskDrawerI> = ({
         okText="Update"
       >
         <Typography>{taskDetail?.taskName}</Typography>
+        <Select
+          sx={{
+            position: 'absolute',
+            top: 7,
+            right: '4rem',
+            border: `1px solid ${
+              taskDetail?.status === 'To do'
+                ? '#38CAB5'
+                : taskDetail?.status === 'In-Progress'
+                ? '#0AADC7'
+                : '#FF4A4A'
+            }`,
+            bgcolor:
+              taskDetail?.status === 'To do'
+                ? '#EBFAF8'
+                : taskDetail?.status === 'In-Progress'
+                ? '#E6F7F9'
+                : '#FFEDED',
+            color:
+              taskDetail?.status === 'To do'
+                ? '#38CAB5'
+                : taskDetail?.status === 'In-Progress'
+                ? '#0AADC7'
+                : '#FF4A4A',
+            padding: '8px 18px',
+            width: '18%',
+          }}
+          size="small"
+        >
+          {tasksTableData?.map((i: any) => (
+            <MenuItem
+              key={uuid()}
+              sx={{
+                border: `1px solid ${
+                  i?.status === 'To do'
+                    ? '#38CAB5'
+                    : i?.status === 'In-Progress'
+                    ? '#0AADC7'
+                    : '#FF4A4A'
+                }`,
+                bgcolor:
+                  i?.status === 'To do'
+                    ? '#EBFAF8'
+                    : i?.status === 'In-Progress'
+                    ? '#E6F7F9'
+                    : '#FFEDED',
+                color:
+                  i?.status === 'To do'
+                    ? '#38CAB5'
+                    : i?.status === 'In-Progress'
+                    ? '#0AADC7'
+                    : '#FF4A4A',
+                width: 'fit-content',
+                borderRadius: '4px',
+                m: '10px',
+                p: '2px 14px',
+              }}
+            >
+              {i?.status}
+            </MenuItem>
+          ))}
+        </Select>
+        <Popover open={false}></Popover>
         <Grid container spacing={1.5} sx={{ mt: 2, flexDirection: 'column' }}>
           {drawerDetail(taskDetail)?.map((item: any) => (
             <Grid
