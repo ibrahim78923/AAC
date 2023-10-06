38b28942f5c2ebc4232eb17eb1879fa6ac9586d0
Waqas Ahmed <waqas.ahmed@ceative.co.uk>
2023-10-06 06:39:44 +0000

    Squashed commit of the following
    commit 67866db8 
    Author: zahir <zahir.abbas@ceative.co.uk> 
    Date: Fri Oct 06 2023 11:36:37 GMT+0500 (Pakistan Standard Time) 
    
        feat(): meregd branch with dev morning and update some changes (#AACSA-83)
    
    
    commit e8da9e96 
    Author: zahir <zahir.abbas@ceative.co.uk> 
    Date: Fri Oct 06 2023 11:29:43 GMT+0500 (Pakistan Standard Time) 
    
        fix(): import issues fixed and some remove comments component table and mock data  (#AACSA-83)
    
    
    commit c3e9600e 
    Author: zahir <zahir.abbas@ceative.co.uk> 
    Date: Fri Oct 06 2023 11:25:20 GMT+0500 (Pakistan Standard Time) 
    
        fix(): import issues fixed and some remove comments component table and mock data  (#AACSA-83)
    
    
    commit 8f71fe8b 
    Author: zahir <zahir.abbas@ceative.co.uk> 
    Date: Fri Oct 06 2023 11:13:04 GMT+0500 (Pakistan Standard Time) 
    
        feat(): added generate invoice component table and mock data  (#AACSA-83)
    
    
    commit 510f2176 
    Author: zahir <zahir.abbas@ceative.co.uk> 
    Date: Thu Oct 05 2023 15:53:39 GMT+0500 (Pakistan Standard Time) 
    
        feat(): added mock data of billing details and added assign plan form (#AACSA-103)
    
    
    commit f52d526e 
    Author: zahir <zahir.abbas@ceative.co.uk> 
    Date: Thu Oct 05 2023 14:21:44 GMT+0500 (Pakistan Standard Time) 
    
        feat(): added menu options and added billing and details mock table data (#AACSA-103)
    
    
    commit f4f45a88 
    Author: zahir <zahir.abbas@ceative.co.uk> 
    Date: Wed Oct 04 2023 15:02:30 GMT+0500 (Pakistan Standard Time) 
    
        #feat(): added generate invoice info and theme card border radius (#AACSA-103)
    
    
    commit 8b9e003c 
    Author: zahir <zahir.abbas@ceative.co.uk> 
    Date: Tue Oct 03 2023 16:03:33 GMT+0500 (Pakistan Standard Time) 
    
        fix(): changes in common search change box shadow color in on hover (#0000)

--- src/modules/superAdmin/BillingAndInvoices/BillingAndInvoicesTable/index.tsx
+++ src/modules/superAdmin/BillingAndInvoices/BillingAndInvoicesTable/index.tsx
@@ -2,17 +2,31 @@ import { useState } from 'react';
 
 import { Grid, Typography, Button } from '@mui/material';
 
+import { BillingAndInvoicesTableData } from '@/mock/modules/BillingAndDetails';
+
 import Search from '@/components/Search';
-
 import GenerateInvoice from '../GenerateInvoice';
-
+import TanstackTable from '@/components/Tabel/TanstackTable';
 import ViewBillingDetails from '../ViewBillingDetails';
+import { columns } from './BillingAndInvoices.data';
+import EditForm from '../EditForm';
+import MenuItems from './MenuOptions';
+import useMenuOptions from './MenuOptions/useMenuOptions';
+import Filters from './Filters';
+import CustomPagination from '@/components/CustomPagination';
 
 import PlusSharedIcon from '@/assets/icons/shared/plus-shared';
+import { FilterSharedIcon } from '@/assets/icons';
 
 const BillingAndInvoicesTable = () => {
   const [searchByClientName, setSearchByClientName] = useState('');
   const [isViewDetailOpen, setIsViewDeailOpen] = useState<boolean>(false);
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+  const [isOpenFilter, setIsOpenFilter] = useState(false);
+  const [isShowGenerateInvoice, setisShowGenerateInvoice] = useState(false);
+  const { isShowViewBillingDetails, setIsShowViewBillingDetails } =
+    useMenuOptions();
+
   return (
     <>
       <Grid>
@@ -22,7 +36,7 @@ const BillingAndInvoicesTable = () => {
           </Grid>
           <Grid item xs={6} sm={6} sx={{ textAlign: 'end' }}>
             <Button
-              onClick={() => setIsViewDeailOpen(true)}
+              onClick={() => setIsOpenDrawer(true)}
               startIcon={<PlusSharedIcon />}
               variant="contained"
             >
@@ -31,22 +45,51 @@ const BillingAndInvoicesTable = () => {
           </Grid>
         </Grid>
         <Grid container mt={2}>
-          <Grid item xs={6} sm={6}>
+          <Grid item xs={10} sm={10} mt={4}>
             <Search
               searchBy={searchByClientName}
               setSearchBy={setSearchByClientName}
               label="Search By Name"
-              size="small"
-              fullWidth
+              size="medium"
             />
           </Grid>
+          <Grid item xs={1} sm={1} mt={4}>
+            <MenuItems
+              isViewDetailOpen={isViewDetailOpen}
+              setIsViewDeailOpen={setIsViewDeailOpen}
+              setIsOpenDrawer={setIsOpenDrawer}
+              setIsShowViewBillingDetails={setIsShowViewBillingDetails}
+              setisShowGenerateInvoice={setisShowGenerateInvoice}
+            />
+          </Grid>
+          <Grid item xs={1} sm={1} mt={4}>
+            <Button
+              onClick={() => setIsOpenFilter(true)}
+              startIcon={<FilterSharedIcon />}
+              sx={{ border: '1px solid #D1D5DB', color: '#6B7280' }}
+            >
+              Filters
+            </Button>
+          </Grid>
         </Grid>
-        <GenerateInvoice />
+        {isShowGenerateInvoice && <GenerateInvoice />}
       </Grid>
-      <ViewBillingDetails
-        isViewDetailOpen={isViewDetailOpen}
-        setIsViewDeailOpen={setIsViewDeailOpen}
-      />
+      {isShowViewBillingDetails && (
+        <ViewBillingDetails
+          isOpenDrawer={isShowViewBillingDetails}
+          onClose={setIsShowViewBillingDetails}
+        />
+      )}
+      <Grid item xs={12} sm={12} mt={3}>
+        <TanstackTable columns={columns} data={BillingAndInvoicesTableData} />
+      </Grid>
+      <CustomPagination count={1} rowsPerPageOptions={[1, 2]} entriePages={1} />
+      {isOpenDrawer && (
+        <EditForm isOpenDrawer={isOpenDrawer} onClose={setIsOpenDrawer} />
+      )}
+      {isOpenFilter && (
+        <Filters isOpenDrawer={isOpenFilter} onClose={setIsOpenFilter} />
+      )}
     </>
   );
 };
--- src/modules/superAdmin/BillingAndInvoices/GenerateInvoice/index.tsx
+++ src/modules/superAdmin/BillingAndInvoices/GenerateInvoice/index.tsx
@@ -1,12 +1,14 @@
 import { Grid } from '@mui/material';
 
 import UserInfo from './UserInfo';
+import AddDiscount from './AddDiscount';
 
 const GenerateInvoice = () => {
   return (
-    <Grid container>
+    <Grid container mt={4}>
       <Grid item lg={12}>
         <UserInfo />
+        <AddDiscount />
       </Grid>
     </Grid>
   );
