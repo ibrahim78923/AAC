93f3380087cb49e21502834723959b5614851dea
nouman@ceative.co.uk <nouman@ceative.co.uk>
2023-10-11 13:01:52 +0000

    Squashed commit of the following
    commit ff086add 
    Author: Ahmer <muhammadahmer.akram@ceative.co.uk> 
    Date: Wed Oct 11 2023 17:56:30 GMT+0500 (Pakistan Standard Time) 
    
        feat: created a global component for when there is no data on the screen. (#AASSS-119)
    
    
    commit b64a655e 
    Author: Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk> 
    Date: Wed Oct 11 2023 13:54:34 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev-evening' of https://git-codecommit.eu-west-2.amazonaws.com/v1/repos/AAC-FE into m-ahmer
    
    
    commit 32de1d3f 
    Author: Ahmer <muhammadahmer.akram@ceative.co.uk> 
    Date: Tue Oct 10 2023 20:17:05 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev-evening' of https://git-codecommit.eu-west-2.amazonaws.com/v1/repos/AAC-FE into m-ahmer
    
    
    commit 951be355 
    Author: Ahmer <muhammadahmer.akram@ceative.co.uk> 
    Date: Tue Oct 10 2023 20:16:47 GMT+0500 (Pakistan Standard Time) 
    
        fix: fixed the RHFAutoComplete so that it can be used as Select with search. (#AASSS-832)
    
    
    commit 3c17b7da 
    Author: Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk> 
    Date: Tue Oct 10 2023 14:16:09 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev-evening' of https://git-codecommit.eu-west-2.amazonaws.com/v1/repos/AAC-FE into m-ahmer
    
    
    commit f2a01a02 
    Author: Ahmer <muhammadahmer.akram@ceative.co.uk> 
    Date: Mon Oct 09 2023 21:04:41 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev-evening' of https://git-codecommit.eu-west-2.amazonaws.com/v1/repos/AAC-FE into m-ahmer
    
    
    commit 4265b1d3 
    Author: Ahmer <muhammadahmer.akram@ceative.co.uk> 
    Date: Mon Oct 09 2023 20:18:28 GMT+0500 (Pakistan Standard Time) 
    
        fix: Fixed issues related to best practices. (#AASSS-694)
    
    
    commit 0e5593dc 
    Author: Ahmer <muhammadahmer.akram@ceative.co.uk> 
    Date: Mon Oct 09 2023 08:10:11 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev-evening' of https://git-codecommit.eu-west-2.amazonaws.com/v1/repos/AAC-FE into m-ahmer
    
    
    commit 99e62aad 
    Author: Ahmer <muhammadahmer.akram@ceative.co.uk> 
    Date: Mon Oct 09 2023 07:15:11 GMT+0500 (Pakistan Standard Time) 
    
        fix: Fixed issues related to best practices. (#AASSS-694)
    
    
    commit 919e7e75 
    Author: Ahmer <muhammadahmer.akram@ceative.co.uk> 
    Date: Mon Oct 09 2023 07:12:47 GMT+0500 (Pakistan Standard Time) 
    
        fix: Fixed issues related to best practices. (#AASSS-694)
    
    
    commit bb7d4df1 
    Author: Ahmer <muhammadahmer.akram@ceative.co.uk> 
    Date: Mon Oct 09 2023 07:09:49 GMT+0500 (Pakistan Standard Time) 
    
        fix: Fixed issues related to best practices. (#AASSS-694)

--- src/modules/airServices/Assets/Inventory/SingleInventoryDetail/Associations/index.tsx
+++ src/modules/airServices/Assets/Inventory/SingleInventoryDetail/Associations/index.tsx
@@ -1,3 +1,23 @@
+import { Button, useTheme } from '@mui/material';
+import { Fragment } from 'react';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import NoAssociationFound from '@/assets/images/modules/LogitechMouse/association.png';
+import NoData from '@/components/NoData';
+
 export const Associations = () => {
-  return <>Association</>;
+  const theme: any = useTheme();
+
+  return (
+    <Fragment>
+      <NoData image={NoAssociationFound} message={'There are no associations'}>
+        <Button
+          variant="outlined"
+          sx={{ backgroundColor: theme.palette.grey[400] }}
+        >
+          <AddCircleIcon sx={{ mr: 1 }} />
+          Asssociate
+        </Button>
+      </NoData>
+    </Fragment>
+  );
 };
