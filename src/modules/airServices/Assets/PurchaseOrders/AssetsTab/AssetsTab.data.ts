3f9c1f2c032c300fc0273ca44534be428aeb81c2
tayyab khan satti <tayyabkhan.satti@ceative.co.uk>
2023-10-12 14:37:20 +0500

    feat(hat): Today i have to  worked on assets section purchased order page tab name attachment i have to comlete it and follow all practiceses of project    . (#tayyab-satti)

--- src/modules/airServices/Assets/PurchaseOrders/AssetsTab/AssetsTab.data.ts
+++
@@ -1,7 +0,0 @@
-export const testComponentAssetsTabs = [
-  // 'Overview',
-  // 'Approvals',
-  // 'Associations',
-  // 'Assets Recieved',
-  'Attachements',
-];
--- src/pages/test-components/tayyab/index.tsx
+++ src/pages/test-components/tayyab/index.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import Layout from '@/layout';
-
-// import { SingleTicketDetail } from '@/modules/airServices/ServicesTickets/SingleTicketDetail';
-
-import HorizontalTabs from '@/components/Tabs/HorizontalTabs';
-import { testComponentAssetsTabs } from '@/modules/airServices/Assets/PurchaseOrders/AssetsTab/AssetsTab.data';
-// import UploadAttachments from '@/modules/airServices/Assets/PurchaseOrders/SinglePurchaseOrderDetails/Attachments/UploadAttachment';
-// import FileAttachment from '@/modules/airServices/Assets/PurchaseOrders/SinglePurchaseOrderDetails/Attachments/FileAttachement';
-import Attachments from '@/modules/airServices/Assets/PurchaseOrders/SinglePurchaseOrderDetails/Attachments';
+import { SinglePurchaseOrderDetailTabs } from '@/modules/airServices/Assets/PurchaseOrders/SinglePurchaseOrderDetails/components/SinglePurchaseOrderDetailTabs';
 
 export const UseTicketViewDetailPage = () => {
   return (
     <>
-      {/* <FileAttachment /> */}
-      {/* <SingleTicketDetail />; */}
-      <HorizontalTabs tabsDataArray={testComponentAssetsTabs}>
-        {/* <UploadAttachments /> */}
-        <Attachments />
-      </HorizontalTabs>
+      <SinglePurchaseOrderDetailTabs />
     </>
   );
 };
 
-//export default UseTicketViewDetailPage;
 UseTicketViewDetailPage.getLayout = function getLayout(page: any) {
   return <Layout>{page}</Layout>;
 };
