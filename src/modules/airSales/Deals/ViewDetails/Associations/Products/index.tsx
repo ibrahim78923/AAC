c3becbff41f1abd3e218c0cf631fdf5b4829ec45
Abdullah Akhlaq <abdullah.akhlaq@ceative.co.uk>
2023-10-13 15:57:49 +0500

    feat(): associations products drawer and conditionals rendering of form (#AIR-507)

--- src/modules/airSales/Deals/ViewDetails/Associations/Products/index.tsx
+++ src/modules/airSales/Deals/ViewDetails/Associations/Products/index.tsx
@@ -1,15 +1,33 @@
-import { Box, Button, Grid, Typography, useTheme } from '@mui/material';
-import React, { useState } from 'react';
-import { styles } from '../Associations.style';
+import React from 'react';
+import { Box, Button, Grid, Typography } from '@mui/material';
+
 import Search from '@/components/Search';
-import { PlusSharedIcon } from '@/assets/icons';
+import { AlertModals } from '@/components/AlertModals';
+
 import TanstackTable from '@/components/Tabel/TanstackTable';
-import { columns } from '../../Tasks/Tasks.data';
-import { TasksTableData } from '@/mock/modules/Deals';
+
+import useProducts from './useProducts';
+
+import { columns } from './Products.data';
+import { productsData } from '@/mock/modules/Deals';
+
+import { PlusSharedIcon } from '@/assets/icons';
+
+import { styles } from '../Associations.style';
+import ProductEditorDrawer from './ProductEditorDrawer';
 
 const Products = () => {
-  const theme = useTheme();
-  const [searchName, setSearchName] = useState('');
+  const {
+    theme,
+    isOpenAlert,
+    setIsOpenAlert,
+    searchName,
+    setSearchName,
+    openDrawer,
+    setOpenDrawer,
+    handleCloseAlert,
+  } = useProducts();
+
   return (
     <Box
       sx={{
@@ -36,17 +54,32 @@ const Products = () => {
             />
             <Button
               variant="contained"
-              sx={{ minWidth: '0px', height: '35px', gap: 0.5 }}
-              // onClick={() => setOpenDrawer('Add')}
+              className="small"
+              sx={{ minWidth: '0px', gap: 0.5 }}
+              onClick={() => setOpenDrawer('Add')}
             >
-              <PlusSharedIcon /> Add New Task
+              <PlusSharedIcon /> Add Products
             </Button>
           </Box>
         </Grid>
         <Grid item xs={12}>
-          <TanstackTable columns={columns} data={TasksTableData} />
+          <TanstackTable
+            columns={columns({ setOpenDrawer, setIsOpenAlert })}
+            data={productsData}
+          />
         </Grid>
       </Grid>
+      <ProductEditorDrawer
+        openDrawer={openDrawer}
+        setOpenDrawer={setOpenDrawer}
+      />
+      <AlertModals
+        message={"You're about to remove a record. Are you Sure?"}
+        type={'info'}
+        open={isOpenAlert}
+        handleClose={handleCloseAlert}
+        handleSubmit={() => {}}
+      />
     </Box>
   );
 };
