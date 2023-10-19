3ea2d400b58bf712900d9e79ef9b0f2abc8b1b8b
Muhammad Shoaib <muhammad.shoaib@ceative.co.uk>
2023-10-19 18:17:55 +0500

    feat(): add software (#190)

--- src/modules/airServices/Assets/Software/SingleSoftwareDetail/Installations/index.tsx
+++ src/modules/airServices/Assets/Software/SingleSoftwareDetail/Installations/index.tsx
@@ -1,16 +1,8 @@
 import { InstallationDetails } from './InstallationDetails';
-import AddDevice from './addDevice';
 
 export const Installations = () => {
   return (
     <>
-      Installation
-      <AddDevice
-        isModalOpen={false}
-        setIsmodalOpen={{}}
-        onSubmit={() => {}}
-        options={[]}
-      />
       <InstallationDetails />
     </>
   );
