bc69e5c5daaed0f2f822b6aa6a3c973e14447cf4
Azeem Aslam <azeem.aslam@ceative.co.uk>
2023-10-19 09:08:36 +0500

    feat() : implement layout in invoices and user reports (#AIR-288)

---
+++ src/modules/superAdmin/Reports/UsersReports/UserReports.data.ts
@@ -0,0 +1,3 @@
+// table columns
+
+// table data
--- src/modules/superAdmin/Reports/UsersReports/index.tsx
+++ src/modules/superAdmin/Reports/UsersReports/index.tsx
@@ -1,5 +1,7 @@
+import { Box } from '@mui/material';
+
 const UsersReports = () => {
-  return <div>Reports Users</div>;
+  return <Box></Box>;
 };
 
 export default UsersReports;
