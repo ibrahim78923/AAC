8fcf7ee7b1c761163a03b40605095ac9ba049a07
Azeem Aslam <azeem.aslam@ceative.co.uk>
2023-10-19 09:40:55 +0500

    feat() : implement layout in invoices and user reports (#AIR-288)

--- src/modules/superAdmin/Reports/UsersReports/index.tsx
+++ src/modules/superAdmin/Reports/UsersReports/index.tsx
@@ -1,7 +1,26 @@
-import { Box } from '@mui/material';
+import Search from '@/components/Search';
+import { Box, Paper, Typography } from '@mui/material';
+import useUserReports from './useUserReports';
 
 const UsersReports = () => {
-  return <Box></Box>;
+  const { searchBy, setSearchBy }: any = useUserReports;
+
+  return (
+    <Box>
+      <Typography variant="h3">User Reports</Typography>
+      <Paper>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="h4">Users</Typography>
+          <Search
+            label="Search Here"
+            searchBy={searchBy}
+            setSearchBy={setSearchBy}
+            width={240}
+          />
+        </Box>
+      </Paper>
+    </Box>
+  );
 };
 
 export default UsersReports;
