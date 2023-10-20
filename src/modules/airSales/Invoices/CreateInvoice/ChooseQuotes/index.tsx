028e40a815b605a9227239e12d81caec52821b84
Azeem Aslam <azeem.aslam@ceative.co.uk>
2023-10-20 12:38:40 +0500

    feat() : implementation of super admin reports (#AIR-288-fixed)

--- src/modules/airSales/Invoices/CreateInvoice/ChooseQuotes/index.tsx
+++ src/modules/airSales/Invoices/CreateInvoice/ChooseQuotes/index.tsx
@@ -1,8 +1,10 @@
-import { Grid, Typography } from '@mui/material';
+import { Box, Button, Divider, Grid, Stack, Typography } from '@mui/material';
 import SearchableSelect from '@/components/SearchableSelect';
 import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
 
 const ChooseQuotes = () => {
+  const router = useRouter();
   const {
     control,
     formState: { errors },
@@ -28,19 +30,52 @@ const ChooseQuotes = () => {
   ];
 
   return (
-    <Grid container>
-      <Grid xs={12} md={4}>
-        <SearchableSelect
-          dropdownData={options}
-          renderOption={renderCustomOption}
-          name="Search candidate"
-          label="Select Quotes"
-          control={control}
-          rules={{ required: 'required field' }}
-          error={!!errors.message}
-        />
+    <Box>
+      <Grid container height="55vh">
+        <Grid xs={12} md={4}>
+          <SearchableSelect
+            dropdownData={options}
+            renderOption={renderCustomOption}
+            name="Search candidate"
+            label="Select Quotes"
+            control={control}
+            rules={{ required: 'required field' }}
+            error={!!errors.message}
+          />
+        </Grid>
       </Grid>
-    </Grid>
+      <Divider sx={{ backgroundColor: '#E5E7EB' }} />
+      <Stack
+        justifyContent="space-between"
+        alignItems="center"
+        direction="row"
+        mt={2}
+      >
+        <Button
+          variant="outlined"
+          // onClick={() => setIsListView(false)}
+        >
+          Back
+        </Button>
+        <Box>
+          <Stack gap="10px" direction="row">
+            <Button
+              variant="outlined"
+              onClick={() => router.push('/air-sales/sales-invoices')}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="outlined"
+              onClick={() => router.push('/air-sales/sales-invoices')}
+            >
+              Skip
+            </Button>
+            <Button variant="contained">Next</Button>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
   );
 };
 
