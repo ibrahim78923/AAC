f69a488b2fd46911c370d330dc4f7f70a994cc25
ali <ali.raza@ceative.co.uk>
2023-10-20 22:24:41 +0500

    feat(): Working on Inventory section attachment tab. (#115)

--- src/modules/airServices/Assets/Contracts/AddNewContract/AddNewContractDetail.tsx
+++ src/modules/airServices/Assets/Contracts/AddNewContract/AddNewContractDetail.tsx
@@ -1,51 +1,35 @@
-import { FormProvider } from '@/components/ReactHookForm';
 import { Grid, Typography } from '@mui/material';
 import { addNewContractData } from './AddNewContractDetailForm';
 import { v4 as uuidv4 } from 'uuid';
 import ItemsDetails from './ItemsDetails';
 
-const AddNewContractDetail = ({
-  handleSubmitForm,
-  methods,
-  handleSubmit,
-}: any) => {
+const AddNewContractDetail = () => {
   return (
     <div>
-      <Grid
-        container
-        justifyContent={'center'}
-        display={'flex'}
-        alignItems={'center'}
-        flexDirection={'row'}
-      >
+      <Grid container justifyContent={'center'} alignItems={'center'}>
         <Grid item xs={12} sx={{ mb: '2rem' }}>
           <Typography variant="h5">General Details</Typography>
         </Grid>
         <Grid item xs={12}>
-          <FormProvider
-            methods={methods}
-            onSubmit={handleSubmit(handleSubmitForm)}
-          >
-            <Grid container spacing={4}>
-              {addNewContractData?.map((item: any) => (
-                <Grid item xs={12} md={item?.md} key={uuidv4()}>
-                  <item.component {...item.componentProps} size={'small'}>
-                    {item?.componentProps?.select ? (
-                      item?.options?.map((option: any) => (
-                        <option key={option?.value} value={option?.value}>
-                          {option?.label}
-                        </option>
-                      ))
-                    ) : item?.heading ? (
-                      item?.heading
-                    ) : (
-                      <ItemsDetails />
-                    )}
-                  </item.component>
-                </Grid>
-              ))}
-            </Grid>
-          </FormProvider>
+          <Grid container spacing={4}>
+            {addNewContractData?.map((item: any) => (
+              <Grid item xs={12} md={item?.md} key={uuidv4()}>
+                <item.component {...item.componentProps} size={'small'}>
+                  {item?.componentProps?.select ? (
+                    item?.options?.map((option: any) => (
+                      <option key={option?.value} value={option?.value}>
+                        {option?.label}
+                      </option>
+                    ))
+                  ) : item?.heading ? (
+                    item?.heading
+                  ) : (
+                    <ItemsDetails />
+                  )}
+                </item.component>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
       </Grid>
     </div>
--- src/modules/airServices/Assets/Contracts/AddNewContract/index.tsx
+++ src/modules/airServices/Assets/Contracts/AddNewContract/index.tsx
@@ -5,50 +5,49 @@ import { FormProvider, RHFDropZone } from '@/components/ReactHookForm';
 import { styles } from '../Contracts.style';
 
 const AddNewContract = () => {
-  const { methodsAddNewContract, handleSubmitForm, handleContractClick } =
+  const { methodsAddNewContract, handleSubmitForm, handleContractClick }: any =
     useAddNewContractDetailForm();
 
   return (
     <>
-      <Grid container spacing={3}>
-        <Grid item lg={9}>
-          <Box sx={styles.parentBoxAddContract}>
-            <AddNewContractDetail
-              SubmitForm={handleSubmitForm}
-              methods={methodsAddNewContract}
-              handleSubmit={methodsAddNewContract.handleSubmit}
-            />
-            <Box sx={{ display: { lg: 'none', xs: 'block' } }}>
-              <FormProvider methods={methodsAddNewContract} onSubmit={() => {}}>
+      <FormProvider
+        methods={methodsAddNewContract}
+        onSubmit={methodsAddNewContract.handleSubmit(handleSubmitForm)}
+      >
+        <Grid container spacing={3}>
+          <Grid item md={9} xs={12}>
+            <Box sx={styles.parentBoxAddContract}>
+              <AddNewContractDetail />
+              <Box sx={{ display: { lg: 'none', xs: 'block' } }}>
                 <RHFDropZone name="file" />
-              </FormProvider>
+              </Box>
             </Box>
-          </Box>
-          <Box sx={styles.childBoxAddContract}>
-            <Button
-              sx={styles.cancelButtonAddContract}
-              onClick={handleContractClick}
-            >
-              Cancel
-            </Button>
-            <Button
-              variant="contained"
-              onClick={handleSubmitForm}
-              sx={{ padding: '25px' }}
-            >
-              Save
-            </Button>
-          </Box>
-        </Grid>
-        <Grid item lg={3} sx={{ display: { xs: 'none', lg: 'block' } }}>
-          <FormProvider
-            methods={methodsAddNewContract}
-            onSubmit={handleSubmitForm}
+            <Box sx={styles.childBoxAddContract}>
+              <Button
+                sx={styles.cancelButtonAddContract}
+                onClick={handleContractClick}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                onClick={handleSubmitForm}
+                sx={{ padding: '25px' }}
+              >
+                Save
+              </Button>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            md={3}
+            xs={12}
+            sx={{ display: { xs: 'none', lg: 'block' } }}
           >
             <RHFDropZone name="file" />
-          </FormProvider>
+          </Grid>
         </Grid>
-      </Grid>
+      </FormProvider>
     </>
   );
 };
