3317d3e503a2788cec6fe3add4caf168fb1f0b07
Ali Haider <ali.r4565081@gmail.com>
2023-10-23 21:33:18 +0500

    feat(hat): working on software component in inventory detail section (#000)

--- src/modules/airServices/Assets/Inventory/SingleInventoryDetail/Software/index.tsx
+++ src/modules/airServices/Assets/Inventory/SingleInventoryDetail/Software/index.tsx
@@ -1,3 +1,26 @@
+import NoData from '@/components/NoData';
+import { Grid } from '@mui/material';
+import NoSoftwareFound from '@/assets/images/modules/LogitechMouse/Expense.png';
+import { softwareData } from './software.data';
+import Card from '@/modules/airServices/common/card';
+import { v4 as uuidv4 } from 'uuid';
+
 export const Software = () => {
-  return <>Software</>;
+  return (
+    <Grid container>
+      <Grid item xs={12} md={12} xl={12}>
+        {!!softwareData?.length ? (
+          softwareData?.map((singleSoftware: any) => (
+            <Card
+              heading={singleSoftware.heading}
+              status={singleSoftware.status}
+              key={uuidv4()}
+            />
+          ))
+        ) : (
+          <NoData image={NoSoftwareFound} message={'No Software found'} />
+        )}
+      </Grid>
+    </Grid>
+  );
 };
