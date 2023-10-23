fc40517147aac91253ea8dbd0caaf1519d248dd7
Ahmer <muhammadahmer.akram@ceative.co.uk>
2023-10-23 21:35:02 +0500

    feat: new incident UI Design Working. (#AASSS-119)

---
+++ src/modules/airServices/Assets/Inventory/SingleInventoryDetail/Associations/ExistingIncident/index.tsx
@@ -0,0 +1,101 @@
+import CommonDrawer from '@/components/CommonDrawer';
+import Search from '@/components/Search';
+import {
+  Box,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  useTheme,
+  Chip,
+} from '@mui/material';
+import { useState } from 'react';
+import { checkboxes, chipColor } from './ExistingIncident.data';
+import { v4 as uuidv4 } from 'uuid';
+import { enqueueSnackbar } from 'notistack';
+
+export default function ExistingIncident({ openDrawer, onClose }: any) {
+  const [searchBy, setSearchBy] = useState<any>();
+
+  const [checkboxValues, setCheckboxValues] = useState<any>({});
+
+  const theme: any = useTheme();
+
+  // Function to handle checkbox change
+  const handleCheckboxChange = (event: any) => {
+    const { id, checked } = event.target;
+    setCheckboxValues((prevValues: any) => ({
+      ...prevValues,
+      [id]: checked,
+    }));
+  };
+
+  const handleSubmit: any = (event: any) => {
+    event.preventDefault();
+    // Filter out checkboxes with values set to false
+    Object.keys(checkboxValues).filter((id) => checkboxValues[id]);
+    // const selectedCheckboxes = Object.keys(checkboxValues).filter(
+    //   (id) => checkboxValues[id],
+    // );
+
+    enqueueSnackbar('Incident Associated Successfully!', {
+      variant: 'success',
+    });
+    onClose(false);
+
+    // console.log('Selected checkboxes on submit:', selectedCheckboxes);
+  };
+
+  return (
+    <CommonDrawer
+      isDrawerOpen={openDrawer}
+      onClose={() => onClose(false)}
+      title={'Associate Existing Incident'}
+      okText={'Associate'}
+      isOk
+      cancelText={'Cancel'}
+      footer
+      submitHandler={handleSubmit}
+    >
+      <Box mt={1}>
+        <Search
+          label={'Search'}
+          searchBy={searchBy}
+          setSearchBy={setSearchBy}
+        />
+      </Box>
+
+      {checkboxes?.map((item) => (
+        <Box
+          border={`1px solid ${theme.palette.grey[400]}`}
+          borderRadius={2}
+          p={1}
+          mt={2}
+          display={'flex'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          key={uuidv4()}
+        >
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={checkboxValues[item?.id] || false}
+                  onChange={handleCheckboxChange}
+                  id={item?.id}
+                />
+              }
+              label={item?.label}
+            />
+          </FormGroup>
+          <Chip
+            label={item?.status}
+            sx={{
+              bgcolor: theme['palette'][`${chipColor(item?.status)}`]['main'],
+              color: theme.palette.common.white,
+            }}
+          />
+        </Box>
+      ))}
+    </CommonDrawer>
+  );
+}
--- src/modules/airServices/Assets/Inventory/SingleInventoryDetail/Associations/index.tsx
+++ src/modules/airServices/Assets/Inventory/SingleInventoryDetail/Associations/index.tsx
@@ -1,23 +1,88 @@
-import { Button, useTheme } from '@mui/material';
-import { Fragment } from 'react';
+import { Button, useTheme, Box, Typography, Chip } from '@mui/material';
+import { Fragment, useState } from 'react';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import NoAssociationFound from '@/assets/images/modules/LogitechMouse/association.png';
 import NoData from '@/components/NoData';
+import NewIncident from './NewIncident';
+import ExistingIncident from './ExistingIncident';
+import DialogBox from './DialogBox';
+import { associationsDataArray, chipColor } from './Associations.data';
+import { v4 as uuidv4 } from 'uuid';
 
 export const Associations = () => {
   const theme: any = useTheme();
 
+  const [openDialog, setOpenDialog] = useState(false);
+  const [openNewIncident, setNewIncident] = useState(false);
+  const [openExistingIncident, setExistingIncident] = useState(false);
+
   return (
     <Fragment>
-      <NoData image={NoAssociationFound} message={'There are no associations'}>
-        <Button
-          variant="outlined"
-          sx={{ backgroundColor: theme.palette.grey[400] }}
+      {associationsDataArray?.length <= 0 ? (
+        <NoData
+          image={NoAssociationFound}
+          message={'There are no associations'}
         >
-          <AddCircleIcon sx={{ mr: 1 }} />
-          Asssociate
-        </Button>
-      </NoData>
+          <Button
+            variant="outlined"
+            sx={{ backgroundColor: theme.palette.grey[400] }}
+            onClick={() => setOpenDialog(true)}
+          >
+            <AddCircleIcon sx={{ mr: 1 }} />
+            Associate
+          </Button>
+        </NoData>
+      ) : (
+        <Fragment>
+          <Box textAlign={'end'}>
+            <Button variant="contained" onClick={() => setOpenDialog(true)}>
+              <AddCircleIcon sx={{ mr: 1 }} />
+              Associate
+            </Button>
+          </Box>
+          {associationsDataArray?.map((item: any) => (
+            <Box
+              border={`1px solid ${theme.palette.grey[400]}`}
+              borderLeft={`8px solid ${
+                theme['palette'][`${chipColor(item?.status)}`]['main']
+              }`}
+              boxShadow={4}
+              borderRadius={2}
+              p={1}
+              mt={2}
+              display={'flex'}
+              justifyContent={'space-between'}
+              alignItems={'center'}
+              key={uuidv4()}
+            >
+              <Typography variant="body2" fontWeight={600}>
+                {item?.ticketNo}
+              </Typography>
+              <Chip
+                label={item?.status}
+                sx={{
+                  bgcolor:
+                    theme['palette'][`${chipColor(item?.status)}`]['main'],
+                  color: theme.palette.common.white,
+                }}
+              />
+            </Box>
+          ))}
+        </Fragment>
+      )}
+
+      <DialogBox
+        openDialog={openDialog}
+        setOpenDialog={setOpenDialog}
+        setNewIncident={setNewIncident}
+        setExistingIncident={setExistingIncident}
+      />
+
+      <NewIncident openDrawer={openNewIncident} onClose={setNewIncident} />
+      <ExistingIncident
+        openDrawer={openExistingIncident}
+        onClose={setExistingIncident}
+      />
     </Fragment>
   );
 };
