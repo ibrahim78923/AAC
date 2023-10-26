e5236119abb3f98e6f106c28492eac3ad880d8e8
zain jahan <zain.jahan@ceative.co.uk>
2023-10-26 22:09:44 +0500

    feat(hat): Working on header section of services customize dashboard(#4)

---
+++ src/components/HeaderDashboard/HeaderDashboard.data.tsx
@@ -0,0 +1,29 @@
+export const ActionsFunction = [
+  {
+    title: 'Copy URL',
+  },
+  {
+    title: 'Email this dashboard',
+  },
+  {
+    title: 'Edit',
+  },
+];
+
+export const DashboardFunction = [
+  {
+    title: 'Services',
+  },
+  {
+    title: 'Services_1',
+  },
+  {
+    title: 'Services_2',
+  },
+  {
+    title: 'Services_3',
+  },
+  {
+    title: 'Services_4',
+  },
+];
--- src/components/HeaderDashboard/index.tsx
+++ src/components/HeaderDashboard/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import { SingleDropdownButton } from '../SingleDropdownButton';
+import { ActionsFunction, DashboardFunction } from './HeaderDashboard.data';
 
 export const HeaderDashboard = ({}: any) => {
   return (
@@ -8,8 +9,19 @@ export const HeaderDashboard = ({}: any) => {
         <Typography variant="h3">Service</Typography>
       </Box>
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="h4">Hi Sam ! Happy to See You again</Typography>
-        <SingleDropdownButton />
+        <Typography variant="h4" sx={{ marginTop: 2 }}>
+          Hi Sam ! Happy to See You again
+        </Typography>
+        <Box sx={{ display: 'flex', gap: 1 }}>
+          <SingleDropdownButton
+            dropdownOptions={ActionsFunction}
+            dropdownName="Action"
+          />
+          <SingleDropdownButton
+            dropdownOptions={DashboardFunction}
+            dropdownName="Dashboards"
+          />
+        </Box>
       </Box>
     </>
   );
