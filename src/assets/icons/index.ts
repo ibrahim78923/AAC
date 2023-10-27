aca92cf7dda04b49b875940c2712c1d185d402eb
ahsan mukhtar <muhammadahsan.mukhtar@ceative.co.uk>
2023-10-27 11:01:34 +0500

    feat(): design Billing Details modal in super admin/org admin in invoices. (#AIR-719)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -143,3 +143,4 @@ export { default as MessageDullIcon } from './modules/SocialComponents/Calling/m
 export { default as ClearIcon } from './modules/SocialComponents/Calling/clear';
 export { default as PhoneGreenIcon } from './modules/SocialComponents/Calling/phone-green';
 export { default as TimeClockIcon } from './modules/airSales/deals/time-clock';
+export { default as AirPlaneIcon } from './shared/airplane';
--- src/modules/superAdmin/BillingAndInvoices/ViewBillingDetails/index.tsx
+++ src/modules/superAdmin/BillingAndInvoices/ViewBillingDetails/index.tsx
@@ -1,46 +1,189 @@
-import { Grid, Typography } from '@mui/material';
+import { Box, Divider, Typography, useTheme } from '@mui/material';
 
 import CommonDrawer from '@/components/CommonDrawer';
+import { AirPlaneIcon } from '@/assets/icons';
+import { v4 as uuidv4 } from 'uuid';
 
 const ViewBillingDetails = ({
   isOpenDrawer,
 
   onClose,
 }: any) => {
+  const theme: any = useTheme();
+  const DataArr = [
+    {
+      bilingType: 'Growth',
+      paymentType: 'paid Monthly',
+      payment: 'pending',
+      paymentDetailes: {
+        invoiceDate: '01/01/2023',
+        dueDate: '6/01/2023',
+        planPrice: '£ 20',
+        additionalUser: '£ 45',
+        AdditionalStorage: '£ 1',
+        Discount: '£ 10',
+        Tax: '£ 27',
+      },
+      totalCost: '£ 158',
+    },
+    {
+      bilingType: 'Basic',
+      paymentType: 'paid Monthly',
+      payment: 'Paid',
+      paymentDetailes: {
+        invoiceDate: '01/01/2023',
+        dueDate: '6/01/2023',
+        planPrice: '£ 20',
+        additionalUser: '£ 45',
+        AdditionalStorage: '£ 1',
+        Discount: '£ 10',
+        Tax: '£ 27',
+      },
+      totalCost: '£ 158',
+    },
+  ];
+
   return (
     <CommonDrawer
       isDrawerOpen={isOpenDrawer}
       onClose={() => onClose(false)}
-      title={'Assign Plan'}
-      okText={'Apply'}
+      title={'Billing Details'}
       isOk
-      cancelText={'Cancel'}
-      footer
+      footer={false}
     >
-      <Grid container>
-        <Grid item sm={12}>
-          <Typography variant="h5" mt={1}>
-            Committed terms
-          </Typography>
-          <Typography variant="body1" mt={3}>
-            12 Month subscription term | Feb 4,2023 to Feb 3,2024
-          </Typography>
-        </Grid>
-        <Grid item sm={6} mt={3}>
-          <Typography variant="subtitle2">
-            Sales Growth plan (Include 2 users)
-          </Typography>
-        </Grid>
-        <Grid item sm={6} mt={3} sx={{ textAlign: 'end' }}>
-          <Typography variant="subtitle2">£ 400.00</Typography>
-        </Grid>
-        <Grid item sm={6} mt={3}>
-          <Typography variant="body1">Feb 2,2023</Typography>
-        </Grid>
-        <Grid item sm={6} mt={3} sx={{ textAlign: 'end' }}>
-          <Typography variant="body1">After £400.00 custom </Typography>
-        </Grid>
-      </Grid>
+      {DataArr.map((data: any) => (
+        <Box
+          key={uuidv4()}
+          sx={{
+            boxShadow: '0px 3px 6px 0px rgba(107, 114, 128, 0.10)',
+            padding: '10px',
+            borderRadius: '8px',
+            marginBottom: '15px',
+          }}
+        >
+          <Box sx={{ display: 'flex', alignItems: 'center', my: '15px' }}>
+            <Box
+              sx={{
+                background: theme?.palette?.primary?.lighter,
+                padding: '5px 8px',
+                marginRight: '13px',
+              }}
+            >
+              <AirPlaneIcon />
+            </Box>
+            <Box>
+              <Typography
+                variant="overline"
+                sx={{ textTransform: 'capitalize' }}
+              >
+                Air Sales ( {data.bilingType})
+              </Typography>
+              <Typography variant="body1">{data.paymentType}</Typography>
+            </Box>
+
+            <Box sx={{ ml: 'auto' }}>
+              <Typography
+                variant="body3"
+                sx={{
+                  background:
+                    data.payment === 'pending'
+                      ? theme?.palette?.warning?.main
+                      : theme?.palette?.primary?.main,
+                  borderRadius: '15px',
+                  padding: '7px',
+                  color: 'white',
+                }}
+              >
+                {data.payment}
+              </Typography>
+            </Box>
+          </Box>
+          <Divider />
+
+          <Box sx={{ display: 'flex', alignItems: 'center', mt: '15px' }}>
+            <Typography variant="caption">
+              Invoice Date: {data.paymentDetailes.invoiceDate}
+            </Typography>
+            <Box sx={{ ml: 'auto' }}>
+              <Typography variant="caption">
+                Due Date: {data.paymentDetailes.dueDate}
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box sx={{ display: 'flex', alignItems: 'center', mt: '15px' }}>
+            <Typography variant="caption">Plan Price</Typography>
+            <Box sx={{ ml: 'auto' }}>
+              <Typography variant="overline">
+                {data.paymentDetailes.planPrice}
+              </Typography>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', mt: '15px' }}>
+            <Typography variant="caption">
+              3 Additional Users (£ 15/user)
+            </Typography>
+            <Box sx={{ ml: 'auto' }}>
+              <Typography variant="overline">
+                {data.paymentDetailes.additionalUser}
+              </Typography>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', mt: '15px' }}>
+            <Typography variant="caption">
+              Additional Storage (£ 1/GB)
+            </Typography>
+            <Box sx={{ ml: 'auto' }}>
+              <Typography variant="overline">
+                {data.paymentDetailes.AdditionalStorage}
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box sx={{ display: 'flex', alignItems: 'center', mt: '15px' }}>
+            <Typography variant="caption">
+              <Typography
+                variant="overline"
+                sx={{ textTransform: 'capitalize' }}
+              >
+                Discount{' '}
+              </Typography>{' '}
+              (10%)
+            </Typography>
+            <Box sx={{ ml: 'auto' }}>
+              <Typography variant="overline">
+                {data.paymentDetailes.Discount}
+              </Typography>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', my: '15px' }}>
+            <Typography variant="caption">
+              <Typography
+                variant="overline"
+                sx={{ textTransform: 'capitalize' }}
+              >
+                Tax{' '}
+              </Typography>{' '}
+              (Vat 20%)
+            </Typography>
+            <Box sx={{ ml: 'auto' }}>
+              <Typography variant="overline">
+                {data.paymentDetailes.Tax}
+              </Typography>
+            </Box>
+          </Box>
+          <Divider />
+          <Box sx={{ display: 'flex', alignItems: 'center', mt: '15px' }}>
+            <Typography variant="overline" sx={{ textTransform: 'capitalize' }}>
+              Total Cost{' '}
+            </Typography>
+
+            <Box sx={{ ml: 'auto' }}>
+              <Typography variant="overline">{data.totalCost}</Typography>
+            </Box>
+          </Box>
+        </Box>
+      ))}
     </CommonDrawer>
   );
 };
