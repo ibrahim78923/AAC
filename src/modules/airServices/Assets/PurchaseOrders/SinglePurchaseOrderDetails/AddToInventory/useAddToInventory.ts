a3cc5f5803d318ae9523a9e385a58fba598deed7
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2023-10-30 16:56:04 +0000

    Squashed commit of the following
    commit 8ee7abc0 
    Author: tayyab khan satti <tayyabkhan.satti@ceative.co.uk> 
    Date: Mon Oct 30 2023 21:23:16 GMT+0500 (Pakistan Standard Time) 
    
        feat(hat): Today i have to worked on knowledge base modeule article filter button      . (#new-tayyab)
    
    
    commit b23f687b 
    Author: tayyab khan satti <tayyabkhan.satti@ceative.co.uk> 
    Date: Mon Oct 30 2023 20:40:32 GMT+0500 (Pakistan Standard Time) 
    
        feat(hat): Today i have to worked on knowledge base modeule article filter button      . (#new-tayyab)
    
    
    commit 1b0e577f 
    Author: tayyab khan satti <tayyabkhan.satti@ceative.co.uk> 
    Date: Mon Oct 30 2023 19:01:59 GMT+0500 (Pakistan Standard Time) 
    
        feat(hat): Today i have to worked on knowledge base modeule article filter button      . (#new-tayyab)
    
    
    commit b01ddd54 
    Author: tayyab khan satti <tayyabkhan.satti@ceative.co.uk> 
    Date: Mon Oct 30 2023 16:31:25 GMT+0500 (Pakistan Standard Time) 
    
        feat(hat): Today i have to worked on knowledge base modeule article filter button      . (#new-tayyab)
    
    
    commit 9d1b1d51 
    Author: tayyab khan satti <tayyabkhan.satti@ceative.co.uk> 
    Date: Fri Oct 27 2023 22:09:37 GMT+0500 (Pakistan Standard Time) 
    
        Merge branch 'dev-evening' of https://git-codecommit.eu-west-2.amazonaws.com/v1/repos/AAC-FE into new-tayyab
    
    
    commit ed164899 
    Author: tayyab khan satti <tayyabkhan.satti@ceative.co.uk> 
    Date: Fri Oct 27 2023 20:45:48 GMT+0500 (Pakistan Standard Time) 
    
        feat(hat): Today i have to resolve issues in add to inventory n of purchas      . (#new-tayyab)

--- src/modules/airServices/Assets/PurchaseOrders/SinglePurchaseOrderDetails/AddToInventory/useAddToInventory.ts
+++ src/modules/airServices/Assets/PurchaseOrders/SinglePurchaseOrderDetails/AddToInventory/useAddToInventory.ts
@@ -1,18 +1,22 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import {
-  addInventoryDefaultValuesTwo,
-  addInventoryValidationSchemaTwo,
+  addToInventoryItemStatusDefaultValues,
+  addToInventoryItemStatusValidationSchema,
   addInventoryDefaultValuesOne,
   addInventoryDefaultValuesOneUpdate,
   addInventoryValidationSchemaOne,
   addInventoryValidationSchemaUpdate,
+  addToInventoryItemAdded,
 } from './AddToInventory.data';
+import { useState } from 'react';
+import { enqueueSnackbar } from 'notistack';
 
-export default function useAddToInventoryDrawer() {
+export default function useAddToInventoryDrawer(props: any) {
+  const { setIsADrawerOpen } = props;
   const methodsTwo: any = useForm({
-    resolver: yupResolver(addInventoryValidationSchemaTwo),
-    defaultValues: addInventoryDefaultValuesTwo,
+    resolver: yupResolver(addToInventoryItemStatusValidationSchema),
+    defaultValues: addToInventoryItemStatusDefaultValues,
   });
 
   const { handleSubmit: handleSubmitTwo } = methodsTwo;
@@ -29,7 +33,35 @@ export default function useAddToInventoryDrawer() {
   });
   const { handleSubmit: handleSubmitNo } = methodsNo;
   const onSubmit = async () => {};
-
+  const [boolVariable, setBoolVariable] = useState(true);
+  const [toShow, setToShow] = useState(true);
+  const handleRadioChange = (event: { target: { value: string } }) => {
+    setToShow(event.target.value === 'Add New');
+  };
+  const submitHandlerYes = handleSubmitYes(() => {
+    setBoolVariable(false);
+    methodsYes.reset(addInventoryDefaultValuesOne);
+  });
+  const submitHandlerNo = handleSubmitNo(() => {
+    enqueueSnackbar('item added to inventory Successfully', {
+      variant: 'success',
+    });
+    methodsNo.reset(addInventoryDefaultValuesOneUpdate);
+  });
+  const submitHandlerTwo = handleSubmitTwo(() => {
+    enqueueSnackbar('item added to inventory Successfully', {
+      variant: 'success',
+    });
+    setIsADrawerOpen(false);
+    setBoolVariable(true);
+    methodsTwo.reset(addToInventoryItemStatusDefaultValues);
+  });
+  const filteredYes = addToInventoryItemAdded.filter((item: any) => {
+    return item.toShow === 'Yes';
+  });
+  const filteredNo = addToInventoryItemAdded.filter((item: any) => {
+    return item.toShow === 'No';
+  });
   return {
     methodsTwo,
     handleSubmitTwo,
@@ -38,5 +70,14 @@ export default function useAddToInventoryDrawer() {
     methodsNo,
     methodsYes,
     handleSubmitNo,
+    boolVariable,
+    filteredYes,
+    filteredNo,
+    submitHandlerTwo,
+    submitHandlerNo,
+    submitHandlerYes,
+    handleRadioChange,
+    toShow,
+    setToShow,
   };
 }
