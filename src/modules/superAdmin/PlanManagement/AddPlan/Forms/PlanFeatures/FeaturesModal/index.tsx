2b221ef3134cd3bff685bbaebbfee0e9c9099f16
zahir abbas <zahir.abbas@ceative.co.uk>
2023-10-30 03:55:35 -0700

    feat(): added style file for Feature modal and added in index.tsx (#AACSA-206)

--- src/modules/superAdmin/PlanManagement/AddPlan/Forms/PlanFeatures/FeaturesModal/index.tsx
+++ src/modules/superAdmin/PlanManagement/AddPlan/Forms/PlanFeatures/FeaturesModal/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Grid, Box, Button, useTheme } from '@mui/material';
+import { Grid, Box, Button } from '@mui/material';
 import { FormProvider } from '@/components/ReactHookForm';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
@@ -14,25 +14,14 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { setFeatureDetails } from '@/redux/slices/planManagement/planManagementSlice';
-
-const style = {
-  position: 'absolute' as const,
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+import { styles } from './FeaturesModal.style';
+import CloseIcon from '@/assets/icons/shared/close-icon';
 
 export default function FeaturesModal({
   openFeaturesModal,
   handleCloseFeaturesModal,
   featureName,
 }: any) {
-  const theme = useTheme();
   const dispatch = useDispatch();
 
   const methods: any = useForm({
@@ -59,55 +48,48 @@ export default function FeaturesModal({
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h3" component="h2">
-            {featureName}
-          </Typography>
-          <Typography
-            id="modal-modal-description"
-            variant="body2"
-            sx={{ mt: 2, fontWeight: 500 }}
-          >
-            {featureName}
-          </Typography>
-          <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-            <Grid container spacing={4}>
-              {dataArrayFeatures?.map((item: any) => (
-                <Grid item xs={12} md={item?.md} key={uuidv4()}>
-                  <item.component {...item.componentProps} size={'small'}>
-                    {item?.componentProps?.select &&
-                      item?.options?.map((option: any) => (
-                        <option key={option?.value} value={option?.value}>
-                          {option?.label}
-                        </option>
-                      ))}
-                  </item.component>
+        <Box sx={styles.parentBox}>
+          <Box sx={styles.modalBox}>
+            <Box sx={styles.innerBoxOne}>
+              <Box sx={{ cursor: 'pointer' }}>
+                <CloseIcon />
+              </Box>
+            </Box>
+            <Box sx={{ margin: '20px 0' }}>
+              <Typography id="modal-modal-title" variant="h3" component="h2">
+                {featureName}
+              </Typography>
+              <Typography
+                id="modal-modal-description"
+                variant="body2"
+                sx={{ mt: 2, fontWeight: 500 }}
+              >
+                {featureName}
+              </Typography>
+              <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+                <Grid container spacing={4}>
+                  {dataArrayFeatures?.map((item: any) => (
+                    <Grid item xs={12} md={item?.md} key={uuidv4()}>
+                      <item.component {...item.componentProps} size={'small'}>
+                        {item?.componentProps?.select &&
+                          item?.options?.map((option: any) => (
+                            <option key={option?.value} value={option?.value}>
+                              {option?.label}
+                            </option>
+                          ))}
+                      </item.component>
+                    </Grid>
+                  ))}
                 </Grid>
-              ))}
-            </Grid>
-            <Grid item sm={8} style={{ textAlign: 'end' }}>
-              <Button
-                className="small"
-                variant="outlined"
-                onClick={handleCloseFeaturesModal}
-                sx={{
-                  border: `1px solid ${theme?.palette?.custom?.dark}`,
-                  color: theme?.palette?.custom?.main,
-                  width: '112px',
-                }}
-              >
-                Cancel
+              </FormProvider>
+            </Box>
+            <Box sx={styles.buttonBox}>
+              <Button variant="outlined">No</Button>
+              <Button variant="contained" onClick={handleSubmit}>
+                Yes
               </Button>
-              <Button
-                variant="contained"
-                className="small"
-                type="submit"
-                sx={{ marginLeft: '10px' }}
-              >
-                Save
-              </Button>
-            </Grid>
-          </FormProvider>
+            </Box>
+          </Box>
         </Box>
       </Modal>
     </div>
