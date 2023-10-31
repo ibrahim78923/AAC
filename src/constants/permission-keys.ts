e68fdf2840de5b57c96680382539fa7ba3b0c328
ahsan mukhtar <muhammadahsan.mukhtar@ceative.co.uk>
2023-10-31 15:20:16 +0500

    feat(): create permssion constant in constant.key file for permissions. (#AIR-719)

--- src/constants/permission-keys.ts
+++ src/constants/permission-keys.ts
@@ -12,3 +12,12 @@ export const SUPER_ADMIN_DASHBOARD_PERMISSIONS = {
 export const SUPER_ADMIN_PLAN_MANAGEMENT_PERMISSIONS = {
   PLAN_LIST: 'plan-list',
 };
+
+export const SUPER_ADMIN_BILLING_INVOICES_PERMISSIONS = {
+  BILLING_INVOICES_LIST: 'billing-and-invoice-list',
+  BILLING_SEARCH_AND_FILTER: 'billing-search-and-filter',
+  ASSIGN_PLAN: 'assign-plan',
+  BILLING_EDIT_PLAN: 'billing-edit-plan',
+  GENERATE_INVOICE: 'generate-invoice',
+  VIEW_BILLING_DETAILS: 'view-billing-details',
+};
--- src/modules/superAdmin/BillingAndInvoices/constant.tsx
+++
@@ -1,8 +0,0 @@
-export const SUPER_ADMIN_BILLING_INVOICES_PERMISSIONS = {
-  BILLING_INVOICES_LIST: 'billing-and-invoice-list',
-  BILLING_SEARCH_AND_FILTER: 'billing-search-and-filter',
-  ASSIGN_PLAN: 'assign-plan',
-  BILLING_EDIT_PLAN: 'billing-edit-plan',
-  GENERATE_INVOICE: 'generate-invoice',
-  VIEW_BILLING_DETAILS: 'view-billing-details',
-};
