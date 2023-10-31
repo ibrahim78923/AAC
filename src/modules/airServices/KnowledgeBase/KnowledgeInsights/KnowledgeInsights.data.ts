3064a20f4bf6347412749bf174fd6c8433d5b05a
ali <ali.raza@ceative.co.uk>
2023-10-31 17:41:07 +0500

    fix(): Fixing and updating & Knowlegde Insights code. (#91)

---
+++ src/modules/airServices/KnowledgeBase/KnowledgeInsights/KnowledgeInsights.data.ts
@@ -0,0 +1,37 @@
+export const trendingInsightsData = [
+  {
+    id: '1',
+    insights: 'system memory',
+    mentions: 12,
+  },
+  {
+    id: '2',
+    insights: 'password reset',
+    mentions: 15,
+  },
+  {
+    id: '3',
+    insights: 'security firewall',
+    mentions: 456,
+  },
+  {
+    id: '4',
+    insights: 'leave approval',
+    mentions: 454,
+  },
+  {
+    id: '4',
+    insights: 'saved files',
+    mentions: 124,
+  },
+  {
+    id: '4',
+    insights: 'directory',
+    mentions: 214,
+  },
+  {
+    id: '4',
+    insights: 'salary',
+    mentions: 123,
+  },
+];
--- src/modules/airServices/KnowledgeBase/KnowledgeInsights/index.tsx
+++ src/modules/airServices/KnowledgeBase/KnowledgeInsights/index.tsx
@@ -1,44 +1,8 @@
 import { Grid, Typography } from '@mui/material';
 import { styles } from './KnowledgeInsights.style';
+import { trendingInsightsData } from './KnowledgeInsights.data';
 import { v4 as uuidv4 } from 'uuid';
 
-const trendingInsightsData = [
-  {
-    id: '1',
-    insights: 'system memory',
-    mentions: 12,
-  },
-  {
-    id: '2',
-    insights: 'password reset',
-    mentions: 15,
-  },
-  {
-    id: '3',
-    insights: 'security firewall',
-    mentions: 456,
-  },
-  {
-    id: '4',
-    insights: 'leave approval',
-    mentions: 454,
-  },
-  {
-    id: '4',
-    insights: 'saved files',
-    mentions: 124,
-  },
-  {
-    id: '4',
-    insights: 'directory',
-    mentions: 214,
-  },
-  {
-    id: '4',
-    insights: 'salary',
-    mentions: 123,
-  },
-];
 export const KnowledgeInsights = () => {
   return (
     <>
