ec31589ce7fb498b57698da84d7b163241547cdc
Azeem Aslam <azeem.aslam@ceative.co.uk>
2023-11-13 13:04:48 +0500

    feat() : implement the compare post data (#AACSA-224)

--- src/pages/compare-post/index.tsx
+++ src/pages/compare-post/index.tsx
@@ -1,5 +1,5 @@
 import Layout from '@/layout';
-import CompareSocialPost from '@/modules/CompareSocialPost';
+import CompareSocialPost from '@/modules/airMarketer/CompareSocialPost';
 
 const ComparePostPage = () => {
   return <CompareSocialPost />;
