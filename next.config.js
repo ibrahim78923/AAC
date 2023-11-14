06fa131c32cc30853cad28e4a77235adc0ff90f1
Abdullah Akhlaq <abdullah.akhlaq@ceative.co.uk>
2023-11-14 15:07:49 +0500

    feat(): product suite and authentications async await domain  (#AIR-733)

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  images: {
+    domains: ['airapplecart-public.s3.eu-west-2.amazonaws.com'],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
