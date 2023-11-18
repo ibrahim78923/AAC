d493bdf4d8755ad4ce248485c9e48bce5b969c25
unknown <unknown@gmail.com>
2023-11-18 17:33:40 +0500

    feat(): conversation design is in progress (#000)

--- src/modules/airServices/Assets/Inventory/SingleInventoryDetail/Associations/NewIncident/useNewIncident.tsx
+++ src/modules/airServices/Assets/Inventory/SingleInventoryDetail/Associations/NewIncident/useNewIncident.tsx
@@ -5,74 +5,20 @@ import {
 } from './NewIncident.data';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { enqueueSnackbar } from 'notistack';
-import { usePostNewIncidentMutation } from '@/services/airServices/assets/inventory/single-inventory-details/associations/new-incident';
 
 export const useNewIncident = ({ onClose }: any) => {
   const methods: any = useForm({
     resolver: yupResolver(incidentFormValidationSchema),
-    incidentFormDefaultValues,
+    defaultValues: incidentFormDefaultValues,
   });
 
   const { handleSubmit } = methods;
-  const [newIncidentData] = usePostNewIncidentMutation();
 
-  const onSubmit = async (newIncident: any) => {
-    const {
-      requester,
-      subject,
-      description,
-      category,
-      status,
-      priority,
-      department,
-      source,
-      impact,
-      agent,
-      plannedStartDate,
-      plannedStartTime,
-      plannedEndDate,
-      plannedEndTime,
-      plannedEffort,
-      associateAssets,
-    } = newIncident;
-    const modifiedFormData = {
-      status,
-      details: {
-        ...(department && { department }),
-        ...(source && { source }),
-        ...(requester && { requester }),
-        ...(impact && { impact }),
-        ...(agent && { agent }),
-        ...(plannedStartDate && { plannedStartDate }),
-        ...(plannedStartTime && { plannedStartTime }),
-        ...(plannedEndDate && { plannedEndDate }),
-        ...(plannedEndTime && { plannedEndTime }),
-        ...(plannedEffort && { plannedEffort }),
-        ...(associateAssets && { associateAssets }),
-        ...(description && { description }),
-        ...(category && { category }),
-      },
-      subject,
-      pirority: priority,
-      internalType: 'INTERNAL',
-      type: 'INTERNAL',
-      isChildTicket: false,
-    };
-
-    const postInventoryParameter = {
-      body: modifiedFormData,
-    };
-    try {
-      const response = await newIncidentData(postInventoryParameter)?.unwrap();
-      enqueueSnackbar(response?.message ?? 'Ticket Added Successfully', {
-        variant: 'success',
-      });
-      onClose(false);
-    } catch (error) {
-      enqueueSnackbar('There is something wrong', {
-        variant: 'error',
-      });
-    }
+  const onSubmit = async () => {
+    enqueueSnackbar('Incident Associated Successfully!', {
+      variant: 'success',
+    });
+    onClose(false);
   };
   return { handleSubmit, onSubmit, methods };
 };
