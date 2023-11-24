0d14e5426f10baa4367a6305c1ef53cf3cbbd0c5
Ali Haider <ali.haider@ceative.co.uk>
2023-11-24 16:32:07 +0500

    feat(hat): working on AASSS-537-location-import-design (#537)

---
+++ src/modules/airServices/Settings/AssetManagement/Location/Header/index.tsx
@@ -0,0 +1,47 @@
+import { Box, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { AIR_SERVICES } from '@/constants';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { ImportLocation } from '../ImportLocation';
+
+export const Header = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const router = useRouter();
+  const handleLocationSetting = () => {
+    router?.push({
+      pathname: AIR_SERVICES?.ASSET_MANAGEMENT_SETTINGS,
+    });
+  };
+
+  return (
+    <>
+      <Box
+        display={'flex'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+      >
+        <Box
+          sx={{ cursor: 'pointer' }}
+          display={'flex'}
+          alignItems={'center'}
+          gap={1}
+        >
+          <ArrowBackIcon onClick={() => handleLocationSetting()} />
+          <Typography variant="h5">Location</Typography>
+        </Box>
+        <Box display={'flex'} alignItems={'center'} gap={1}>
+          <Button variant="outlined" onClick={() => setIsDrawerOpen(true)}>
+            Import
+          </Button>
+          <Button variant="contained">New Location</Button>
+        </Box>
+      </Box>
+      <ImportLocation
+        setIsDrawerOpen={setIsDrawerOpen}
+        isDrawerOpen={isDrawerOpen}
+      />
+    </>
+  );
+};
