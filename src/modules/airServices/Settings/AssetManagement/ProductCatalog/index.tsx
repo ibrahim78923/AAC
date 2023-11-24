770be6d29a3a129092ce3787bba32c86d53df7a9
unknown <unknown@gmail.com>
2023-11-24 14:36:36 +0500

    feat(): working on product catalog list desig n (#1038)

--- src/modules/airServices/Settings/AssetManagement/ProductCatalog/index.tsx
+++ src/modules/airServices/Settings/AssetManagement/ProductCatalog/index.tsx
@@ -1,3 +1,53 @@
+import { PageTitledHeader } from '@/components/PageTitledHeader';
+import Search from '@/components/Search';
+import TanstackTable from '@/components/Table/TanstackTable';
+import { useProductCatalog } from './useProductCatalog';
+import { EXPORT_TYPE } from '@/constants/strings';
+import {
+  PRODUCT_LISTS_ACTION_CONSTANTS,
+  productListsData,
+} from './ProductCatalog.data';
+import { Box } from '@mui/material';
+
 export const ProductCatalog = () => {
-  return <>ProductCatalog</>;
+  const {
+    search,
+    setSearch,
+    getProductListsDataExport,
+    productListsColumn,
+    productListActionComponent,
+    hasProductAction,
+    router,
+    setProductListAction,
+  } = useProductCatalog();
+  return (
+    <>
+      <PageTitledHeader
+        title={'Product'}
+        addTitle={'New Product'}
+        hasExport
+        hasImport
+        handleExcelExport={() => getProductListsDataExport?.(EXPORT_TYPE?.XLS)}
+        handleCsvExport={() => getProductListsDataExport?.(EXPORT_TYPE?.CSV)}
+        handleAction={() => {}}
+        handleImport={() =>
+          setProductListAction(PRODUCT_LISTS_ACTION_CONSTANTS?.IMPORT)
+        }
+      />
+      <Search
+        label="search"
+        width="100%"
+        value={search}
+        onChange={(e: any) => setSearch(e?.target?.value)}
+      />
+      <Box marginY={3}></Box>
+      <TanstackTable
+        columns={productListsColumn}
+        data={productListsData}
+        isPagination
+      />
+      {hasProductAction &&
+        productListActionComponent?.[router?.query?.ticketAction as string]}
+    </>
+  );
 };
