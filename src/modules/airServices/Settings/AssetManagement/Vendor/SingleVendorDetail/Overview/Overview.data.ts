f2d48f7290f3128718f65157cabc7c31453cbd50
Ali Haider <ali.haider@ceative.co.uk>
2023-11-24 18:21:44 +0500

    feat(hat): working on AASSS-1067-Vendors-viewdetail-overview-design (#1067)

---
+++ src/modules/airServices/Settings/AssetManagement/Vendor/SingleVendorDetail/Overview/Overview.data.ts
@@ -0,0 +1,35 @@
+export const overviewData = [
+  {
+    id: '1',
+    heading: 'General Details',
+    detailsData: [
+      { name: 'Contact Number', detail: 'CNTR-8' },
+      { name: 'Vendor', detail: 'Dell' },
+      { name: 'Type', detail: 'Lease' },
+      { name: 'Status', detail: 'Active' },
+      { name: 'Cost', detail: '£654' },
+      { name: 'Approver', detail: '--' },
+      { name: 'Validity', detail: '28 Mar, 2023 to 28 Dec, 2024' },
+    ],
+  },
+  {
+    id: '2',
+    heading: 'Items and Cost Details',
+    detailsData: [
+      { name: 'Software', detail: 'Microsoft' },
+      { name: 'Pricing Model', detail: 'Fixes' },
+      { name: 'Cost', detail: '£654' },
+      { name: 'Count', detail: 'Unlimited' },
+      { name: 'Comments', detail: '--' },
+      { name: 'Billing Cycle', detail: 'One Time' },
+    ],
+  },
+  {
+    id: '3',
+    heading: 'Software License Properties',
+    detailsData: [
+      { name: 'License Type', detail: 'Volume' },
+      { name: 'License Key', detail: 'Volume' },
+    ],
+  },
+];
