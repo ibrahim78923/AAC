989cfa407422cd4ae0995872f8659fbeb5ed2d08
Muhammad Fahad <muhammad.fahad@ceative.co.uk>
2023-11-24 20:28:23 +0500

    feat(): create Department inside of Settings > User-Management.(#AASSS-52)

--- src/modules/airServices/KnowledgeBase/Articles/ViewArticles/ViewArticlesDetail/index.tsx
+++ src/modules/airServices/KnowledgeBase/Articles/ViewArticles/ViewArticlesDetail/index.tsx
@@ -41,11 +41,7 @@ export const ViewArticlesDetail = () => {
               View Article
             </Typography>
           </Box>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: articleEditorData ? articleEditorData : '',
-            }}
-          ></div>
+          <div dangerouslySetInnerHTML={{ __html: articleEditorData }}></div>
         </Grid>
         <Grid item xs={12} lg={3}>
           <Box sx={styles?.sideStyle(theme)}>
