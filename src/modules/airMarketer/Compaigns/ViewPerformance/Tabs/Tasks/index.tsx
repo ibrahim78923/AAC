b2fd2a038518be6a67bd1d9a5d52ab52dec82db7
zahir <zahirr060@gmail.com>
2023-11-26 06:45:10 -0800

    feat(): added delete modal and make some states in usetasks hooks(#AAM-616)

--- src/modules/airMarketer/Compaigns/ViewPerformance/Tabs/Tasks/index.tsx
+++ src/modules/airMarketer/Compaigns/ViewPerformance/Tabs/Tasks/index.tsx
@@ -2,12 +2,108 @@ import CustomPagination from '@/components/CustomPagination';
 import TanstackTable from '@/components/Table/TanstackTable';
 import { columns } from './Tasks.data';
 import { tableData } from '@/mock/modules/airMarketer/Compaigns/Tasks';
+import { Box, Button, Menu, MenuItem } from '@mui/material';
+import { AlertModalDeleteIcon, DownIcon } from '@/assets/icons';
+import useTasks from './useTasks';
+import EditTask from './EditTask';
+import { AlertModals } from '@/components/AlertModals';
 
 const Tasks = () => {
+  const {
+    anchorEl,
+    theme,
+    actionMenuOpen,
+    handleActionsMenuClose,
+    handleActionsMenuClick,
+    isOpenEditTaskDrawer,
+    setIsOpenEditTaskDrawer,
+    handleTaskDrawer,
+    isOpenDeleteDrawer,
+    handleDeleteModal,
+    setIsOpenDeleteDrawer,
+  } = useTasks();
   return (
     <>
+      <Box sx={{ padding: '16px 24px' }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: '19px',
+          }}
+        ></Box>
+        <Box
+          mt={2}
+          mb={3}
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            gap: '10px',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              alignItems: 'center',
+              gap: '10px',
+            }}
+          >
+            <Button
+              id="basic-button"
+              aria-controls={'basic-menu'}
+              aria-haspopup="true"
+              aria-expanded={'true'}
+              onClick={handleActionsMenuClick}
+              sx={{
+                color: theme.palette.grey[500],
+                height: '40px',
+                border: '1.5px solid #e7e7e9',
+                '@media (max-width:581px)': {
+                  width: '100%',
+                },
+              }}
+            >
+              Actions &nbsp; <DownIcon />
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={actionMenuOpen}
+              onClose={handleActionsMenuClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleTaskDrawer}>Edit</MenuItem>
+
+              <MenuItem onClick={handleDeleteModal}>Delete</MenuItem>
+            </Menu>
+          </Box>
+        </Box>
+      </Box>
       <TanstackTable columns={columns} data={tableData} />
       <CustomPagination count={1} rowsPerPageOptions={[1, 2]} entriePages={1} />
+      {isOpenEditTaskDrawer && (
+        <EditTask
+          isOpenDrawer={isOpenEditTaskDrawer}
+          onClose={() => setIsOpenEditTaskDrawer(false)}
+        />
+      )}
+      {isOpenDeleteDrawer && (
+        <AlertModals
+          message="Are you sure you want to delete this broadcast?"
+          type="Delete SMS Broadcast"
+          typeImage={<AlertModalDeleteIcon />}
+          open={isOpenDeleteDrawer}
+          handleClose={() => setIsOpenDeleteDrawer(false)}
+          handleSubmit={() => setIsOpenDeleteDrawer(false)}
+        />
+      )}
     </>
   );
 };
