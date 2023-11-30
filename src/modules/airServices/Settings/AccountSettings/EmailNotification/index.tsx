f598b0ac76f183ac95ed2df1c713a15c49d89495
umer-arshad <umer.arshad@ceative.co.uk>
2023-11-30 18:21:52 +0500

    feat(hat): Working on Email-Notification design (#AASSS-590)

--- src/modules/airServices/Settings/AccountSettings/EmailNotification/index.tsx
+++ src/modules/airServices/Settings/AccountSettings/EmailNotification/index.tsx
@@ -1,12 +1,24 @@
 import { Box, Divider, Typography } from '@mui/material';
 import { EmailNotificationDetailTabs } from './EmailNotificationDetailTabs';
-import BackArrow from '@/assets/icons/modules/airSales/deals/back-arrow';
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Router from 'next/router';
+import { AIR_SERVICES } from '@/constants';
 
 export const EmailNotification = () => {
   return (
     <Box>
       <Box display={'flex'} alignItems={'center'} gap={1}>
-        <BackArrow />
+        <Box sx={{ cursor: 'pointer' }}>
+          <ArrowBackIcon
+            sx={{ cursor: 'pointer' }}
+            onClick={() =>
+              Router?.push({
+                pathname: AIR_SERVICES?.ACCOUNT_SETTINGS,
+              })
+            }
+          />
+        </Box>
         <Typography variant="h3">Email Notification</Typography>
       </Box>
       <Box mt={2}>
