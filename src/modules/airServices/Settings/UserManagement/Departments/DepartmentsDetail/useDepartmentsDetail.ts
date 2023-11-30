3b3096e7c044969f992a309b4fb210ba9991b01f
Muhammad Fahad <muhammad.fahad@ceative.co.uk>
2023-11-30 18:24:24 +0500

    feat(): Implement Edit Department in Setting> User Management> Departments.(#1088)

--- src/modules/airServices/Settings/UserManagement/Departments/DepartmentsDetail/useDepartmentsDetail.ts
+++ src/modules/airServices/Settings/UserManagement/Departments/DepartmentsDetail/useDepartmentsDetail.ts
@@ -1,11 +1,18 @@
 import { useState } from 'react';
 import { useTheme } from '@mui/material';
 import { enqueueSnackbar } from 'notistack';
+import { useForm } from 'react-hook-form';
 import { NOTISTACK_VARIANTS } from '@/constants/strings';
+import { yupResolver } from '@hookform/resolvers/yup';
+import {
+  departmentFormValidation,
+  departmentFormValues,
+} from '../DepartmentsFormModal/DepartmentsFormModal.data';
 
 export const useDepartmentsDetail = () => {
   const [actionPop, setActionPop] = useState<HTMLElement | null>(null);
   const [openDelete, setOpenDelete] = useState<boolean>(false);
+  const [openEdit, setOpenEdit] = useState<boolean>(false);
   const handleActionClick = (event: React.MouseEvent<HTMLElement>) => {
     setActionPop(event?.currentTarget);
   };
@@ -24,6 +31,18 @@ export const useDepartmentsDetail = () => {
     setOpenDelete(false);
     setActionPop(null);
   };
+  const editFormMethod = useForm({
+    resolver: yupResolver(departmentFormValidation),
+    defaultValues: departmentFormValues,
+  });
+  const { handleSubmit, reset } = editFormMethod;
+  const submitEditForm = async () => {
+    enqueueSnackbar('Department Edit Successfully', {
+      variant: NOTISTACK_VARIANTS?.SUCCESS,
+    });
+    reset();
+  };
+  const formProps = { editFormMethod, submitEditForm, handleSubmit };
   const theme: any = useTheme();
   return {
     theme,
@@ -35,5 +54,8 @@ export const useDepartmentsDetail = () => {
     setOpenDelete,
     handleDeleteSubmit,
     handleDeleteClose,
+    openEdit,
+    setOpenEdit,
+    formProps,
   };
 };
