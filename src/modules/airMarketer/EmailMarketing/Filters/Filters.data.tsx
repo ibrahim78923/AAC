5a5e2d710506f9c17985e5a47fef872b32f7c0c1
zahir abbas <zahir.abbas@ceative.co.uk>
2023-12-05 15:44:48 +0500

    #feat(): worked on filters in all tab(#AAM-736)

---
+++ src/modules/airMarketer/EmailMarketing/Filters/Filters.data.tsx
@@ -0,0 +1,47 @@
+import { RHFDatePicker, RHFSelect } from '@/components/ReactHookForm';
+
+import * as Yup from 'yup';
+
+export const validationSchema = Yup.object().shape({
+  users: Yup?.string()?.required('Field is Required'),
+  createdDate: Yup?.string()?.trim()?.required('Field is Required'),
+});
+
+export const defaultValues = {
+  users: '',
+  createdDate: '',
+};
+
+export const dataArray = [
+  {
+    componentProps: {
+      name: 'users',
+      label: 'users',
+      fullWidth: true,
+      select: true,
+    },
+
+    options: [
+      { value: 'Admin', label: 'Admin' },
+      { value: 'Albert Paul', label: 'Albert Paul' },
+      { value: 'Nick James', label: 'Nick James' },
+      { value: 'Robert Fox', label: 'Robert Fox' },
+    ],
+
+    component: RHFSelect,
+
+    md: 12,
+  },
+
+  {
+    componentProps: {
+      name: 'createdDate',
+      label: 'Created Date',
+      fullWidth: true,
+    },
+
+    component: RHFDatePicker,
+
+    md: 12,
+  },
+];
