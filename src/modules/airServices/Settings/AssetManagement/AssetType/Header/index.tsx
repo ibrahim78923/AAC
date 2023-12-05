05792402e9dc16a17a914d091b8109c8a3624154
Aliraza1623 <152487095+Aliraza1623@users.noreply.github.com>
2023-12-05 19:18:28 +0500

    Aasss 496 & 575 add new asset types design (#23)
    * feat(): Working on add new asset types in asset types. (#496)
    
    * feat(): Working on add new asset and edit asset type modal. (#496)
    
    * feat(): Working on add new asset and edit asset type modal. (#496)
    
    * fix(): fixing and updating. (#496)

--- src/modules/airServices/Settings/AssetManagement/AssetType/Header/index.tsx
+++ src/modules/airServices/Settings/AssetManagement/AssetType/Header/index.tsx
@@ -1,34 +1,59 @@
 import { AIR_SERVICES } from '@/constants';
 import { ArrowBack } from '@mui/icons-material';
 import { Box, Button, Typography } from '@mui/material';
-import { useRouter } from 'next/router';
+import AddNewAssetTypesModal from '../AddNewAssetTypesModal';
+import { useHeader } from './useHeader';
 
 const Header = () => {
-  const router = useRouter();
+  const {
+    router,
+    handleSubmit,
+    addNewAssetTypesMethods,
+    submitAddForm,
+    openAddNewAssetTypesModal,
+    setOpenAddNewAssetTypesModal,
+  } = useHeader();
   return (
-    <Box
-      display={'flex'}
-      alignItems={'center'}
-      justifyContent={'space-between'}
-      flexWrap={'wrap'}
-    >
-      <Box display={'flex'} alignItems={'center'} gap={1} flexWrap={'wrap'}>
-        <ArrowBack
-          sx={{
-            cursor: 'pointer',
-          }}
-          onClick={() => {
-            router?.push({
-              pathname: AIR_SERVICES?.ASSET_MANAGEMENT_SETTINGS,
-            });
-          }}
-        />
-        <Typography variant="h3" textTransform="capitalize">
-          asset type & fields
-        </Typography>
+    <>
+      <Box
+        display={'flex'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexWrap={'wrap'}
+      >
+        <Box display={'flex'} alignItems={'center'} gap={1} flexWrap={'wrap'}>
+          <ArrowBack
+            sx={{
+              cursor: 'pointer',
+            }}
+            onClick={() => {
+              router?.push({
+                pathname: AIR_SERVICES?.ASSET_MANAGEMENT_SETTINGS,
+              });
+            }}
+          />
+          <Typography variant="h3" textTransform="capitalize">
+            asset type & fields
+          </Typography>
+        </Box>
+        <Button
+          variant="contained"
+          onClick={() => setOpenAddNewAssetTypesModal?.(true)}
+        >
+          New Asset Type
+        </Button>
       </Box>
-      <Button variant="contained">New Asset Type</Button>
-    </Box>
+      <Box>
+        <AddNewAssetTypesModal
+          open={openAddNewAssetTypesModal}
+          handleClose={setOpenAddNewAssetTypesModal}
+          methods={addNewAssetTypesMethods}
+          submitForm={submitAddForm}
+          handleSubmit={handleSubmit}
+          modalTitle="Add New Asset Type"
+        />
+      </Box>
+    </>
   );
 };
 
