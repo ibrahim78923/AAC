a7f5aa3ddce17bc83ef872db50f652c5b3d1a2b4
Abdullah Akhlaq <abdullah.akhlaq@ceative.co.uk>
2023-12-06 10:55:12 +0500

    feat(): theme color added in marketing sales   (#729)

--- src/modules/airMarketer/SocialMarketing/SocialInbox/SocialInboxSettings/index.tsx
+++ src/modules/airMarketer/SocialMarketing/SocialInbox/SocialInboxSettings/index.tsx
@@ -11,29 +11,31 @@ import SkeletonForm from '@/components/Skeletons/SkeletonForm';
 
 import { v4 as uuidv4 } from 'uuid';
 
-function TabPanel(props: SalesSettingProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Box
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-      sx={styles?.tabData}
-    >
-      {value === index && (
-        <Box sx={{ p: { xs: 1, md: 3 } }}>
-          <>{children}</>
-        </Box>
-      )}
-    </Box>
-  );
-}
-
 const SocialInboxSettings = () => {
-  const { tabsOrientation, tabValue, handleChange } = useSocialSettings();
+  const { tabsOrientation, tabValue, handleChange, theme } =
+    useSocialSettings();
+
+  function TabPanel(props: SalesSettingProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <Box
+        role="tabpanel"
+        hidden={value !== index}
+        id={`vertical-tabpanel-${index}`}
+        aria-labelledby={`vertical-tab-${index}`}
+        {...other}
+        sx={styles?.tabData(theme)}
+      >
+        {value === index && (
+          <Box sx={{ p: { xs: 1, md: 3 } }}>
+            <>{children}</>
+          </Box>
+        )}
+      </Box>
+    );
+  }
+
   return (
     <Box>
       <Box>
@@ -49,14 +51,14 @@ const SocialInboxSettings = () => {
             value={Number(tabValue)}
             onChange={handleChange}
             aria-label="Vertical tabs example"
-            sx={styles?.tabsStyle}
+            sx={styles?.tabsStyle(theme)}
           >
             {tabLabels?.map((label) => <Tab key={uuidv4()} label={label} />)}
           </Tabs>
 
           {tabComponents?.map((Component, index) => (
             <TabPanel key={uuidv4()} value={Number(tabValue)} index={index}>
-              <Box sx={styles?.tabsPanel}>
+              <Box sx={styles?.tabsPanel(theme)}>
                 {!tabValue ? <SkeletonForm /> : <Component />}
               </Box>
             </TabPanel>
