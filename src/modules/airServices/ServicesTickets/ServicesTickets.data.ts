2b51962cf1108f9b6e3dfb6f4b08572108d8f07f
unknown <unknown@unknown.unknown>
2023-12-07 15:43:37 +0500

    fix: issues fixing  (#000)

--- src/modules/airServices/ServicesTickets/ServicesTickets.data.ts
+++ src/modules/airServices/ServicesTickets/ServicesTickets.data.ts
@@ -6,111 +6,45 @@ import {
 } from '@/constants/strings';
 
 export const ticketStatusOptions = [
-  {
-    value: TICKET_STATUS?.OPEN,
-    label: 'Open',
-  },
-  {
-    value: TICKET_STATUS?.PENDING,
-    label: 'Pending',
-  },
-  {
-    value: TICKET_STATUS?.RESOLVED,
-    label: 'Resolved',
-  },
-  {
-    value: TICKET_STATUS?.CLOSED,
-    label: 'Closed',
-  },
+  TICKET_STATUS?.OPEN,
+  TICKET_STATUS?.PENDING,
+  TICKET_STATUS?.RESOLVED,
+  TICKET_STATUS?.CLOSED,
 ];
 
 export const ticketPriorityOptions = [
-  {
-    value: TICKET_PRIORITY?.LOW,
-    label: 'Low',
-  },
-  {
-    value: TICKET_PRIORITY?.MEDIUM,
-    label: 'Medium',
-  },
-  {
-    value: TICKET_PRIORITY?.HIGH,
-    label: 'High',
-  },
-  {
-    value: TICKET_PRIORITY?.URGENT,
-    label: 'Urgent',
-  },
+  TICKET_PRIORITY?.LOW,
+  TICKET_PRIORITY?.MEDIUM,
+  TICKET_PRIORITY?.HIGH,
+  TICKET_PRIORITY?.URGENT,
 ];
 
 export const ticketImpactOptions = [
-  {
-    value: TICKET_IMPACT?.LOW,
-    label: 'Low',
-  },
-  {
-    value: TICKET_IMPACT?.MEDIUM,
-    label: 'Medium',
-  },
-  {
-    value: TICKET_IMPACT?.HIGH,
-    label: 'High',
-  },
+  TICKET_IMPACT?.LOW,
+  TICKET_IMPACT?.MEDIUM,
+  TICKET_IMPACT?.HIGH,
 ];
 
 export const ticketSourceOptions = [
-  { value: 'Phone No.', label: 'Phone No.' },
-  { value: 'Email', label: 'Email' },
-  { value: 'Portal', label: 'Portal' },
-  { value: 'Chat', label: 'Chat' },
-  { value: 'Walk Up', label: 'Walk Up' },
-  { value: 'Slack', label: 'Slack' },
-  { value: 'MS Team', label: 'MS Team' },
+  'Phone No.',
+  'Email',
+  'Portal',
+  'Chat',
+  'Walk Up',
+  'Slack',
+  'MS Team',
 ];
 
 export const ticketsTypeOptions = [
-  {
-    value: TICKET_TYPES?.ALL_TICKETS,
-    label: 'All Tickets',
-  },
-  {
-    value: TICKET_TYPES?.URGENT_AND_HIGH_PRIORITY,
-    label: 'Urgent and High Priority',
-  },
-  {
-    value: TICKET_TYPES?.URGENT_AND_HIGH_PRIORITY,
-    label: 'Urgent and High Priority',
-  },
-  {
-    value: TICKET_TYPES?.MY_OVERDUE_TICKETS,
-    label: 'My Overdue Tickets',
-  },
-  {
-    value: TICKET_TYPES?.SPAM,
-    label: 'Spam',
-  },
-  {
-    value: TICKET_TYPES?.NEW_AND_MY_OPEN_TICKETS,
-    label: 'New & My Open Tickets',
-  },
-  {
-    value: TICKET_TYPES?.UNRESOLVED_TICKETS,
-    label: 'All Unresolved Tickets',
-  },
-  {
-    value: TICKET_TYPES?.INCIDENTS,
-    label: 'Incidents',
-  },
-  {
-    value: TICKET_TYPES?.SERVICE_REQUEST,
-    label: 'Service Requests',
-  },
-  {
-    value: TICKET_TYPES?.TICKETS_I_REQUESTED,
-    label: 'Tickets I Requested',
-  },
-  {
-    value: TICKET_TYPES?.SHARED_WITH_ME,
-    label: 'Shared with me',
-  },
+  TICKET_TYPES?.ALL_TICKETS,
+  TICKET_TYPES?.URGENT_AND_HIGH_PRIORITY,
+  TICKET_TYPES?.MY_OPEN_AND_PENDING_TICKETS,
+  TICKET_TYPES?.MY_OVERDUE_TICKETS,
+  TICKET_TYPES?.SPAM,
+  TICKET_TYPES?.NEW_AND_MY_OPEN_TICKETS,
+  TICKET_TYPES?.UNRESOLVED_TICKETS,
+  TICKET_TYPES?.INCIDENTS,
+  TICKET_TYPES?.SERVICE_REQUEST,
+  TICKET_TYPES?.TICKETS_I_REQUESTED,
+  TICKET_TYPES?.SHARED_WITH_ME,
 ];
