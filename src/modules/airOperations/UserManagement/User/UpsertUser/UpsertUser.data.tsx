f9497774760f81977ebbcd7ce325ae225fa92591
zainjahan1 <152486716+zainjahan1@users.noreply.github.com>
2023-12-07 15:04:27 +0500

    Aao 209 user management user add user (#43)
    * feat(hat): Working on user management of user of add user component of operation module(#209)
    
    * feat(hat): Working on user management of user of add user component of operation module(#209)
    
    * feat(hat): Working on user management user add user(#209)

---
+++ src/modules/airOperations/UserManagement/User/UpsertUser/UpsertUser.data.tsx
@@ -0,0 +1,196 @@
+import { RHFAutocomplete, RHFTextField } from '@/components/ReactHookForm';
+import * as Yup from 'yup';
+
+const jobTitleOptions = [
+  'Senior HR Executive',
+  'Software Engineer',
+  'Software Developer',
+];
+
+export const upsertUserValidationSchema: any = Yup?.object()?.shape({
+  firstName: Yup?.string()?.required('Required'),
+  middleName: Yup?.string(),
+  lastName: Yup?.string()?.required('Required'),
+  address: Yup?.string()?.required('Required'),
+  email: Yup?.string()?.required('Required'),
+  phoneNumber: Yup?.string(),
+  jobTitle: Yup?.string(),
+  assignRole: Yup?.string()?.required('Required'),
+  selectTeam: Yup?.string()?.required('Required'),
+  language: Yup?.string(),
+  facebookUrl: Yup?.string(),
+  linkedinUrl: Yup?.string(),
+  twitterUrl: Yup?.string(),
+});
+
+export const upsertUserDefaultValues: any = {
+  firstName: '',
+  middleName: '',
+  lastName: '',
+  address: '',
+  email: '',
+  phoneNumber: '',
+  jobTitle: '',
+  assignRole: '',
+  selectTeam: '',
+  language: '',
+  facebookUrl: '',
+  linkedinUrl: '',
+  twitterUrl: '',
+};
+
+export const upsertUserArray = [
+  {
+    id: 1,
+    subheading: 'Add a new user to this organization.',
+    componentProps: {
+      name: 'firstName',
+      label: 'First name',
+      placeholder: 'Enter first name',
+      fullWidth: true,
+      required: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 2,
+    componentProps: {
+      name: 'middleName',
+      label: 'Middle name',
+      placeholder: 'Enter middle name',
+      fullWidth: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 3,
+    componentProps: {
+      name: 'lastName',
+      label: 'Last name',
+      placeholder: 'Enter last name',
+      fullWidth: true,
+      required: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 4,
+    componentProps: {
+      name: 'address',
+      label: 'Address',
+      placeholder: 'Enter business address',
+      fullWidth: true,
+      required: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 5,
+    componentProps: {
+      name: 'email',
+      label: 'Email',
+      placeholder: 'Enter business email',
+      fullWidth: true,
+      required: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 6,
+    componentProps: {
+      name: 'phoneNumber',
+      label: 'Phone number',
+      placeholder: 'Enter phone number',
+      fullWidth: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 6,
+    componentProps: {
+      name: 'jobTitle',
+      label: 'Job title',
+      placeholder: 'UI UX DESIGNER',
+      fullWidth: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 7,
+    componentProps: {
+      name: 'assignRole',
+      label: 'Assign role',
+      placeholder: 'Select role',
+      fullWidth: true,
+      required: true,
+      options: jobTitleOptions,
+    },
+    component: RHFAutocomplete,
+    md: 12,
+  },
+  {
+    id: 7,
+    componentProps: {
+      name: 'selectTeam',
+      label: 'Select team',
+      placeholder: 'Select',
+      fullWidth: true,
+      required: true,
+      options: jobTitleOptions,
+    },
+    component: RHFAutocomplete,
+    md: 12,
+  },
+  {
+    id: 7,
+    componentProps: {
+      name: 'language',
+      label: 'Language',
+      placeholder: 'English',
+      fullWidth: true,
+      options: jobTitleOptions,
+    },
+    component: RHFAutocomplete,
+    md: 12,
+  },
+  {
+    id: 8,
+    componentProps: {
+      name: 'facebookUrl',
+      label: 'Facebook url',
+      placeholder: 'paste URL',
+      fullWidth: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 9,
+    componentProps: {
+      name: 'linkedinUrl',
+      label: 'Linkedin url',
+      placeholder: 'paste URL',
+      fullWidth: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+  {
+    id: 10,
+    componentProps: {
+      name: 'twitterUrl',
+      label: 'Twitter url',
+      placeholder: 'paste URL',
+      fullWidth: true,
+    },
+    component: RHFTextField,
+    md: 12,
+  },
+];
