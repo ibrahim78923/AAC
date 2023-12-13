2c1ca220bdb48041778a4e8d2ea813ecc31d9c83
zainjahan1 <152486716+zainjahan1@users.noreply.github.com>
2023-12-13 19:37:00 +0500

    Aao 212 user management team grid view (#69)
    * feat(hat): Working on user management users details and user management team grid view(#212)
    
    * feat(hat): Working on user management users details and user management team grid view(#212)
    
    * feat(hat): Working on user management users details and user management team grid view(#212)
    
    * feat(hat): Working on user management team grid view(#212)
    
    * feat(hat): Working on user management team grid view(#212)
    
    * feat(hat): Working on user management team grid view(#212)
    
    * feat(hat): Working on user management team grid view(#212)
    
    * feat(hat): Working on user management team grid view(#212)
    
    * feat(hat): Working on user management team grid view(#212)

--- src/modules/airOperations/UserManagement/Teams/TeamsHeader/index.tsx
+++ src/modules/airOperations/UserManagement/Teams/TeamsHeader/index.tsx
@@ -2,11 +2,10 @@ import { CirclePlusIcon } from '@/assets/icons';
 import Search from '@/components/Search';
 import { SingleDropdownButton } from '@/components/SingleDropdownButton';
 import { Box, Button } from '@mui/material';
-import { useUser } from '../useUser';
-import UpsertUser from '../UpsertUser';
+import { useTeams } from '../useTeams';
 
-export const UserHeader = ({ selectedUserList }: any) => {
-  const { setSearchValue, isDrawerOpen, setIsDrawerOpen } = useUser();
+export const TeamsHeader = ({ selectedTeamList }: any) => {
+  const { setSearchValue } = useTeams();
   return (
     <Box
       display={'flex'}
@@ -24,21 +23,11 @@ export const UserHeader = ({ selectedUserList }: any) => {
       <Box display={'flex'} gap={1} mt={{ xs: 2, sm: 0 }}>
         <SingleDropdownButton
           dropdownName={'Actions'}
-          disabled={!selectedUserList?.length}
+          disabled={!selectedTeamList?.length}
         />
-        <Button
-          startIcon={<CirclePlusIcon />}
-          variant="contained"
-          onClick={() => setIsDrawerOpen(true)}
-        >
-          Add User
+        <Button startIcon={<CirclePlusIcon />} variant="contained">
+          Create Team
         </Button>
-        <UpsertUser
-          isDrawerOpen={isDrawerOpen}
-          setIsDrawerOpen={setIsDrawerOpen}
-          title={'Add User'}
-          okText={'Add'}
-        />
       </Box>
     </Box>
   );
