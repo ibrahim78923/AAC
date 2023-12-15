5cbceafcec661ebb41d7c4209aff852e88803ddc
Aliraza1623 <152487095+Aliraza1623@users.noreply.github.com>
2023-12-15 22:23:47 +0500

    Fixing (#131)
    * fix(): Delete services upsert workflow. (#00)
    
    * fix(): Fixing and updating. (#00)

--- src/modules/airOperations/WorkflowAutomation/ServicesWorkflow/EventBaseWorkflows/UpsertEventBasedWorkflow/WorkflowConditions/useWorkflowConditions.tsx
+++ src/modules/airOperations/WorkflowAutomation/ServicesWorkflow/EventBaseWorkflows/UpsertEventBasedWorkflow/WorkflowConditions/useWorkflowConditions.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from '@mui/material';
 import { useFieldArray } from 'react-hook-form';
-import { salesValues } from '../UpsertEventBasedWorkflow.data';
+import { eventBasedWorkflowValues } from '../UpsertEventBasedWorkflow.data';
 
 export const useWorkflowConditions = (props: any) => {
   const { control } = props;
@@ -8,7 +8,8 @@ export const useWorkflowConditions = (props: any) => {
     control,
     name: 'workflowConditions',
   });
-  const handleAddGroup = () => append(salesValues?.workflowConditions[0]);
+  const handleAddGroup = () =>
+    append(eventBasedWorkflowValues?.workflowConditions[0]);
   const { palette } = useTheme();
   return { fields, append, remove, palette, handleAddGroup };
 };
