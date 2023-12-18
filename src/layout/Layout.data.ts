68b360b6f437ae43e6ec618c310148d53263dfc1
zahir abbas <zahir.abbas@ceative.co.uk>
2023-12-18 10:08:15 +0500

    feat(): change air marketer into air marketing (#AAM-719)

--- src/layout/Layout.data.ts
+++ src/layout/Layout.data.ts
@@ -207,28 +207,28 @@ export const AirMarketingRoutes: any = [
     key: AIR_MARKETER?.root,
     icon: DashboardImage,
     label: 'Dashboard',
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     permissions: [],
   },
   {
     key: AIR_MARKETER?.PAID_ADS,
     icon: PaidAdsImage,
     label: 'Paid Ads',
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     permissions: [],
   },
   {
     key: AIR_MARKETER?.CAMPAIGNS,
     icon: CampaignImage,
     label: 'Campaign',
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     permissions: [],
   },
   {
     key: AIR_MARKETER?.EMAIL_MARKETING,
     label: 'Email Marketing',
     icon: EmailMarketingImage,
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     permissions: [],
     textNames: [
       {
@@ -253,7 +253,7 @@ export const AirMarketingRoutes: any = [
     key: AIR_MARKETER?.SOCIAL_MARKETING,
     label: 'Social Marketing',
     icon: SocialMarketingImage,
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     permissions: [],
     textNames: [
       {
@@ -268,14 +268,14 @@ export const AirMarketingRoutes: any = [
     key: AIR_MARKETER?.SMS_MARKETING,
     icon: SmsMarketingImage,
     label: 'SMS Marketing',
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     permissions: [],
   },
   {
     key: AIR_MARKETER?.LEAD_CAPTURE,
     label: 'Lead Capture',
     icon: LeadCaptureImage,
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     permissions: [],
     textNames: [
       {
@@ -294,7 +294,7 @@ export const AirMarketingRoutes: any = [
     key: AIR_MARKETER?.REPORTS,
     icon: ReportsImage,
     label: 'Reports',
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     permissions: [],
   },
 ];
@@ -565,7 +565,7 @@ export const LowerAirMarketingRoutes = [
   {
     label: 'Settings',
     icon: SettingImage,
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     key: AIR_MARKETER?.SETTINGS,
     permissions: [],
   },
@@ -573,7 +573,7 @@ export const LowerAirMarketingRoutes = [
   {
     label: 'Logout',
     key: '',
-    role: 'AIR_MARKETER',
+    role: 'AIR_MARKETING',
     permissions: [],
     icon: LogoutImage,
   },
