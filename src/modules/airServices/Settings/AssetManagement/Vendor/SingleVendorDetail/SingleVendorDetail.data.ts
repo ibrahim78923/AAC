04cd14ed6f719c4cdfe66617ffb4945912c6446d
tayyab khan satti <tayyabkhan.satti@ceative.co.uk>
2024-01-01 14:54:54 +0500

    feat(hat):  i have worked on api correction of vendor edit section. (#0000)

--- src/modules/airServices/Settings/AssetManagement/Vendor/SingleVendorDetail/SingleVendorDetail.data.ts
+++ src/modules/airServices/Settings/AssetManagement/Vendor/SingleVendorDetail/SingleVendorDetail.data.ts
@@ -6,13 +6,6 @@ export const singleVendorDetailsActionDropdownFunction = (
   {
     title: 'Edit',
     handleClick: (closeMenu: any) => {
-      router?.push({
-        pathname: '',
-        query: {
-          ...router?.query,
-          action: 'edit',
-        },
-      });
       setIsADrawerOpen?.(true);
       closeMenu?.();
     },
