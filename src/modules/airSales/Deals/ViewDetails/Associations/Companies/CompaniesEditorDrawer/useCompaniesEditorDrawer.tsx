873238f7a94e81bb778ba3fa3141750448c8ca67
Abdullah Akhlaq <abdullah.akhlaq@ceative.co.uk>
2024-01-10 14:28:48 +0500

    feat(): Air Sales product and companies integration (#800)

--- src/modules/airSales/Deals/ViewDetails/Associations/Companies/CompaniesEditorDrawer/useCompaniesEditorDrawer.tsx
+++ src/modules/airSales/Deals/ViewDetails/Associations/Companies/CompaniesEditorDrawer/useCompaniesEditorDrawer.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -6,24 +5,74 @@ import {
   companiesDefaultValues,
   companiesValidationSchema,
 } from './CompaniesEditorDrawer.data';
+import { useGetCompanyContactsQuery } from '@/services/common-APIs';
+import { getSession } from '@/utils';
+import { usePostCompaniesMutation } from '@/services/commonFeatures/companies';
+import { enqueueSnackbar } from 'notistack';
+import { NOTISTACK_VARIANTS } from '@/constants/strings';
+import { useCreateAssociationMutation } from '@/services/airSales/deals/view-details/association';
 
-const useCompaniesEditorDrawer = () => {
-  const [searchProduct, setSearchProduct] = useState('');
+const useCompaniesEditorDrawer = (openDrawer: any) => {
+  const { user } = getSession();
+  const params = {
+    page: 1,
+    limit: 10,
+    contactOwnerId: user?._id,
+  };
+
+  const { data: getCompanyContacts } = useGetCompanyContactsQuery(params);
+
+  const [postCompanies] = usePostCompaniesMutation();
+  const [createAssociation] = useCreateAssociationMutation();
+
   const methodsCompanies = useForm({
     resolver: yupResolver(companiesValidationSchema),
     defaultValues: companiesDefaultValues,
   });
+  const { handleSubmit, reset } = methodsCompanies;
 
-  const onSubmit = () => {};
-  const { handleSubmit, watch } = methodsCompanies;
-  const watchProducts = watch(['companyStatus']);
+  const onSubmit = async (values: any) => {
+    try {
+      const response = await postCompanies({ body: values })?.unwrap();
+      if (response?.data) {
+        try {
+          await createAssociation({
+            body: {
+              //TODO:temporary id data come from backend
+              dealId: '655b2b2ecd318b576d7d71e8',
+              companyId: response?.data?._id,
+            },
+          }).unwrap();
+          enqueueSnackbar(
+            ` Companies ${
+              openDrawer === 'Edit' ? 'Updated' : 'Added'
+            } Successfully`,
+            {
+              variant: NOTISTACK_VARIANTS?.SUCCESS,
+            },
+          );
+        } catch (error: any) {
+          const errMsg = error?.data?.message;
+          enqueueSnackbar(errMsg ?? 'Error occurred', {
+            variant: NOTISTACK_VARIANTS?.ERROR,
+          });
+        }
+      }
+      // setIsOpenDrawer(false);
+      reset();
+    } catch (error) {
+      enqueueSnackbar(`Something went wrong`, {
+        variant: NOTISTACK_VARIANTS?.ERROR,
+      });
+    }
+  };
+
+  // const watchProducts = watch(['companyStatus']);
   return {
     handleSubmit,
     onSubmit,
     methodsCompanies,
-    searchProduct,
-    setSearchProduct,
-    watchProducts,
+    getCompanyContacts,
   };
 };
 
