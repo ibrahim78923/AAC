bfca6fbe614a6c63032425b47514b6524c5fbded
Muhammad Shoaib Manzoor <152604447+shoaib-manzoor@users.noreply.github.com>
2024-01-11 19:33:51 +0500

    Ac 170 manage shop UI development (#319)
    * feat():  air-loyalty-program/settings/management ui development (#000)
    
    * feat(): /air-loyalty-program/settings/management ui development (#000)
    
    * feat(): management page deleted (#170)

--- src/modules/airLoyaltyProgram/Settings/management/ShopsTab/index.tsx
+++ src/modules/airLoyaltyProgram/Settings/management/ShopsTab/index.tsx
@@ -1,5 +1,67 @@
+import { Grid } from '@mui/material';
+import CheckboxCard from './CheckboxCard';
+import { Header } from './Header';
+import { cardData } from './ShopsTab.data';
+import { useShopsTab } from './useShopsTab';
+import ShopDetailsModal from './ShopDetailsModal';
+import { DeleteShop } from './DeleteShop';
+
 const ShopsTab = () => {
-  return <div>ShopsTab</div>;
+  const {
+    search,
+    setSearch,
+    handleSelect,
+    selectedCardList,
+    handleSelectAll,
+    openDetailDrawer,
+    setOpenDetailDrawer,
+    deleteModalOpen,
+    setDeleteModalOpen,
+    addShopModalOpen,
+    setAddShopModalOpen,
+  } = useShopsTab();
+  return (
+    <>
+      <Header
+        handleSelectAll={() => handleSelectAll(cardData)}
+        search={search}
+        setSearch={setSearch}
+        selectedCardList={selectedCardList}
+        deleteModalOpen={deleteModalOpen}
+        setDeleteModalOpen={setDeleteModalOpen}
+        addShopModalOpen={addShopModalOpen}
+        setAddShopModalOpen={setAddShopModalOpen}
+      />
+      <Grid container spacing={2.4}>
+        {cardData?.map?.((card) => (
+          <Grid item xs={12} md={6} lg={4} key={card?._id}>
+            <CheckboxCard
+              {...card}
+              selectedCardList={selectedCardList}
+              handleSelect={handleSelect}
+              handleOpenDetailModal={() =>
+                setOpenDetailDrawer({ isOpen: true, _id: card?._id })
+              }
+            />
+            {openDetailDrawer?._id === card?._id && (
+              <ShopDetailsModal
+                data={card}
+                isDetailDrawerOpen={openDetailDrawer?.isOpen}
+                setIsDetailDrawerOpen={setOpenDetailDrawer}
+                setAddShopModalOpen={setAddShopModalOpen}
+                setDeleteModalOpen={setDeleteModalOpen}
+              />
+            )}
+          </Grid>
+        ))}
+        <DeleteShop
+          deleteModalOpen={deleteModalOpen}
+          setDeleteModalOpen={setDeleteModalOpen}
+          selectedCardList={selectedCardList}
+        />
+      </Grid>
+    </>
+  );
 };
 
 export default ShopsTab;
