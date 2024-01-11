769fe7fc63148944a2d9e1d5cedfc3613192e197
Azeem Aslam <azeem.aslam@ceative.co.uk>
2024-01-11 12:43:44 +0500

    feat() : implement the design of receiver bank accounts page (#AAM-5)

--- src/modules/orgAdmin/Settings/BankAccounts/ActionDropDown/index.tsx
+++ src/modules/orgAdmin/Settings/BankAccounts/ActionDropDown/index.tsx
@@ -1,9 +1,17 @@
 import { Box, Button, Menu, MenuItem } from '@mui/material';
 import { ArrowDropDown } from '@mui/icons-material';
 import useBankAccounts from '../useBankAccounts';
+import { AlertModals } from '@/components/AlertModals';
 
-const ActionDropDown = () => {
-  const { selectedValue, handleClick, handleClose } = useBankAccounts();
+const ActionDropDown = (props: any) => {
+  const { setIsOpenAddAccountDrawer } = props;
+  const {
+    selectedValue,
+    handleClick,
+    handleClose,
+    isDeleteModal,
+    setIsDeleteModal,
+  } = useBankAccounts();
 
   return (
     <Box sx={{ width: { xs: '100%', sm: 'auto' } }}>
@@ -28,9 +36,34 @@ const ActionDropDown = () => {
           },
         }}
       >
-        <MenuItem onClick={() => handleClose()}>Edit</MenuItem>
-        <MenuItem onClick={() => handleClose()}>Delete</MenuItem>
+        <MenuItem
+          onClick={() => {
+            handleClose();
+            setIsOpenAddAccountDrawer(true);
+          }}
+        >
+          Edit
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            handleClose();
+            setIsDeleteModal(true);
+          }}
+        >
+          Delete
+        </MenuItem>
       </Menu>
+      {isDeleteModal && (
+        <AlertModals
+          type="delete"
+          message="Are you sure you want to Delete this?"
+          cancelBtnText="Cancel"
+          submitBtnText="Delete"
+          open={isDeleteModal}
+          handleClose={() => setIsDeleteModal(false)}
+          handleSubmit={() => setIsDeleteModal(false)}
+        />
+      )}
     </Box>
   );
 };
