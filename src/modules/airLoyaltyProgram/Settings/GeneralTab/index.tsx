f20252f05bd1f7695bfe96525e2616f64cb80ea4
UmerArshad112 <152488739+UmerArshad112@users.noreply.github.com>
2024-01-12 21:54:20 +0500

    Loyality setting general lpd (#344)
    * feat(hat): Working on setting general beta feature design (#237)
    
    * feat(hat): Working on setting general beta feature design (#237)
    
    * feat(hat): Working on loyality setting general design (#0)
    
    * fix: issues resolved. (#237)

--- src/modules/airLoyaltyProgram/Settings/GeneralTab/index.tsx
+++ src/modules/airLoyaltyProgram/Settings/GeneralTab/index.tsx
@@ -1,3 +1,24 @@
+import { Box, Grid, Typography } from '@mui/material';
+import { generalTabData } from './GeneralTab.data';
+import { FormProvider } from '@/components/ReactHookForm';
+import { useForm } from 'react-hook-form';
+
 export const Generaltab = () => {
-  return <>General</>;
+  const method = useForm();
+  return (
+    <Box mt={2}>
+      <Typography variant="h4" fontWeight={500} bgcolor={'grey.200'}>
+        General
+      </Typography>
+      <FormProvider methods={method}>
+        <Grid container display={'block'}>
+          {generalTabData?.map((item) => (
+            <Grid item md={item?.md} key={item?.id} mt={1}>
+              <item.component {...item?.componentProps} size={'small'} />
+            </Grid>
+          ))}
+        </Grid>
+      </FormProvider>
+    </Box>
+  );
 };
