0adad4c998d0aed81f4918db5654962756272f1b
Tayyabkhansatt129 <152484260+Tayyabkhansatt129@users.noreply.github.com>
2024-01-12 22:20:27 +0500

    Dashboard loyalty design (#322)
    * feat(hat): i worked on add new vendor integration section. (#new-tayyab)
    
    * feat(hat): i have worked on import export and edit new vendor integration. (#new-tayyab)
    
    * feat(hat): i have worked on vendor action section integration. (#new-tayyab)
    
    * feat(hat): worked on service-integration. (#new-tayyab)
    
    * feat(hat): i have worked on service. (#new-tayyab)
    
    * feat(hat): i have worked on add new vendor. (#0000)
    
    * feat(hat):  i have worked on api correction of vendor edit section. (#0000)
    
    * feat(hat): i have worked on issues in this branch on vendor section. (#0000)
    
    * feat(hat): i have worked on fixes of service catalog section. (#0000)
    
    * feat(hat): worked on service-catalog fixes. (#0000)
    
    * feat(hat): worked on service-catalog fixes. (#0000)
    
    * feat(hat): worked on dashborad of airloylaty program. (#00000)
    
    * feat(hat): worked on dashboard. (#0000)
    
    * feat(hat): i have worked on dashboard of air loyalty program and top user card. (#0000
    
    * feat(hat): worked on air loyalty program. (#0000)

--- src/modules/airLoyaltyProgram/Dashboard/index.tsx
+++ src/modules/airLoyaltyProgram/Dashboard/index.tsx
@@ -8,6 +8,8 @@ import {
 import { CustomChart } from '@/components/Chart';
 import { CardsWrapper } from './CardsWrapper';
 import { Widget } from './Widget';
+import Header from './Header';
+import DashboardTopUser from './DashboardTopUser';
 
 export const Dashboard = () => {
   const theme = useTheme();
@@ -15,8 +17,13 @@ export const Dashboard = () => {
     <>
       <Grid container spacing={3}>
         <Grid item xs={12}>
+          <Header />
+        </Grid>
+
+        <Grid item xs={12}>
           <Widget />
         </Grid>
+
         <Grid item xs={12}>
           <CardsWrapper title="LOYALTY ANALYTICS">
             <CustomChart
@@ -27,7 +34,11 @@ export const Dashboard = () => {
             />
           </CardsWrapper>
         </Grid>
-        <Grid item xs={12}></Grid>
+
+        <Grid item xs={12}>
+          <DashboardTopUser />
+        </Grid>
+
         {dashboardCardsData?.map((card) => (
           <Grid item xl={4} md={6} xs={12} key={card?.id}>
             <DashboardTableCard
