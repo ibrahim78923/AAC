9033ee29884f0ef69088a06524e7342188db0235
auns-mehmood <152486643+auns-mehmood@users.noreply.github.com>
2024-01-12 20:54:39 +0500

    Lpd 243 setting loyalty design (#339)
    * feat(hat): Working on setting loyalty design (#243)
    
    * feat(hat): Working on setting loyalty design (#243)
    
    * feat(hat): Setting loyalty Design (#243)

--- src/modules/airLoyaltyProgram/Settings/useSettings.tsx
+++ src/modules/airLoyaltyProgram/Settings/useSettings.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { SETTINGS_MODULES, settingsDataDynamic } from './Settings.data';
 import Management from './management';
+import Loyalty from './Modules/Loyalty';
 
 export const useSettings = () => {
   const [activeModule, setActiveModule] = useState(SETTINGS_MODULES?.GENERAL);
@@ -9,7 +10,7 @@ export const useSettings = () => {
     [SETTINGS_MODULES?.GENERAL]: 'g',
     [SETTINGS_MODULES?.SHOP_AND_PROFILES]: <Management />,
     [SETTINGS_MODULES?.GIFTCARD]: 'gift',
-    [SETTINGS_MODULES?.LOYALTY]: 'loy',
+    [SETTINGS_MODULES?.LOYALTY]: <Loyalty />,
   };
 
   return {
