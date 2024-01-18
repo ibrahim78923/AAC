424c656ff25071175e4a6d1043699d35cb4e44aa
Ahmer <muhammadahmer.akram@ceative.co.uk>
2024-01-18 21:35:43 +0500

    feat: email notification design. (#0000)

--- src/components/AntSwitch/index.tsx
+++ src/components/AntSwitch/index.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react';
 import { Switch, styled } from '@mui/material';
 import { styles } from './AntSwitch.style';
 
-export const AntSwitch = styled((props: any) => {
-  const [switchVal, setSwitchVal] = useState<boolean>(!!props?.values);
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSwitchVal(e?.target?.checked);
-  };
-  return <Switch {...props} checked={switchVal} onChange={handleChange} />;
+export const AntSwitch = styled(({ ...other }: any) => {
+  return <Switch {...other} />;
 })(({ theme }) => styles(theme));
--- src/modules/airServices/Settings/AccountSettings/EmailNotification/Tasks/index.tsx
+++ src/modules/airServices/Settings/AccountSettings/EmailNotification/Tasks/index.tsx
@@ -1,54 +1,62 @@
 import { Box, Divider, Typography } from '@mui/material';
-import { AntSwitch } from '../SwitchButton.style';
-import { EditYellowBGPenIcon } from '@/assets/icons';
-import { useTask } from './useTask';
+import { Fragment, useState } from 'react';
+import { taskData } from './Task.data';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import { AntSwitch } from '@/components/AntSwitch';
 
 export const Tasks = () => {
-  const { tasks, setShowIcon, showIcon, onSwitchChange } = useTask();
+  const [showIcon, setShowIcon] = useState<any>(null);
+
+  const onSwitchChange = (id: any) => {
+    alert(id);
+  };
 
   return (
-    <Box>
-      <Box>
-        <Typography variant="h4">Requester Notification</Typography>
-      </Box>
-      <Box mt={2}>
-        <Divider />
-      </Box>
+    <>
+      {taskData?.map((head: any) => (
+        <Fragment key={head?._id}>
+          <Typography variant={'h5'} color={'blue.main'}>
+            {head?.heading}
+          </Typography>
 
-      {tasks?.map((item) => {
-        return (
-          <Box
-            key={item?.id}
-            p={2}
-            my={1}
-            display={'flex'}
-            height={{ xs: 'unset', md: 50 }}
-            bgcolor={'grey.300'}
-            borderRadius={2}
-            justifyContent={'space-between'}
-            onMouseEnter={() => setShowIcon(item)}
-            onMouseLeave={() => setShowIcon(null)}
-            sx={{ cursor: 'pointer' }}
-          >
-            {item?.name}
-            <Box display={'flex'} alignItems={'center'} gap={1}>
-              <Box>
-                {showIcon === item ? (
-                  <Box>
-                    <EditYellowBGPenIcon />
-                  </Box>
-                ) : null}
+          <Divider sx={{ my: 2, borderColor: 'custom.dark' }} />
+
+          {head?.details?.map((item: any) => (
+            <Box
+              key={item?._id}
+              p={2}
+              my={1}
+              borderRadius={2}
+              bgcolor={'custom.white_fifty'}
+              display={'flex'}
+              justifyContent={'space-between'}
+              onMouseEnter={() => setShowIcon(item)}
+              onMouseLeave={() => setShowIcon(null)}
+              sx={{ cursor: 'pointer' }}
+            >
+              {item.value}
+              <Typography
+                color={'custom.dim_blue'}
+                variant={'body1'}
+                fontWeight={500}
+              >
+                {item?.title}
+              </Typography>
+
+              <Box display={'flex'} alignItems={'center'} gap={1}>
+                {showIcon === item && (
+                  <BorderColorIcon sx={{ color: 'custom.dim_blue' }} />
+                )}
+
+                <AntSwitch
+                  onChange={() => onSwitchChange(item?._id)}
+                  checked={item?.value}
+                />
               </Box>
-              <AntSwitch
-                onChange={() => {
-                  onSwitchChange(item?.id);
-                }}
-                checked={item?.value}
-              />
             </Box>
-          </Box>
-        );
-      })}
-    </Box>
+          ))}
+        </Fragment>
+      ))}
+    </>
   );
 };
