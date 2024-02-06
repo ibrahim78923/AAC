294f44dd038e7747aa1260f23e9e42893ca52d21
shayan ul haq <Shayan.ulhaq@ceative.co.uk>
2024-02-06 10:20:10 +0500

    feat() : creating new branch for bugs fixing  (#AAM-10)

--- src/modules/airSales/Tasks/TaskHeader/index.tsx
+++ src/modules/airSales/Tasks/TaskHeader/index.tsx
@@ -4,43 +4,8 @@ import Import from '../Import';
 import ActivityAndPerformance from '../ActivityAndPerformance';
 import CreateTask from '../CreateTask';
 const TaskHeader = () => {
-  // const { header, title, action } = styles(useTheme());
   const theme = useTheme();
   return (
-    // <Box
-    //   sx={{
-    //     display: 'flex',
-    //     alignItems: 'center',
-    //     justifyContent: 'space-between',
-    //     padding: '16px 24px',
-    //     flexWrap: 'wrap',
-    //     gap: '15px',
-    //   }}
-    // >
-    //   <Typography
-    //     sx={{
-    //       flex: 1,
-    //       fontSize: '24px',
-    //       fontWeight: 600,
-    //       color: theme?.palette?.grey[800],
-    //     }}
-    //   >
-    //     Tasks
-    //   </Typography>
-    //   <Box
-    //     sx={{
-    //       display: 'flex',
-    //       alignItems: 'center',
-    //       flexWrap: 'wrap',
-    //       gap: '8px',
-    //     }}
-    //   >
-    //     <Import />
-    //     <ActivityAndPerformance />
-    //     <CreateTask />
-    //   </Box>
-    // </Box>
-
     <Box
       sx={{
         display: 'flex',
