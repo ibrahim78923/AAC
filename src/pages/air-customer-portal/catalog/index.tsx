7c453b28d686f26846430501178a16604e629c90
iamflash <reverse@flash.com>
2024-02-15 15:21:32 +0500

    fix: build error (#000)

--- src/pages/air-customer-portal/catalog/index.tsx
+++ src/pages/air-customer-portal/catalog/index.tsx
@@ -1,12 +1,12 @@
-// import Layout from '@/layout';
-// import Catalog from '@/modules/airCustomerPortal/Catalog';
+import Layout from '@/layout';
+import Catalog from '@/modules/airCustomerPortal/Catalog';
 
-// import React from 'react';
+import React from 'react';
 
-// const CatalogPage = () => {
-//   return <Catalog />;
-// };
-// CatalogPage.getLayout = function getLayout(page: any) {
-//   return <Layout>{page}</Layout>;
-// };
-// export default CatalogPage;
+const CatalogPage = () => {
+  return <Catalog />;
+};
+CatalogPage.getLayout = function getLayout(page: any) {
+  return <Layout>{page}</Layout>;
+};
+export default CatalogPage;
