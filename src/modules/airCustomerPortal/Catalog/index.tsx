dad0a26d32b015e7007d6eef13d3c8d1483b4565
Tayyabkhansatt129 <152484260+Tayyabkhansatt129@users.noreply.github.com>
2024-02-19 11:36:29 +0500

    Catalog integration (#417)
    * feat(hat): i worked on add new vendor integration section. (#new-tayyab)
    
    * feat(hat): i have worked on import export and edit new vendor integration. (#new-tayyab)
    
    * feat(hat): i have worked on vendor action section integration. (#new-tayyab)
    
    * feat(hat): worked on service-integration. (#new-tayyab)
    
    * feat(hat): i have worked on service. (#new-tayyab)
    
    * feat(hat): i have worked on add new vendor. (#0000)
    
    * feat(hat):  i have worked on api correction of vendor edit section. (#0000)
    
    * feat(hat): i have worked on issues in this branch on vendor section. (#0000)
    
    * feat(hat): worked on fixes for vendor  section. (#0000)
    
    * feat(hat): i have woorked to fix image. (#0000)
    
    * feat(hat): worked on add to inventory api. (#0000)
    
    * feat(hat): worked on dashboard header and trop user card section of air loyalty program. (#0000)
    
    * feat: added inventor4y (#00)
    
    * feat(hat): worked on assets inbventory. (#00000)
    
    * feat(hat): i have worked on assets inventory section of add assets inventory api has been done in this pr. (#0000)
    
    * feat(hat): worked . (#0000)
    
    * feat(hat): worked on assets add to inventory. (#0000)
    
    * feat(hat): worked on add to inventory section. (#0000)
    
    * feat(hat): i have worked on add new item in purchase order. (#00000)
    
    * feat(hat): i have worked on add to invenjtory sectionn of purchase order. (#0000)
    
    * feat(hat): worked on add item to inventory. (#0000)
    
    * feat(hat): worked on add new item deatils and add to inventory . (#0000)
    
    * feat(hat): worked on add to item. (#0000)
    
    * feat(hat): i have worked on recieved item and add to inventory section.  (#00000)
    
    * feat(hat): i have worked on assets inventory and assets purchase order. (#00000)
    
    * feat(hyat): i have worked on purchase order details add to inventory section. (#0000)
    
    * feat(heat): worked on purchase order details integration. (#0000)
    
    * feat(hat): worked on purchase order deatial integration. (#0000)
    
    * feat(hat): worked on air custromer portal catalog integratio0n. (#0000)

--- src/modules/airCustomerPortal/Catalog/index.tsx
+++ src/modules/airCustomerPortal/Catalog/index.tsx
@@ -1,70 +1,125 @@
-import React from 'react';
-import { Avatar, Box, Grid, Typography, useTheme } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import Image from 'next/image';
-import { v4 as uuidv4 } from 'uuid';
-import { services } from './Catalog.data';
 import useCatalog from './useCatalog';
+import { FolderIcon } from '@/assets/icons';
+import CustomPagination from '@/components/CustomPagination';
 
-const Catalog = () => {
-  const theme = useTheme();
-  const { handleClick, result, handleClickService } = useCatalog();
+export const Catalog = () => {
+  const {
+    handleClick,
+    result,
+    handleClickService,
+    data,
+    handlePageChange,
+    setPageLimit,
+    setPage,
+  } = useCatalog();
+
   return (
     <>
       <Typography variant="h3">All Services</Typography>
       <Grid container spacing={2}>
-        {services?.map((service) => (
-          <Grid item xs={12} md={6} lg={3} key={uuidv4()}>
-            <Box
-              onClick={() => handleClick(service?.title)}
-              borderRadius={2}
-              border={'0.2rem solid'}
-              borderColor={'primary.lighter'}
-              textAlign="center"
-              mt={2}
-              p={2}
-              sx={{ cursor: 'pointer' }}
+        <Grid
+          item
+          xs={12}
+          md={6}
+          lg={3}
+          borderRadius={2}
+          border={'0.2rem solid'}
+          borderColor={'primary.lighter'}
+          textAlign="center"
+          mt={4}
+          sx={{ cursor: 'pointer' }}
+          onClick={() => handleClick('ALL Services')}
+        >
+          <Box
+            alignItems={'center'}
+            display={'flex'}
+            justifyContent={'center'}
+            mt={2}
+          >
+            <FolderIcon />
+          </Box>
+          <Typography variant="h5" mt={2}>
+            All Services
+          </Typography>
+          <Box alignItems={'center'} display={'flex'} justifyContent={'center'}>
+            <Typography
+              variant="body2"
+              align="center"
+              gutterBottom
+              mt={1}
+              mb={2}
+              ml={2}
+              mr={2}
             >
-              <Box textAlign={'center'}>
-                <Avatar
-                  sx={{
-                    backgroundColor: theme?.palette?.primary?.light,
-                    margin: 'auto',
-                  }}
-                  alt={`Service ${service?.id} Image`}
-                >
-                  <service.image />
-                </Avatar>
-              </Box>
-              <Typography variant="h5" mt={2}>
-                {service?.title}
-              </Typography>
+              Browse the list of all services offered and raise a request.
+            </Typography>
+          </Box>
+        </Grid>
+        {!!data?.data?.servicecategories?.length &&
+          data?.data?.servicecategories?.map((service: any) => (
+            <Grid item xs={12} md={6} lg={3} key={service?._id}>
               <Box
-                alignItems={'center'}
-                display={'flex'}
-                justifyContent={'center'}
+                onClick={() => handleClick(service?._id)}
+                borderRadius={2}
+                border={'0.2rem solid'}
+                borderColor={'primary.lighter'}
+                textAlign="center"
+                mt={2}
+                p={2}
+                sx={{ cursor: 'pointer' }}
               >
-                <Typography
-                  variant="body2"
-                  align="center"
-                  gutterBottom
-                  mt={1}
-                  mb={2}
-                  ml={2}
-                  mr={2}
+                <Box
+                  alignItems={'center'}
+                  display={'flex'}
+                  justifyContent={'center'}
+                  mt={2}
                 >
-                  {service?.description}
+                  <FolderIcon />
+                </Box>
+                <Typography variant="h5" mt={2}>
+                  {service?.categoryName}
                 </Typography>
+                <Box
+                  alignItems={'center'}
+                  display={'flex'}
+                  justifyContent={'center'}
+                >
+                  <Typography
+                    variant="body2"
+                    align="center"
+                    gutterBottom
+                    mt={1}
+                    mb={2}
+                    ml={2}
+                    mr={2}
+                  >
+                    {service?.description}
+                  </Typography>
+                </Box>
               </Box>
-            </Box>
-          </Grid>
-        ))}
+            </Grid>
+          ))}
       </Grid>
+      <CustomPagination
+        count={data?.data?.servicecategories?.meta?.count}
+        pageLimit={data?.data?.servicecategories?.meta?.pageLimit}
+        rowsPerPageOptions={
+          data?.data?.servicecategories?.meta?.rowsPerPageOptions
+        }
+        currentPage={data?.data?.servicecategories?.meta?.currentPage}
+        totalRecords={data?.data?.servicecategories?.meta?.totalRecords}
+        onPageChange={handlePageChange}
+        setPage={setPage}
+        setPageLimit={setPageLimit}
+      />
       <Grid container>
         {result?.map((allService: any) => (
-          <Grid item xs={12} md={6} lg={4} key={uuidv4()}>
+          <Grid item xs={12} md={6} lg={4} key={allService?._id}>
             <Box
-              key={uuidv4()}
-              onClick={() => handleClickService?.(allService?.id)}
+              key={allService?._id}
+              onClick={() => handleClickService?.(allService?._id)}
               borderRadius={2}
               border={'0.3rem solid'}
               borderColor={'primary.lighter'}
@@ -94,13 +149,13 @@ const Catalog = () => {
                 flexDirection={'column'}
                 mt={2}
               >
-                <Typography variant="h5">{allService?.title}</Typography>
+                <Typography variant="h5">{allService?.itemName}</Typography>
 
                 <Typography variant="body2" component={'span'}>
                   {allService?.description}
                 </Typography>
                 <Typography variant="body2" component={'span'}>
-                  {allService?.price}
+                  {allService?.cost}
                 </Typography>
               </Box>
             </Box>
@@ -110,5 +165,3 @@ const Catalog = () => {
     </>
   );
 };
-
-export default Catalog;
--- src/modules/airCustomerPortal/Catalog/useCatalog.tsx
+++ src/modules/airCustomerPortal/Catalog/useCatalog.tsx
@@ -1,20 +1,56 @@
 import { useState } from 'react';
 
-import { allServices } from './Catalog.data';
 import { useRouter } from 'next/router';
 import { AIR_CUSTOMER_PORTAL } from '@/constants';
 import { CATALOG_SERVICE_TYPES } from '@/constants/strings';
+import { PAGINATION } from '@/config';
+import {
+  useGetServiceCatalogCategoriesQuery,
+  useGetServiceCatalogQuery,
+} from '@/services/airCustomerPortal/catalog';
 const useCatalog = () => {
-  const [result, setResult] = useState<any[]>(allServices);
   const [open, setOpen] = useState(false);
   const router = useRouter();
+  const [page, setPage] = useState(PAGINATION?.CURRENT_PAGE);
+  const [pageLimit, setPageLimit] = useState(PAGINATION?.PAGE_LIMIT);
+  const [search, setSearch] = useState('');
+  const handlePageChange = (page: number) => {
+    setPage(page);
+  };
+  const param = {
+    page: page,
+    limit: pageLimit,
+    search,
+  };
+  const { data } = useGetServiceCatalogCategoriesQuery({
+    param,
+  });
+
+  const { categoryId } = router?.query;
+
+  const getServiceCatalogCategoriesParameter = {
+    queryParam: {
+      categoryId,
+    },
+  };
+  const {
+    data: services,
+    isLoading,
+    isFetching,
+  } = useGetServiceCatalogQuery(getServiceCatalogCategoriesParameter, {
+    refetchOnMountOrArgChange: true,
+  });
+
+  const results = services?.data;
+  const [result, setResult] = useState<any[]>(results);
+
   const handleClick = (prop: string) => {
     let filteredServices;
     if (prop === CATALOG_SERVICE_TYPES?.ALL) {
-      filteredServices = allServices;
+      filteredServices = results;
     } else {
-      filteredServices = allServices?.filter(
-        (service) => service?.serviceId === prop,
+      filteredServices = results?.filter(
+        (service: any) => service?.serviceCategory === prop,
       );
     }
 
@@ -39,10 +75,17 @@ const useCatalog = () => {
     handleClick,
     result,
     handleClickService,
-    open,
+    data,
     handleClickOpen,
+    isLoading,
+    isFetching,
+    handlePageChange,
+    setPageLimit,
+    search,
+    setPage,
     handleClose,
-    setOpen,
+    setSearch,
+    open,
   };
 };
 
