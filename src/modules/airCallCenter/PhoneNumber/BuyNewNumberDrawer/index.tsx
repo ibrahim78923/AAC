f3cff4c5080e72d1dfb6eeca65d7b0e3a37c8c8f
Azeem Aslam <azeem.aslam@ceative.co.uk>
2024-02-20 15:50:52 +0500

    feat() : implement the design of setting > phone number (#AACSA-4)

---
+++ src/modules/airCallCenter/PhoneNumber/BuyNewNumberDrawer/index.tsx
@@ -0,0 +1,30 @@
+import CommonDrawer from '@/components/CommonDrawer';
+import CommonTabs from '@/components/Tabs';
+import LocalNumber from './LocalNumber';
+import TollFreeNumber from './TollFreeNumber';
+import useBuyNewNumber from './useBuyNewNumber';
+
+const BuyNewNumberDrawer = (props: any) => {
+  const { isBuyNewNumber, setIsBuyNewNumber } = props;
+  const { isNumberDetail } = useBuyNewNumber();
+  return (
+    <CommonDrawer
+      isDrawerOpen={isBuyNewNumber}
+      onClose={() => setIsBuyNewNumber(false)}
+      title="Buy New Number"
+      okText="Buy"
+      isOk={false}
+      // submitHandler={() => { }}
+      footer
+    >
+      {!isNumberDetail && (
+        <CommonTabs tabsArray={['Local', 'Toll-Free']}>
+          <LocalNumber />
+          <TollFreeNumber />
+        </CommonTabs>
+      )}
+    </CommonDrawer>
+  );
+};
+
+export default BuyNewNumberDrawer;
