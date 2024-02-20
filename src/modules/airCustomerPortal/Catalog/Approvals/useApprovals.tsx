633ecf6e8f1fbfef147de2290bb41c902354fd09
nouman130 <134398361+nouman130@users.noreply.github.com>
2024-02-20 16:48:24 +0500

    Article integration module (#425)
    * feat: knowledge base articles integration (#93)
    
    * feat(): article integration (#93)
    
    * feat(): Working on article in knowledge base. (#93)
    
    * fix(): fix issues in design (#00)
    
    * fix: edit design fixes of articles (#000)
    
    * fix: edit design fixes of articles (#000)
    
    * fix: edit design fixes of articles (#000)
    
    * fix: edit design fixes of articles (#000)
    
    * fix: fixes issue in ui of articles for integration purposes (#00)
    
    * fix(): fix issues in the design of articles (#000)
    
    * fix(): design issues fixation in articles (#000)
    
    * fix(): working on the articles issues (#000)
    
    * feat(): write api call of article get (#000)
    
    * fix(): issues api call writing of patch and post (#000)
    
    * fix(): issues api call writing of patch and post (#000)
    
    * fix: build error (#000)
    
    * fix: build error (#000)
    
    * fix(): write api calls for unapproved articles get (#000)
    
    * fix(): write api calls (#000)
    
    * fix(): write api calls (#000)
    
    * fix(): weite api call for articles (#000)
    
    * fix(): fix issues in api (#000)
    
    * fix(): bugs in articles and settings (#000)
    
    * fix(): issues in api design (#000)
    
    ---------
    
    Co-authored-by: Muhammad Shoaib <muhammad.shoaib@ceative.co.uk>
    Co-authored-by: unknown <unknow@unknown.com>
    Co-authored-by: Ali Raza <ali.raza@ceative.co.uk>
    Co-authored-by: unknown <unknown@gmail.com>
    Co-authored-by: iamflash <reverse@flash.com>

--- src/modules/airCustomerPortal/Catalog/Approvals/useApprovals.tsx
+++ src/modules/airCustomerPortal/Catalog/Approvals/useApprovals.tsx
@@ -1,8 +1,25 @@
 import { useRouter } from 'next/router';
 import { useTheme } from '@mui/material/styles';
+import { useState } from 'react';
 export const useApprovals = () => {
   const router = useRouter();
   const { palette }: any = useTheme();
   const theme = useTheme();
-  return { router, theme, palette };
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [selectedApproval, setSelectedApproval] = useState<any>({});
+  const setApproval = (approval: any) => {
+    setSelectedApproval(approval);
+    setIsConfirmModalOpen(true);
+  };
+
+  return {
+    router,
+    theme,
+    palette,
+    isConfirmModalOpen,
+    setIsConfirmModalOpen,
+    selectedApproval,
+    setSelectedApproval,
+    setApproval,
+  };
 };
