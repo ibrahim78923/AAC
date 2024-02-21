03515f02694e3ca3cf44d0442fe77e63e22798c2
Nabeel Ahmed <nabeel.ahmed@ceative.co.uk>
2024-02-21 16:45:55 +0500

    feat(hat): working on call metrices PR bugs resolved  (#AACC-2)

--- src/modules/airCallCenter/CallMetrics/index.tsx
+++ src/modules/airCallCenter/CallMetrics/index.tsx
@@ -1,13 +1,19 @@
 import HorizontalTabs from '@/components/Tabs/HorizontalTabs';
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import React from 'react';
 import AllCalls from './tabsComponents/allCalls';
 import CallsInQueue from './tabsComponents/callsInQueue';
 import CallsInConversation from './tabsComponents/callsInConversation';
 
 const CallMetrics = () => {
+  const theme = useTheme();
   return (
-    <Box sx={{ border: '1px solid #EAECF0', borderRadius: '18px' }}>
+    <Box
+      sx={{
+        border: `1px solid ${theme?.palette?.custom?.off_white_three}`,
+        borderRadius: '18px',
+      }}
+    >
       <Box sx={{ padding: '10px 20px' }}>
         <Typography variant="h3">Call Metrics</Typography>
       </Box>
