4810bcaead08edfaab7f1f55088781acc8f5219b
unknown <unknown@gmail.com>
2024-02-22 16:00:54 +0500

    fix(): fix api call url and design minor issue (#000)

--- src/modules/airCustomerPortal/Dashboard/PopularArticles/index.tsx
+++ src/modules/airCustomerPortal/Dashboard/PopularArticles/index.tsx
@@ -2,23 +2,27 @@ import { CardLayout } from '../CardLayout';
 import { Box, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { DocumentTextIcon } from '@/assets/icons';
-import { styles } from './PopularArticles.style';
 import { usePopularArticles } from './usePopularArticles';
 import ApiErrorState from '@/components/ApiErrorState';
 import SkeletonForm from '@/components/Skeletons/SkeletonForm';
 import NoData from '@/components/NoData';
+import { AIR_CUSTOMER_PORTAL } from '@/constants';
 
-export const PopularArticles = ({ title, handleViewMore }: any) => {
+export const PopularArticles = () => {
   const { palette }: any = useTheme();
-  const { data, isLoading, isFetching, isError } = usePopularArticles();
-  const { articleWrapper } = styles;
+  const { data, isLoading, isFetching, isError, router } = usePopularArticles();
 
   return (
     <CardLayout
-      title={title}
-      btnClick={handleViewMore}
+      title={'Popular Articles'}
+      btnClick={() => {
+        router?.push({
+          pathname: AIR_CUSTOMER_PORTAL?.KNOWLEDGE_BASE,
+        });
+      }}
       maxHeight={260}
       btnPosition={'center'}
+      buttonText={'View More'}
     >
       {isLoading || isFetching ? (
         <SkeletonForm />
@@ -37,7 +41,28 @@ export const PopularArticles = ({ title, handleViewMore }: any) => {
         >
           {!!data?.data?.articles?.length ? (
             data?.data?.articles?.map((article: any) => (
-              <Box key={article?._id} sx={articleWrapper(palette)}>
+              <Box
+                key={article?._id}
+                sx={{
+                  px: 1.2,
+                  py: 0.6,
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: 0.6,
+                  borderRadius: 1,
+                  background: palette?.grey?.[100],
+                  flexBasis: { xs: '100%', lg: '48%' },
+                  cursor: 'pointer',
+                }}
+                onClick={() => {
+                  router?.push({
+                    pathname: AIR_CUSTOMER_PORTAL?.KNOWLEDGE_BASE_TICKET_DETAIL,
+                    query: {
+                      id: article?._id,
+                    },
+                  });
+                }}
+              >
                 <DocumentTextIcon />
                 <Typography variant="body2" color={palette?.grey?.[600]}>
                   {article?.title}
