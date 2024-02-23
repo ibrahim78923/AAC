540298f145d7631c7088b55062f058e0cd55c7ef
Azeem Aslam <azeem.aslam@ceative.co.uk>
2024-02-23 12:42:42 +0500

    feat() : paid adds bugs resolved (#AACC-4)

---
+++ src/modules/airCallCenter/Settings/PhoneNumber/PhoneNumber.data.tsx
@@ -0,0 +1,59 @@
+import { Avatar, Box, Typography } from '@mui/material';
+
+import { AvatarImage } from '@/assets/images';
+import Link from 'next/link';
+
+export const phoneNumberData: any = [
+  {
+    number: (
+      <Box sx={{ display: 'flex', gap: '5px' }}>
+        <Avatar alt="Remy Sharp" src={AvatarImage.src} />
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Typography component={'span'} variant="body3">
+            Jerome Bell
+          </Typography>
+          <Typography component={'span'} variant="body3">
+            (267) 380 - 2788
+          </Typography>
+          <Typography component={'span'} variant="body3">
+            Pennslyvania, US
+          </Typography>
+        </Box>
+      </Box>
+    ),
+    businessHours: 'Company Owner',
+    callActions: 'Orcalo Holdings',
+    action: <Link href="/super-admin/user-management">View Users</Link>,
+  },
+];
+
+export const phoneNumberColumns: any = [
+  {
+    accessorFn: (row: any) => row?.number,
+    id: 'number',
+    cell: (info: any) => info?.getValue(),
+    header: 'Number',
+    isSortable: false,
+  },
+  {
+    accessorFn: (row: any) => row?.businessHours,
+    id: 'businessHours',
+    isSortable: true,
+    header: 'Business Hours',
+    cell: (info: any) => info?.getValue(),
+  },
+  {
+    accessorFn: (row: any) => row?.callActions,
+    id: 'callActions',
+    isSortable: true,
+    header: 'Call Actions',
+    cell: (info: any) => info?.getValue(),
+  },
+  {
+    accessorFn: (row: any) => row?.action,
+    id: 'action',
+    isSortable: true,
+    header: 'Action',
+    cell: (info: any) => info?.getValue(),
+  },
+];
