06baebcb23f314f438d0488b2b2c927d9889f098
Nabeel Ahmed <nabeel.ahmed@ceative.co.uk>
2024-02-23 12:34:12 +0500

    feat(hat): uifixations  (#AIR-7-0)

--- src/modules/airCallCenter/CallMetrics/tabsComponents/allCalls/callsDetailsDrawer/index.tsx
+++ src/modules/airCallCenter/CallMetrics/tabsComponents/allCalls/callsDetailsDrawer/index.tsx
@@ -1,7 +1,70 @@
+import CommonDrawer from '@/components/CommonDrawer';
+import { Box, Tab, Tabs } from '@mui/material';
 import React from 'react';
 
-const CallsDetailsDrawer = () => {
-  return <div></div>;
+const CallsDetailsDrawer = ({
+  isCallDetailsDrawerOpen,
+  setIsCallDetailsDrawerOpen,
+}: any) => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <CommonDrawer
+      isDrawerOpen={isCallDetailsDrawerOpen}
+      onClose={() => setIsCallDetailsDrawerOpen(false)}
+      title={'Call Details for john'}
+      okText={'Create'}
+      isOk
+      cancelText={'Cancel'}
+      footer={false}
+      submitHandler={() => setIsCallDetailsDrawerOpen(false)}
+    >
+      {/* <HorizontalTabs
+        tabsDataArray={[
+          'Call Summary',
+          'Call Lifecycle',
+          'Call Transcription',
+          'Network Log',
+        ]}
+      >
+        <>1</>
+        <>2</>
+        <>3</>
+        <>4</>
+      </HorizontalTabs> */}
+
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+        sx={{
+          padding: '0',
+          marginLeft: '-25px',
+          '@media (max-width:599px)': {
+            marginLeft: '0px',
+          },
+          '& .MuiTab-root': {
+            marginRight: '30px',
+          },
+        }}
+      >
+        <Tab label="Call Summary" />
+        <Tab label="Call Lifecycle" />
+        <Tab label="Call Transcription" />
+        <Tab label="Network Log" />
+      </Tabs>
+
+      <Box>
+        <Box></Box>
+      </Box>
+    </CommonDrawer>
+  );
 };
 
 export default CallsDetailsDrawer;
