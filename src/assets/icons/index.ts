2ce1a5ffea29557559c179ae6f3e647bfcccee41
Nabeel Ahmed <nabeel.ahmed@ceative.co.uk>
2024-02-26 10:07:20 +0500

    feat(hat): working on call metrices viw details drawer  (#AIR-7-0)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -434,3 +434,4 @@ export { default as ResetFilterIcon } from './shared/reset-filter-icon';
 export { default as ReplyDoubleArrowIcon } from './shared/reply-doublearrow-icon';
 export { default as AnnoucementIcon } from './shared/annoucement-icon';
 export { default as TimmerIcon } from './shared/timmer-icon';
+export { default as LeftArrowIcon } from './modules/airCallCenter/CallMetrices/left-arrow';
--- src/modules/airCallCenter/CallMetrics/tabsComponents/allCalls/callsDetailsDrawer/index.tsx
+++ src/modules/airCallCenter/CallMetrics/tabsComponents/allCalls/callsDetailsDrawer/index.tsx
@@ -1,5 +1,8 @@
+import { LeftArrowIcon } from '@/assets/icons';
+import { CardBGBubbles } from '@/assets/images';
 import CommonDrawer from '@/components/CommonDrawer';
-import { Box, Tab, Tabs } from '@mui/material';
+import { Box, Tab, Tabs, Typography } from '@mui/material';
+import Image from 'next/image';
 import React from 'react';
 
 const CallsDetailsDrawer = ({
@@ -14,6 +17,7 @@ const CallsDetailsDrawer = ({
 
   return (
     <CommonDrawer
+      // isDrawerOpen={true}
       isDrawerOpen={isCallDetailsDrawerOpen}
       onClose={() => setIsCallDetailsDrawerOpen(false)}
       title={'Call Details for john'}
@@ -61,7 +65,84 @@ const CallsDetailsDrawer = ({
       </Tabs>
 
       <Box>
-        <Box></Box>
+        <Box
+          sx={{
+            background: '#F6F7F9',
+            height: '122px',
+            borderRadius: '4px',
+            padding: '15px',
+            mt: 2,
+            position: 'relative',
+          }}
+        >
+          <Box
+            sx={{
+              position: 'absolute',
+              right: '0',
+              top: '0',
+            }}
+          >
+            <Image src={CardBGBubbles} alt="card-bg" />
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              flexWrap: 'wrap',
+              mb: 2,
+              position: 'relative',
+            }}
+          >
+            <Typography variant="body4" fontWeight={400}>
+              Incoming call From +12013409847 (Medicines & Pharmacy)
+            </Typography>
+            <Typography variant="body4" fontWeight={400}>
+              07 feb 21,2:15
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              width: '90%',
+              margin: '0 auto',
+            }}
+          >
+            {/* to */}
+            <Box>
+              <Typography variant="body2" textAlign={'center'} fontWeight={400}>
+                Jhon
+              </Typography>
+              <Typography variant="body2" textAlign={'center'} fontWeight={400}>
+                Skynet Services
+              </Typography>
+            </Box>
+            <Box>
+              {' '}
+              <LeftArrowIcon />{' '}
+            </Box>
+            {/* form  */}
+            <Box>
+              <Typography variant="body2" textAlign={'center'} fontWeight={400}>
+                Jhon
+              </Typography>
+              <Typography variant="body2" textAlign={'center'} fontWeight={400}>
+                Skynet Services
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+
+        <Box
+          sx={{
+            background: '#F6F7F9',
+            height: '122px',
+            borderRadius: '4px',
+            padding: '15px',
+            mt: 2,
+          }}
+        ></Box>
       </Box>
     </CommonDrawer>
   );
