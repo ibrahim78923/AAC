e1caf3ff9638c17a419f9267e82c510292feecee
ahsan mukhtar <muhammadahsan.mukhtar@ceative.co.uk>
2024-02-27 09:46:07 +0500

    feat(): work on to do list on call center dashboard my performance. (#AACC-104)

--- src/modules/airCallCenter/Dashboard/MyPerformance/toDoList/index.tsx
+++ src/modules/airCallCenter/Dashboard/MyPerformance/toDoList/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, useTheme } from '@mui/material';
 import {
   AddPlusPrimaryIcon,
   WhiteMessageIcon,
@@ -6,21 +6,27 @@ import {
 } from '@/assets/icons';
 
 export const ToDoList = () => {
+  const theme = useTheme();
+
   return (
     <>
-      <Typography variant="h4" sx={{ marginLeft: '10px' }}>
-        Incoming Calls
-      </Typography>
       <Box
         sx={{
-          border: '1px solid #E5E7EB',
+          border: `1px solid ${theme?.palette?.grey[700]}`,
           borderRadius: '8px',
           padding: '20px',
+          marginTop: '60px',
         }}
       >
-        <Typography>To-Do</Typography>
+        <Typography variant="h5" sx={{ marginBottom: '10px' }}>
+          To-Do
+        </Typography>
         <Button
-          sx={{ border: '1px solid #E5E7EB', height: '70px', width: '100%' }}
+          sx={{
+            border: `1px solid ${theme?.palette?.grey[700]}`,
+            height: '70px',
+            width: '100%',
+          }}
           startIcon={<AddPlusPrimaryIcon />}
         >
           {' '}
@@ -33,7 +39,7 @@ export const ToDoList = () => {
           alignItems={'center'}
           gap={'12px'}
           sx={{
-            backgroundColor: '#38CAB5',
+            backgroundColor: theme?.palette?.primary?.main,
             marginTop: '20px',
             padding: '15px',
             borderRadius: '8px',
@@ -49,7 +55,35 @@ export const ToDoList = () => {
               Presentation about the Air Applecart project
             </Typography>
           </Box>
+          <Box display={'grid'} gap={'15px'}>
+            <Typography variant="body3">1 day</Typography>
+            <WhiteTrashIcon />
+          </Box>
+        </Box>
+
+        <Box
+          display={'flex'}
+          justifyContent={'space-between'}
+          alignItems={'center'}
+          gap={'12px'}
+          sx={{
+            backgroundColor: theme?.palette?.warning?.main,
+            marginTop: '20px',
+            padding: '15px',
+            borderRadius: '8px',
+            color: 'white',
+          }}
+        >
           <Box>
+            <WhiteMessageIcon />
+          </Box>
+          <Box>
+            <Typography variant="body2">Lunch</Typography>
+            <Typography variant="body3">
+              Lunch with stakeholders at Glascow at 01:30 PM
+            </Typography>
+          </Box>
+          <Box display={'grid'} gap={'15px'}>
             <Typography variant="body3">1 day</Typography>
             <WhiteTrashIcon />
           </Box>
