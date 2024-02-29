b5a82a840c1de32e6b124fff1924872b5cefe466
Zain Jahan~ <zain.jahan@ceative.co.uk>
2024-02-29 12:57:05 +0500

    feat(hat): Working on dashboard services(#653)

--- src/modules/airServices/Dashboard/AnnouncementDashboard/AnnouncementDashboardCard/AnnouncementDashboardCard.data.tsx
+++ src/modules/airServices/Dashboard/AnnouncementDashboard/AnnouncementDashboardCard/AnnouncementDashboardCard.data.tsx
@@ -14,6 +14,6 @@ export const announcementDashboardCardData = (customerAnnouncement: any[]) => {
       />
     ),
     announcementTime: announcementItem?.createdAt,
-    announcementAvatar: announcementItem?.userName,
+    announcementAvatar: announcementItem?.userNamess,
   }));
 };
