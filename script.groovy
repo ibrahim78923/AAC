caf0fbd0805d25fa0c4e457807ab99dea089c3e1
Ali Ahmad <aliahmadqj@gmail.com>
2024-03-03 20:29:45 +0500

    #Adjusting Jenkinsfile

--- script.groovy
+++ script.groovy
@@ -3,8 +3,8 @@ def deployImg(){
           lock('deploy_AAC_FE_lock'){
             sshagent(['AAC']) {
               sh """
-                ssh -o StrictHostKeyChecking=no -tt ubuntu@18.171.60.181 '
-                  cd AAC-FE
+                ssh -o StrictHostKeyChecking=no -tt ubuntu@192.168.100.131 '
+                  cd aac/AAC-FE
                   git pull origin dev
                   docker compose up -d --build
                   docker system prune -a -f
