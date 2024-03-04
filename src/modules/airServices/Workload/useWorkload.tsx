cdfa1f7dec0c3c2081666d2bd1678d1823a0a617
Muhammad Ahmer Akram <muhammadahmer.akram@ceative.co.uk>
2024-03-04 13:40:47 +0500

    #feat: workload integration. (#0000)

---
+++ src/modules/airServices/Workload/useWorkload.tsx
@@ -0,0 +1,93 @@
+import {
+  useLazyGetWorkloadFilterQuery,
+  useLazyGetWorkloadQuery,
+} from '@/services/airServices/workload';
+import dayjs from 'dayjs';
+import { useRouter } from 'next/router';
+import { useEffect, useRef, useState } from 'react';
+
+export default function useWorkload() {
+  const calendarRef = useRef<any>(null);
+  const router: any = useRouter();
+
+  const [filter, setFilter] = useState<any>({
+    countDayWise: undefined,
+    countDayWiseHours: undefined,
+    countDayWiseHoursAverage: undefined,
+  });
+
+  const [onClickEvent, setOnClickEvent] = useState<any>({
+    open: null,
+    data: null,
+  });
+  const [addPlannedEffort, setAddPlannedEffort] = useState<any>({
+    open: null,
+    data: null,
+  });
+  const [dateCalendar, setDateCalendar] = useState<any>(
+    dayjs()?.startOf('week')?.format('YYYY-MM-DD'),
+  );
+  const [selected, setSelected] = useState<any>(null);
+  const [trigger, status] = useLazyGetWorkloadQuery();
+  const [triggerFilter, statusFilter] = useLazyGetWorkloadFilterQuery();
+
+  useEffect(() => {
+    trigger({
+      startDate: dayjs()?.startOf('week')?.add(1, 'day')?.toISOString(),
+      endDate: dayjs()?.endOf('week')?.toISOString(),
+      userIds: selected?._id,
+    });
+  }, [selected]);
+
+  useEffect(() => {
+    triggerFilter({
+      startDate: dayjs()?.startOf('week')?.add(1, 'day')?.toISOString(),
+      endDate: dayjs()?.endOf('week')?.toISOString(),
+      countDayWise: filter?.countDayWise,
+      countDayWiseHours: filter?.countDayWiseHours,
+      countDayWiseHoursAverage: filter?.countDayWiseHoursAverage,
+    });
+  }, [filter]);
+
+  const COMPLETED = 'Done';
+  const IN_PROGRESS = 'In-Progress';
+
+  const dateChangeHandler = async (date: any) => {
+    setDateCalendar(date);
+    try {
+      await trigger({
+        startDate: dayjs(date)?.startOf('week')?.add(1, 'day')?.toISOString(),
+        endDate: dayjs(date)?.endOf('week')?.toISOString(),
+        userIds: selected?._id,
+      })?.unwrap();
+
+      await triggerFilter({
+        startDate: dayjs(date)?.startOf('week')?.add(1, 'day')?.toISOString(),
+        endDate: dayjs(date)?.endOf('week')?.toISOString(),
+        countDayWise: filter?.countDayWise,
+        countDayWiseHours: filter?.countDayWiseHours,
+        countDayWiseHoursAverage: filter?.countDayWiseHoursAverage,
+      })?.unwrap();
+
+      calendarRef?.current?.getApi()?.gotoDate(date);
+    } catch (error: any) {}
+  };
+
+  return {
+    status,
+    statusFilter,
+    dateChangeHandler,
+    dateCalendar,
+    selected,
+    setSelected,
+    setFilter,
+    calendarRef,
+    COMPLETED,
+    IN_PROGRESS,
+    setAddPlannedEffort,
+    router,
+    setOnClickEvent,
+    onClickEvent,
+    addPlannedEffort,
+  };
+}
