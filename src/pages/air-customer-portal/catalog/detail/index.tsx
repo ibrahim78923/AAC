bb45ad2744a4f154b5383df956e034b1b66b0e5d
tayyab khan satti <tayyabkhan.satti@ceative.co.uk>
2024-03-05 14:29:08 +0500

    feat:(hat): i waorked on permission of customer portal catalog. (#0000)

--- src/pages/air-customer-portal/catalog/detail/index.tsx
+++ src/pages/air-customer-portal/catalog/detail/index.tsx
@@ -1,3 +1,4 @@
+import { Permissions } from '@/constants/permissions';
 import Layout from '@/layout';
 import CatalogService from '@/modules/airCustomerPortal/Catalog/CatalogService';
 
@@ -5,6 +6,15 @@ const CatalogServicePage = () => {
   return <CatalogService />;
 };
 CatalogServicePage.getLayout = function getLayout(page: any) {
-  return <Layout>{page}</Layout>;
+  return (
+    <Layout
+      permission={[
+        Permissions?.AIR_CUSTOMER_PORTAL_CATALOG_DETAIL,
+        '65e0175d29624d4d419196a6',
+      ]}
+    >
+      {page}
+    </Layout>
+  );
 };
 export default CatalogServicePage;
--- src/pages/air-customer-portal/catalog/index.tsx
+++ src/pages/air-customer-portal/catalog/index.tsx
@@ -1,10 +1,20 @@
+import { Permissions } from '@/constants/permissions';
 import Layout from '@/layout';
 import { Catalog } from '@/modules/airCustomerPortal/Catalog';
 
 const CatalogPage = () => <Catalog />;
 
 CatalogPage.getLayout = function getLayout(page: any) {
-  return <Layout>{page}</Layout>;
+  return (
+    <Layout
+      permission={[
+        Permissions?.AIR_CUSTOMER_PORTAL_CATALOG,
+        '6565bf7695129275781dda3b',
+      ]}
+    >
+      {page}
+    </Layout>
+  );
 };
 
 export default CatalogPage;
