f3fed4717beb2872856e7d313b067fd20e19400d
Rao Mudasir <raomuhammad.mudasir@ceative.co.uk>
2024-03-08 11:29:29 +0500

    feat():  Software overview  (#AASSS-186)

--- src/modules/airServices/Assets/Software/SingleSoftwareDetail/Overview/UsageActivity/index.tsx
+++ src/modules/airServices/Assets/Software/SingleSoftwareDetail/Overview/UsageActivity/index.tsx
@@ -1,53 +1,76 @@
-import { useDrawingArea } from '@mui/x-charts';
 import { PieChart } from '@mui/x-charts/PieChart';
 import { StyledText, styles } from './UsageActivity.style';
-import { Box, Typography, useTheme } from '@mui/material';
+import { Box, Skeleton, Typography } from '@mui/material';
+import ApiErrorState from '@/components/ApiErrorState';
+import { useUsageActivity } from './useUsageActivity';
 
-function UsageActivity({ usageActivityLabel, usageActivityData }: any) {
-  const theme: any = useTheme();
-  const { width, height, left, top } = useDrawingArea();
+function UsageActivity(props: any) {
+  const {
+    theme,
+    usageActivityLabel,
+    isLoading,
+    isError,
+    data,
+    usageActivityData,
+    width,
+    height,
+    left,
+    top,
+  } = useUsageActivity(props);
+
   return (
     <>
       <Box sx={styles.mainBox(theme)}>
         <Typography sx={styles?.heading}>
           {usageActivityLabel?.heading}
         </Typography>
-        <PieChart
-          series={[
-            {
-              data: usageActivityData,
-              cx: styles?.alignGraph?.cx,
-              cy: styles?.alignGraph?.cy,
-              innerRadius: styles?.alignGraph?.innerRadius,
-              outerRadius: styles?.alignGraph?.outerRadius,
-              paddingAngle: styles?.alignGraph?.paddingAngle,
-              cornerRadius: styles?.alignGraph?.cornerRadius,
-              startAngle: styles?.alignGraph?.startAngle,
-              endAngle: styles?.alignGraph?.endAngle,
-            },
-          ]}
-          sx={styles?.chart}
-          legend={{ hidden: true }}
-          width={370}
-          height={280}
-        >
-          <StyledText
-            x={left + width / styles?.alignGraph?.numberLabelX}
-            y={top + height / styles?.alignGraph?.numberLabelY}
+        {isLoading ? (
+          <Box p={2}>
+            <Skeleton height={250} />
+          </Box>
+        ) : isError ? (
+          <ApiErrorState />
+        ) : (
+          <PieChart
+            series={[
+              {
+                data: usageActivityData?.map((item: any) => ({
+                  ...item,
+                  value: data?.data?.usageActivity,
+                })),
+                cx: styles?.alignGraph?.cx,
+                cy: styles?.alignGraph?.cy,
+                innerRadius: styles?.alignGraph?.innerRadius,
+                outerRadius: styles?.alignGraph?.outerRadius,
+                paddingAngle: styles?.alignGraph?.paddingAngle,
+                cornerRadius: styles?.alignGraph?.cornerRadius,
+                startAngle: styles?.alignGraph?.startAngle,
+                endAngle: styles?.alignGraph?.endAngle,
+              },
+            ]}
+            sx={styles?.chart}
+            legend={{ hidden: true }}
+            width={370}
+            height={280}
           >
-            {usageActivityLabel?.numLabel}
-          </StyledText>
+            <StyledText
+              x={left + width / styles?.alignGraph?.numberLabelX}
+              y={top + height / styles?.alignGraph?.numberLabelY}
+            >
+              {data?.data?.usageActivity}
+            </StyledText>
 
-          <StyledText
-            x={left + width / styles?.alignGraph?.textLabelX}
-            y={top + height / styles?.alignGraph?.textLabelY}
-          >
-            {usageActivityLabel?.textLabel}
-          </StyledText>
-        </PieChart>
+            <StyledText
+              x={left + width / styles?.alignGraph?.textLabelX}
+              y={top + height / styles?.alignGraph?.textLabelY}
+            >
+              {usageActivityLabel?.textLabel}
+            </StyledText>
+          </PieChart>
+        )}
         <Box sx={styles?.footerBox}>
           <Box sx={styles?.footerTypographyBox(theme)}>
-            {usageActivityLabel?.inactive}
+            {data?.data?.inActiveUsers}
           </Box>
           <Typography>Inactive</Typography>
         </Box>
