645552a6412f7baa52edc0a8152da97b0e214cf8
Ghulam Mohiuddin <ghulam.mohiuddin@ceative.co.uk>
2024-03-08 15:48:21 +0500

    feat(): Worked on implementting permission . (# AACSA-487777)

--- src/modules/orgAdmin/Organization/index.tsx
+++ src/modules/orgAdmin/Organization/index.tsx
@@ -4,6 +4,8 @@ import { Box, Typography, Theme, useTheme } from '@mui/material';
 
 import OrganizationCard from './OrganizationSubFolder/OrganizationCard';
 import OrganizationTable from './OrganizationSubFolder/OrganizationTable';
+import PermissionsGuard from '@/GuardsAndPermissions/PermissonsGuard';
+import { ORG_ADMIN_ORGANIZATION_PERMISSIONS } from '@/constants/permission-keys';
 
 const Organization = () => {
   const theme = useTheme<Theme>();
@@ -35,23 +37,29 @@ const Organization = () => {
             Organization
           </Typography>
           <OrganizationCard />
-          <Typography
-            variant="h4"
-            sx={{
-              fontWeight: 500,
-              paddingTop: '20px',
-              paddingBottom: '20px',
-              textAlign: {
-                lg: 'start',
-                md: 'start',
-                sm: 'center',
-                xs: 'center',
-              },
-            }}
+          <PermissionsGuard
+            permissions={[
+              ORG_ADMIN_ORGANIZATION_PERMISSIONS?.COMPANY_ACCOUNT_LIST,
+            ]}
           >
-            Company Accounts
-          </Typography>
-          <OrganizationTable />
+            <Typography
+              variant="h4"
+              sx={{
+                fontWeight: 500,
+                paddingTop: '20px',
+                paddingBottom: '20px',
+                textAlign: {
+                  lg: 'start',
+                  md: 'start',
+                  sm: 'center',
+                  xs: 'center',
+                },
+              }}
+            >
+              Company Accounts
+            </Typography>
+            <OrganizationTable />
+          </PermissionsGuard>
         </Box>
       </Box>
     </>
