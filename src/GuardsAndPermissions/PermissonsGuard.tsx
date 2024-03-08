f2d9425beddb800741dbb096c4c1e9b2398f13c2
Zain Jahan~ <zain.jahan@ceative.co.uk>
2024-03-08 09:58:06 +0500

    feat(hat): Working on purchase order status(#362)

--- src/GuardsAndPermissions/PermissonsGuard.tsx
+++ src/GuardsAndPermissions/PermissonsGuard.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react';
 
 import PermissionDenied from '@/components/PermisisonDenied';
 import { getActivePermissionsSession } from '@/utils';
-import { Box } from '@mui/material';
 
 const useCurrentPermissions = () => {
   const permissions = getActivePermissionsSession();
@@ -14,32 +13,34 @@ function checkPermissions(permissions: any, modulePermissions: any) {
   modulePermissions?.forEach((value: any) => {
     componentPermissionsDictionary[value] = true;
   });
-  if (permissions?.length > 0) {
-    for (const permission of permissions) {
-      if (componentPermissionsDictionary[permission]) {
-        return true; // At least one permission is available
-      }
-    }
-  }
-  return false; // None of the permissions are available
+  return true;
+  // if (permissions?.length > 0) {
+  //   for (const permission of permissions) {
+  //     if (componentPermissionsDictionary[permission]) {
+  //       return true; // At least one permission is available
+  //     }
+  //   }
+  // }
+  // return false; // None of the permissions are available
 }
 
 export default function PermissionsGuard({
   children,
   permissions,
-  isPage = false,
+  sidebar,
 }: {
   children: ReactNode;
   permissions: any;
-  isPage?: boolean;
+  sidebar?: string;
 }) {
   const currentPermissions = useCurrentPermissions();
   const permissionsCheck = checkPermissions(currentPermissions, permissions);
-  if (permissionsCheck) {
-    return <>{children}</>;
-  } else if (isPage) {
-    <PermissionDenied />;
-  } else {
-    return <Box></Box>;
-  }
+
+  return permissionsCheck ? (
+    <>{children}</>
+  ) : sidebar === 'ItemPermission' ? (
+    ' '
+  ) : (
+    <PermissionDenied />
+  );
 }
