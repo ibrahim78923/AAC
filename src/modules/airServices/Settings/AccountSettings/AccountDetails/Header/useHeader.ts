5a2fad04c16bb82d42aa85b551f8415e16e62bdb
Ali Haider <ali.haider@ceative.co.uk>
2024-03-13 14:11:15 +0500

    feat(hat): working on AASSS-577 (#0577)

--- src/modules/airServices/Settings/AccountSettings/AccountDetails/Header/useHeader.ts
+++ src/modules/airServices/Settings/AccountSettings/AccountDetails/Header/useHeader.ts
@@ -1,8 +1,12 @@
+import useAuth from '@/hooks/useAuth';
+import { usePatchProfileAvatarMutation } from '@/services/airServices/settings/account-settings/account-details';
+import { errorSnackbar, successSnackbar } from '@/utils/api';
 import { useRouter } from 'next/router';
 import { ChangeEvent, useState } from 'react';
 
 export const useHeader = () => {
   const router = useRouter();
+  const user: any = useAuth();
   const [uploadedImage, setUploadedImage] = useState<string | undefined>(
     undefined,
   );
@@ -10,16 +14,35 @@ export const useHeader = () => {
   const fullScreenPosition = { top: 0, left: 0, right: 0, bottom: 0 };
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event?.target?.files?.[0];
-    if (file) {
-      const imageUrl = URL?.createObjectURL(file);
-      setUploadedImage(imageUrl);
+    const fileInput = event?.target;
+    if (fileInput && fileInput.files && fileInput.files.length > 0) {
+      const file: any = fileInput.files[0];
+      setUploadedImage(file);
+      isSubmit();
+      fileInput.value = '';
+    }
+  };
+
+  const [patchProfileAvatarTrigger] = usePatchProfileAvatarMutation();
+  const isSubmit = async () => {
+    const reportAnIssueData: any = new FormData();
+    reportAnIssueData?.append('avatar', uploadedImage);
+
+    const payload = {
+      id: user?.user?._id,
+      removeAvatar: false,
+      body: reportAnIssueData,
+    };
+    try {
+      const res: any = await patchProfileAvatarTrigger(payload)?.unwrap();
+      successSnackbar(res?.message ?? 'Profile Update Successfully');
+    } catch (error) {
+      errorSnackbar();
     }
   };
 
   return {
     handleFileChange,
-    uploadedImage,
     isHovered,
     setIsHovered,
     fullScreenPosition,
