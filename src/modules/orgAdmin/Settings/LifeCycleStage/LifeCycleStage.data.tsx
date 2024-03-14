17471d370dc961f1dc4a2e188f9f6011dd9c10f3
Ghulam Mohiuddin <ghulam.mohiuddin@ceative.co.uk>
2024-03-14 14:41:50 +0500

    feat(): Added Org admin settings permissons . (#AASS-459)

--- src/modules/orgAdmin/Settings/LifeCycleStage/LifeCycleStage.data.tsx
+++ src/modules/orgAdmin/Settings/LifeCycleStage/LifeCycleStage.data.tsx
@@ -6,6 +6,8 @@ import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import { DeleteCrossIcon, EditPenIcon, ViewEyeIcon } from '@/assets/icons';
 
 import * as Yup from 'yup';
+import PermissionsGuard from '@/GuardsAndPermissions/PermissonsGuard';
+import { ORG_ADMIN_SETTINGS_LIFECYCLE_STAGES_PERMISSIONS } from '@/constants/permission-keys';
 
 export const LifeCycleStagevalidationSchema: any = Yup.object().shape({
   name: Yup.string()
@@ -117,32 +119,38 @@ export const columns = (
       isSortable: true,
       header: 'Action',
       cell: (info: any) => (
-        <Box sx={{ display: 'flex', gap: 0.5 }}>
-          <Box
-            sx={{ cursor: 'pointer' }}
-            onClick={() => {
-              setIsDraweropen(true);
-              setIsModalHeading('View');
-            }}
-          >
-            <ViewEyeIcon />
+        <PermissionsGuard
+          permissions={[
+            ORG_ADMIN_SETTINGS_LIFECYCLE_STAGES_PERMISSIONS?.ACTIONS,
+          ]}
+        >
+          <Box sx={{ display: 'flex', gap: 0.5 }}>
+            <Box
+              sx={{ cursor: 'pointer' }}
+              onClick={() => {
+                setIsDraweropen(true);
+                setIsModalHeading('View');
+              }}
+            >
+              <ViewEyeIcon />
+            </Box>
+            <Box
+              sx={{ cursor: 'pointer' }}
+              onClick={() => {
+                handleEditClick(info?.row?.original);
+                setIsModalHeading('Edit');
+              }}
+            >
+              <EditPenIcon />
+            </Box>
+            <Box
+              sx={{ cursor: 'pointer' }}
+              onClick={() => handleDeleteRecord(info?.row?.original?._id)}
+            >
+              <DeleteCrossIcon />
+            </Box>
           </Box>
-          <Box
-            sx={{ cursor: 'pointer' }}
-            onClick={() => {
-              handleEditClick(info?.row?.original);
-              setIsModalHeading('Edit');
-            }}
-          >
-            <EditPenIcon />
-          </Box>
-          <Box
-            sx={{ cursor: 'pointer' }}
-            onClick={() => handleDeleteRecord(info?.row?.original?._id)}
-          >
-            <DeleteCrossIcon />
-          </Box>
-        </Box>
+        </PermissionsGuard>
       ),
     },
   ];
