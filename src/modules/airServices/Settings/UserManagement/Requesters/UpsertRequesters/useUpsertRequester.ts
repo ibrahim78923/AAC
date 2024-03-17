749ec4c52c577bdf2d844baec5a26273a2dd47ff
iamflash <iamflash>
2024-03-17 23:27:50 +0500

    refactor(): refactoring the code where finds necessary (#38)

---
+++ src/modules/airServices/Settings/UserManagement/Requesters/UpsertRequesters/useUpsertRequester.ts
@@ -0,0 +1,80 @@
+import { ROLES } from '@/constants/strings';
+import { errorSnackbar, successSnackbar } from '@/utils/api';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import {
+  upsertRequestersDefaultValues,
+  upsertRequestersValidationSchema,
+} from './UpsertRequesters.data';
+import {
+  usePatchRequesterMutation,
+  usePostAddRequesterMutation,
+} from '@/services/airServices/settings/user-management';
+import { useRouter } from 'next/router';
+
+export const useUpsertRequester = (props: any) => {
+  const { setIsDrawerOpen } = props;
+  const router = useRouter();
+  const { _id } = router?.query;
+  const methods: any = useForm({
+    resolver: yupResolver(upsertRequestersValidationSchema),
+    defaultValues: upsertRequestersDefaultValues(null),
+  });
+  const { handleSubmit, reset } = methods;
+  const [patchRequesterTrigger, patchRequesterStatus] =
+    usePatchRequesterMutation();
+  const [addRequesterTrigger, addRequesterStatus] =
+    usePostAddRequesterMutation();
+
+  const submitUpsertRequester = async (data: any) => {
+    const payload = {
+      firstName: data?.firstName,
+      lastName: data?.lastName,
+      email: data?.email,
+      jobTitle: data?.jobTitle,
+      phoneNumber: data?.phoneNumber,
+      timezone: data?.timezone,
+      role: ROLES?.ORG_REQUESTER,
+    };
+    if (!!_id) {
+      editRequesterDetails?.(payload);
+      return;
+    }
+    try {
+      await addRequesterTrigger(payload).unwrap();
+      successSnackbar('Requesters Added Successfully');
+      handleClose?.();
+    } catch (error: any) {
+      errorSnackbar(error?.data?.message);
+    }
+  };
+
+  const editRequesterDetails = async (data: any) => {
+    const formData = {
+      id: _id,
+      ...data,
+    };
+    try {
+      await patchRequesterTrigger(formData)?.unwrap();
+      successSnackbar('Single Requesters Edit  Successfully');
+      setIsDrawerOpen(false);
+    } catch (error: any) {
+      errorSnackbar(error?.data?.message);
+    }
+    handleClose?.();
+  };
+
+  const handleClose = () => {
+    setIsDrawerOpen(false);
+    reset?.();
+  };
+  return {
+    handleClose,
+    methods,
+    handleSubmit,
+    submitUpsertRequester,
+    addRequesterStatus,
+    patchRequesterStatus,
+    _id,
+  };
+};
