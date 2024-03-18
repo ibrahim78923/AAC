9629c4160a5aa4c6e3b34ef765b977205f4ed4a7
Ghulam Mohiuddin <ghulam.mohiuddin@ceative.co.uk>
2024-03-18 15:34:43 +0500

    feat(): worked on super admin settings issues . (#AACSA-538)

--- src/components/StatusBadge/index.tsx
+++ src/components/StatusBadge/index.tsx
@@ -9,17 +9,38 @@ import { v4 as uuidv4 } from 'uuid';
 const StatusBadge = (props: BadgeStatusI) => {
   const { options = undefined, onChange, value, defaultValue } = props;
   const theme = useTheme();
+
+  // const switchColor = (val:string) => {
+  //   let bgColor;
+  //   switch (val) {
+  //     case 'pending':
+  //       bgColor = "#BCC1CE";
+  //       break;
+  //       case 'rejected':
+  //         bgColor = "#FF4A4A";
+  //         break;
+  //       case 'shortlisted':
+  //         bgColor = "#FF4A4A";
+  //         break;
+  //     case 'interviewed':
+  //       bgColor = "#47B263";
+  //       break;
+  //     default:
+  //       bgColor = ''
+  //   }
+  //   return bgColor;
+  // }
   return (
     <Select
       IconComponent={KeyboardArrowDown}
       sx={{
         fontSize: '14px',
         backgroundColor:
-          value === 'ACTIVE' || value === 'open'
+          value === 'ACTIVE' || value === 'open' || value === 'OPEN'
             ? 'rgba(71, 178, 99, 0.2)'
             : 'rgba(255, 74, 74, 0.2)',
         color:
-          value === 'ACTIVE' || value === 'open'
+          value === 'ACTIVE' || value === 'open' || value === 'OPEN'
             ? theme?.palette?.success?.main
             : theme?.palette?.error?.main,
         ...styles.select(value, theme),
