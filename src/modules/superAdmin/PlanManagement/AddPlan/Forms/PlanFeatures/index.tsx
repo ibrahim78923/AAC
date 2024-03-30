8df6b866fbcbadd08e9aefd2ad9d4b99682959e9
ahsan mukhtar <ahsanmukhtar349@gmail.com>
2024-03-30 14:11:44 +0500

    feat(fix): work on plan management add edit prduct and in crm case. (#AACSA-482)

--- src/modules/superAdmin/PlanManagement/AddPlan/Forms/PlanFeatures/index.tsx
+++ src/modules/superAdmin/PlanManagement/AddPlan/Forms/PlanFeatures/index.tsx
@@ -142,6 +142,83 @@ const PlanFeatures = ({ methods, handleSubmit }: any) => {
             </AccordionDetails>
           </Accordion>
         ))
+      ) : Array.isArray(planManagement?.addPlanForm?.productId) &&
+        planManagement?.addPlanForm?.productId?.length > 1 ? (
+        planManagement?.addPlanForm?.productId?.map((feature: string) => (
+          <Accordion
+            expanded={expandedAccordion === feature}
+            onChange={handleExpandAccordionChange(feature)}
+            key={uuidv4()}
+            disableGutters
+            sx={{
+              '&.MuiAccordion': {
+                '&.Mui-expanded': {
+                  boxShadow: 'theme.customShadows.z8',
+                  borderRadius: '8px',
+                },
+                '&.Mui-disabled': {
+                  backgroundColor: 'transparent',
+                },
+              },
+              '& .MuiAccordionSummary-root': {
+                backgroundColor: theme?.palette?.blue?.main,
+                color: theme?.palette?.common?.white,
+                borderRadius: '8px',
+                marginBottom: '11px',
+              },
+            }}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="plan-features-sales-accordion-content"
+              id="plan-features-sales-accordion-header"
+            >
+              <Typography variant="h4">
+                {productList &&
+                  productsOptions?.find((obj: any) => obj?.value === feature)
+                    ?.label}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Grid container>
+                {!isNullOrEmpty(productFeatures?.data?.productfeatures)
+                  ? productFeatures?.data?.productfeatures?.map((item: any) => {
+                      return (
+                        <Grid item xs={12} sm={6} lg={4} xl={3} key={uuidv4()}>
+                          <Box sx={{ width: 'max-content', display: 'flex' }}>
+                            <FormProvider
+                              methods={methods}
+                              onSubmit={handleSubmit}
+                            >
+                              <RHFMultiCheckbox
+                                name="features"
+                                label="Features"
+                                options={[
+                                  {
+                                    label: item?.name,
+                                    value: item?._id,
+                                  },
+                                ]}
+                              />
+                            </FormProvider>
+                            <Box
+                              sx={{ cursor: 'pointer' }}
+                              onClick={() => {
+                                handleOpenFeaturesModal();
+                                setFeatureName(item?.name);
+                              }}
+                            >
+                              <AddPlusPrimaryIcon />
+                            </Box>
+                          </Box>
+                        </Grid>
+                      );
+                    })
+                  : 'No Data'}
+              </Grid>
+            </AccordionDetails>
+          </Accordion>
+        ))
       ) : (
         // : planManagement?.addPlanForm?.productId?.map((feature: string) => ())
         <Accordion
@@ -187,20 +264,12 @@ const PlanFeatures = ({ methods, handleSubmit }: any) => {
                   (obj: any) =>
                     obj?.value === planManagement?.addPlanForm?.productId,
                 )?.label}
-              {/* {productList && type === 'edit' &&
-                productsOptions?.find((obj: any) =>
-                  obj?.value === planManagement?.addPlanForm?.productId[0],
-                )?.label} */}
               {productList &&
                 type === 'edit' &&
-                planManagement?.addPlanForm?.productId.map((id: string) => (
-                  <div key={id}>
-                    {
-                      productsOptions?.find((obj: any) => obj?.value === id)
-                        ?.label
-                    }
-                  </div>
-                ))}
+                productsOptions?.find(
+                  (obj: any) =>
+                    obj?.value === planManagement?.addPlanForm?.productId[0],
+                )?.label}
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
