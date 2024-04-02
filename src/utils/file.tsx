1bc537bdc9f94e86f9e7c7b08f8e1b312c4dc6a2
iamflash <iamflash>
2024-04-02 06:48:46 +0500

    feat(): import file csv reder function (#1043)

--- src/utils/file.tsx
+++ src/utils/file.tsx
@@ -33,3 +33,47 @@ export const findAttributeValues = (
 
   return values;
 };
+
+export const processCSV = (str: any, delim = ',') => {
+  const cleanStr = str?.replace?.(/"|\r/g, '');
+  const headers = cleanStr?.slice?.(0, cleanStr?.indexOf?.('\n')).split(delim);
+  const rows = cleanStr.slice(cleanStr.indexOf('\n') + 1).split('\n');
+  const filterEmptyRows = rows?.filter((x: any) => !!x);
+  const newArray = filterEmptyRows?.map((row: any) => {
+    const values = row.split(delim);
+    const eachObject = headers.reduce((obj: any, header: any, i: any) => {
+      obj[header] = values[i];
+      return obj;
+    }, {});
+    return eachObject;
+  });
+  return newArray;
+};
+
+new FileReader().onload = function (e: any) {
+  const text = e?.target?.result;
+  processCSV(text); // plugged in here
+};
+
+export const csvReader = (delim = ',') => {
+  const csvFileReader = new FileReader();
+
+  csvFileReader.onload = function (e: any) {
+    const text = e?.target?.result;
+    const cleanStr = text?.replace?.(/"|\r/g, '');
+    const headers = cleanStr
+      ?.slice?.(0, cleanStr?.indexOf?.('\n'))
+      .split(delim);
+    const rows = cleanStr.slice(cleanStr.indexOf('\n') + 1).split('\n');
+    const filterEmptyRows = rows?.filter((x: any) => !!x);
+    const newArray = filterEmptyRows?.map((row: any) => {
+      const values = row.split(delim);
+      const eachObject = headers.reduce((obj: any, header: any, i: any) => {
+        obj[header] = values[i];
+        return obj;
+      }, {});
+      return eachObject;
+    });
+    return newArray;
+  };
+};
