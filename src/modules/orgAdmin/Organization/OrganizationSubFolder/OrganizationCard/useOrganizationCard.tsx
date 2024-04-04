f2303b492fd4d0d3a54d2f59baa5a8edae94f6cf
Mubashir-orcalo <mubashir.yusuf@ceative.co.uk>
2024-04-04 12:06:10 +0500

    feat() : add validations to edit company profile, update add role loadings, update user loading,and add attachment notification.(#AIR-4)

--- src/modules/orgAdmin/Organization/OrganizationSubFolder/OrganizationCard/useOrganizationCard.tsx
+++ src/modules/orgAdmin/Organization/OrganizationSubFolder/OrganizationCard/useOrganizationCard.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Theme, useTheme } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { useGetOrganizationMainIdQuery } from '@/services/orgAdmin/organization';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { defaultValues, validationSchema } from './OrganizationCard.data';
 
 const useOrganizationCard = () => {
   const [isOpenDrawer, setIsOpenDrawer] = useState(false);
@@ -10,11 +12,18 @@ const useOrganizationCard = () => {
   const { data, isLoading, isError, isFetching, isSuccess } =
     useGetOrganizationMainIdQuery({ id: '6540b33a0637653df4a4f8ac' });
 
-  const methods: any = useForm({});
+  const methods = useForm<any>({
+    resolver: yupResolver(validationSchema),
+    defaultValues: defaultValues,
+  });
 
-  const { handleSubmit } = methods;
+  const { handleSubmit, reset } = methods;
 
   const onSubmit: any = async () => {};
+
+  const handleCloseDrawer = () => {
+    setIsOpenDrawer(false), reset();
+  };
   return {
     theme,
     isOpenDrawer,
@@ -27,6 +36,7 @@ const useOrganizationCard = () => {
     isFetching,
     isSuccess,
     data,
+    handleCloseDrawer,
   };
 };
 
