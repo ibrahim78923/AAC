7ebbbb03068d076020df201f3bcbaac614a77ad3
Ghulam Mohiuddin <ghulam.mohiuddin@ceative.co.uk>
2024-04-18 09:34:08 +0500

    feat(): contact associations issues fixed. (#AIR-939)

--- src/modules/airSocial/Contacts/ViewDetails/Associations/Tickets/useTickets.tsx
+++ src/modules/airSocial/Contacts/ViewDetails/Associations/Tickets/useTickets.tsx
@@ -2,25 +2,10 @@ import { useState } from 'react';
 
 import { useTheme } from '@mui/material';
 // import { PAGINATION } from '@/config';
-import { useGetContactAssociationsQuery } from '@/services/commonFeatures/contacts';
+import { useGetContactAssociationsQuery } from '@/services/commonFeatures/contacts/associations';
 
 const useTickets = (contactId: any) => {
   // Get Association Tickets
-  // const [page, setPage] = useState(PAGINATION?.CURRENT_PAGE);
-  // const [pageLimit, setPageLimit] = useState(PAGINATION?.PAGE_LIMIT);
-  // const defaultParams = {
-  //   page: PAGINATION?.CURRENT_PAGE,
-  //   limit: PAGINATION?.PAGE_LIMIT,
-  // };
-  // const [searchValue, setSearchValue] = useState(null);
-  // const [filterParams, setFilterParams] = useState({
-  //   page: page,
-  //   limit: pageLimit,
-  // });
-  // let searchPayLoad;
-  // if (searchValue) {
-  //   searchPayLoad = { search: searchValue };
-  // }
   const { data: dataGetTickets, isLoading: loadingTickets } =
     useGetContactAssociationsQuery({
       params: { contactId: contactId, association_type: 'tickets' },
