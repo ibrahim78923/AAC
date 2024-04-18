70b4823f867c0409653f2564707f2fd11364f73b
Auns <auns.mehmood@ceative.co.uk>
2024-04-18 16:06:23 +0500

    feat(hat): LPD-183 Api calls (#183)

--- src/services/airLoyaltyProgram/giftCards/transactions/index.tsx
+++ src/services/airLoyaltyProgram/giftCards/transactions/index.tsx
@@ -1,1 +1,18 @@
-export {};
+import { OPERATION } from '@/routesConstants/endpoints';
+import { baseAPI } from '@/services/base-api';
+
+const TAG = 'WORKFLOWS';
+export const giftCardTransactionApi = baseAPI?.injectEndpoints({
+  endpoints: (builder) => ({
+    getTransactionList: builder?.query({
+      query: (params) => ({
+        url: `${OPERATION?.OPERATION_WORKFLOW}`,
+        method: 'GET',
+        params,
+      }),
+      providesTags: [TAG],
+    }),
+  }),
+});
+
+export const { useLazyGetTransactionListQuery } = giftCardTransactionApi;
