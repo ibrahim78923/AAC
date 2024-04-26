198c8694b48e537d686c9670ecb30a2cc1faca44
Ali Raza <ali.raza@ceative.co.uk>
2024-04-26 11:43:42 +0500

    feat(): Working on time slots in meetings. (#127)

---
+++ src/modules/SocialComponents/Meetings/TimeSlotsPreferences/TimeSlotsHeader/index.tsx
@@ -0,0 +1,10 @@
+import { SingleDropdownButton } from '@/components/SingleDropdownButton';
+import { Box } from '@mui/material';
+
+export const TimeSlotsHeader = () => {
+  return (
+    <Box>
+      <SingleDropdownButton />
+    </Box>
+  );
+};
--- src/modules/SocialComponents/Meetings/TimeSlotsPreferences/index.tsx
+++ src/modules/SocialComponents/Meetings/TimeSlotsPreferences/index.tsx
@@ -1,6 +1,9 @@
+import { TimeSlotsHeader } from './TimeSlotsHeader';
+
 export const TimeSlotPreferences = () => {
   return (
     <>
+      <TimeSlotsHeader />
       <>TimeSlotPreferences</>
     </>
   );
