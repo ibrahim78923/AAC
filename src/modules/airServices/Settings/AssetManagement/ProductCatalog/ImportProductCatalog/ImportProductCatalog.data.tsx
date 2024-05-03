31b29264f883dc4705ba7eb4c0c2d272ac0097b8
my_goals_are_beyond_your_understanding <my_goals_are_beyond_your_understanding>
2024-05-03 12:23:55 +0500

    fix: issues in import and services issues by BA (#136)

--- src/modules/airServices/Settings/AssetManagement/ProductCatalog/ImportProductCatalog/ImportProductCatalog.data.tsx
+++ src/modules/airServices/Settings/AssetManagement/ProductCatalog/ImportProductCatalog/ImportProductCatalog.data.tsx
@@ -1,12 +1,26 @@
+import { FIELD_TYPES } from '@/constants/strings';
+
 export const CRM_COLUMNS = [
-  { _id: 'name', label: 'Name', groupBy: 'Mandatory Field' },
-  { _id: 'assetType', label: 'Asset Type', groupBy: 'Mandatory Field' },
-  { _id: 'status', label: 'Status', groupBy: 'Mandatory Field' },
-  { _id: 'manufacturer', label: 'Manufacturer', groupBy: 'Optional Field' },
+  { _id: 'name', label: 'Name', groupBy: FIELD_TYPES?.MANDATORY_FIELD },
+  {
+    _id: 'assetType',
+    label: 'Asset Type',
+    groupBy: FIELD_TYPES?.MANDATORY_FIELD,
+  },
+  { _id: 'status', label: 'Status', groupBy: FIELD_TYPES?.MANDATORY_FIELD },
+  {
+    _id: 'manufacturer',
+    label: 'Manufacturer',
+    groupBy: FIELD_TYPES?.OPTIONAL_FIELD,
+  },
   {
     _id: 'modeOfProcurement',
     label: 'Mode of procurement',
-    groupBy: 'Optional Field',
+    groupBy: FIELD_TYPES?.OPTIONAL_FIELD,
   },
-  { _id: 'description', label: 'Description', groupBy: 'Optional Field' },
+  {
+    _id: 'description',
+    label: 'Description',
+    groupBy: FIELD_TYPES?.OPTIONAL_FIELD,
+  },
 ];
