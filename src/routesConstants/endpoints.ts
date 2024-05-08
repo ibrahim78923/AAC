07aa1f52dad84d507ea30847fcf01352ae6ab83d
Rao Mudasir <raomuhammad.mudasir@ceative.co.uk>
2024-05-08 14:08:55 +0500

    feat():  DESIGN DONE (#AASS-116)

--- src/routesConstants/endpoints.ts
+++ src/routesConstants/endpoints.ts
@@ -358,6 +358,7 @@ export const END_POINTS = {
   IMPORT_FILE: `/import-file`,
   IMPORT_FILE_GET_SIGNED_URL: `/import-file/create-signed-url`,
   GET_PLAN_PRODUCTS_PERMISSIONS: '/plan/plan-product-permissions',
+  ADD_VOUCHERS: '/vouchers/create-voucher',
 };
 
 export const superAdminBillingInvoices = {
