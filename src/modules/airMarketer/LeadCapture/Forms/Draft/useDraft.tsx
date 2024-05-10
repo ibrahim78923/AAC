4b3d8ec819bd258f14807c3ffaa32a01e33225fd
Ghulam Mohiuddin <ghulam.mohiuddin@ceative.co.uk>
2024-05-10 11:57:54 +0500

    feat(): Working on Lead Capture Form. (#AAM-1147)

---
+++ src/modules/airMarketer/LeadCapture/Forms/Draft/useDraft.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { useGetLeadCaptureFormQuery } from '@/services/airMarketer/lead-capture/forms';
+import { PAGINATION } from '@/config';
+
+const useDraft = () => {
+  const [page, setPage] = useState(PAGINATION?.CURRENT_PAGE);
+  const [pageLimit, setPageLimit] = useState(PAGINATION?.PAGE_LIMIT);
+  const [searchValue, setSearchValue] = useState(null);
+  const paginationParams = {
+    page: page,
+    limit: pageLimit,
+  };
+
+  let searchPayLoad;
+  if (searchValue) {
+    searchPayLoad = { search: searchValue };
+  }
+  const { data: dataGetForms, isLoading: loadingGetForms } =
+    useGetLeadCaptureFormQuery({
+      params: { status: 'DRAFT', ...searchPayLoad, ...paginationParams },
+    });
+
+  return {
+    setSearchValue,
+    loadingGetForms,
+    dataGetForms,
+    searchValue,
+    setPageLimit,
+    setPage,
+  };
+};
+
+export default useDraft;
---
+++ src/modules/airMarketer/LeadCapture/Forms/Trash/useDraft.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+import { useGetLeadCaptureFormQuery } from '@/services/airMarketer/lead-capture/forms';
+import { PAGINATION } from '@/config';
+
+const useTrash = () => {
+  const [page, setPage] = useState(PAGINATION?.CURRENT_PAGE);
+  const [pageLimit, setPageLimit] = useState(PAGINATION?.PAGE_LIMIT);
+  const [searchValue, setSearchValue] = useState(null);
+  const paginationParams = {
+    page: page,
+    limit: pageLimit,
+  };
+
+  let searchPayLoad;
+  if (searchValue) {
+    searchPayLoad = { search: searchValue };
+  }
+  const { data: dataGetForms, isLoading: loadingGetForms } =
+    useGetLeadCaptureFormQuery({
+      params: { status: 'TRASH', ...searchPayLoad, ...paginationParams },
+    });
+
+  return {
+    setSearchValue,
+    loadingGetForms,
+    dataGetForms,
+    searchValue,
+    setPageLimit,
+    setPage,
+  };
+};
+
+export default useTrash;
