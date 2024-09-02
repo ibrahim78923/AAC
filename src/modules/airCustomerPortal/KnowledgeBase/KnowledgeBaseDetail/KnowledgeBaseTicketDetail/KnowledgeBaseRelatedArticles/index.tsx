2be5d69deb4880d7e318a02ffc7482da4a0cb10d
Ali Haider <152487343+RAHaider36501@users.noreply.github.com>
2024-09-02 12:01:23 +0500

    AM2D-222-Code-Refactoring (#1622)
    * feat(hat): working on Code-Refactoring (#222)
    
    * feat(hat): working on Code-Refactoring (#222)
    
    * feat(hat): working on Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * fix: issue in autocomplete (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * fix: description issue in ticket detail (#222)
    
    * fix: description issue in ticket detail (#222)
    
    * fix: description issue in ticket detail (#222)
    
    * fix: description issue in ticket detail (#222)
    
    * fix: added interface in customer portal (#222)
    
    * fix: added interface in customer portal (#222)
    
    * fix: added interface in customer portal (#222)
    
    * fix: added interface in customer portal (#222)
    
    * fix: added interface in customer portal (#222)
    
    * fix: added interface in customer portal (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    * feat(hat): working on AM2D-222-Code-Refactoring (#222)
    
    ---------
    
    Co-authored-by: my_goals_are_beyond_your_understanding <my_goals_are_beyond_your_understanding>

---
+++ src/modules/airCustomerPortal/KnowledgeBase/KnowledgeBaseDetail/KnowledgeBaseTicketDetail/KnowledgeBaseRelatedArticles/index.tsx
@@ -0,0 +1,139 @@
+import { DocumentTextIcon } from '@/assets/icons';
+import NoData from '@/components/NoData';
+import SkeletonTable from '@/components/Skeletons/SkeletonTable';
+import { LoadingButton } from '@mui/lab';
+import { Box, Divider, Grid, Typography } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { FormProvider } from '@/components/ReactHookForm';
+
+export const KnowledgeBaseRelatedArticles = (props: any) => {
+  const {
+    loadingArticles,
+    fetchingArticles,
+    relatedArticlesData,
+    handleRelatedArticles,
+    singleArticleId,
+    theme,
+    showFeedbackField,
+    feedbackMethod,
+    feedbackSubmit,
+    feedbackDataArray,
+    showOkFeedback,
+    feedbackIsLoading,
+    helpfulSubmit,
+    setShowFeedbackField,
+  } = props;
+
+  if (loadingArticles || fetchingArticles) return <SkeletonTable />;
+
+  return (
+    <>
+      <Typography variant="h4">Related Articles</Typography>
+      <Box height={showFeedbackField ? '9rem' : '34rem'} overflow="scroll">
+        {relatedArticlesData?.length <= 1 ? (
+          <NoData message="No related articles found" />
+        ) : (
+          relatedArticlesData?.map(
+            (item: any) =>
+              item?._id !== singleArticleId && (
+                <Box
+                  key={item?.id}
+                  display="flex"
+                  alignItems="center"
+                  p={1}
+                  borderRadius={1}
+                  bgcolor={theme?.palette?.grey?.[100]}
+                  mt={0.5}
+                  onClick={() => handleRelatedArticles(item?._id)}
+                  sx={{
+                    cursor: 'pointer',
+                    ':hover': { bgcolor: theme?.palette?.grey?.[700] },
+                  }}
+                >
+                  <DocumentTextIcon />
+                  <Typography color="secondary">{item?.title}</Typography>
+                </Box>
+              ),
+          )
+        )}
+      </Box>
+      {showFeedbackField ? (
+        <Box p={1}>
+          <Typography color="secondary" variant="body2" mb={1}>
+            Sorry we cannot be helpful. Help us improve this article with your
+            feedback.
+          </Typography>
+          <FormProvider methods={feedbackMethod} onSubmit={feedbackSubmit}>
+            <Grid container>
+              {feedbackDataArray?.map((item: any) => (
+                <Grid item key={item?.id} xs={12}>
+                  <item.component {...item?.componentProps} />
+                </Grid>
+              ))}
+            </Grid>
+            <Box display="flex" justifyContent="flex-end" gap={1} mt={1}>
+              <LoadingButton
+                variant="outlined"
+                color="secondary"
+                disabled={feedbackIsLoading}
+                onClick={() => setShowFeedbackField(false)}
+              >
+                Cancel
+              </LoadingButton>
+              <LoadingButton
+                loading={feedbackIsLoading}
+                variant="contained"
+                type="submit"
+              >
+                Submit
+              </LoadingButton>
+            </Box>
+          </FormProvider>
+        </Box>
+      ) : showOkFeedback ? (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          gap={0.5}
+          mt={6}
+        >
+          <CheckCircleIcon sx={{ color: 'success.main' }} />
+          <Typography color="secondary" variant="body2">
+            Glad we could be helpful. Thanks for the feedback.
+          </Typography>
+        </Box>
+      ) : (
+        <>
+          <Typography
+            color="secondary"
+            variant="body2"
+            display="flex"
+            justifyContent="center"
+            mt={3}
+          >
+            Was this answer helpful?
+          </Typography>
+          <Divider sx={{ m: 1.5 }} />
+          <Box display="flex" justifyContent="flex-end" gap={1}>
+            <LoadingButton
+              variant="outlined"
+              color="secondary"
+              disabled={feedbackIsLoading}
+              onClick={() => setShowFeedbackField(true)}
+            >
+              No
+            </LoadingButton>
+            <LoadingButton
+              loading={feedbackIsLoading}
+              variant="contained"
+              onClick={helpfulSubmit}
+            >
+              Yes
+            </LoadingButton>
+          </Box>
+        </>
+      )}
+    </>
+  );
+};
