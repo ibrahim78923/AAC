31c5007d8112fb987d1f4097a36c459134d02c1c
Muhammad Fahad <muhammad.fahad@consultancyoutfit.co.uk>
2024-09-09 09:53:13 +0500

    feat(): create public dropdown button.(#222) (#1654)
    Co-authored-by: Muhammad Fahad <muhammad.fahad@ceative.co.uk>

---
+++ src/components/PublicSingleDropdownButton/index.tsx
@@ -0,0 +1,79 @@
+import { Button, Menu, MenuItem, IconButton, Typography } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { usePublicSingleDropdownButton } from './usePublicSingleDropdownButton';
+import {
+  PublicSingleDropdownButtonPropsI,
+  PublicSingleDropdownOptionI,
+} from './PublicSingleDropdownButton .interface';
+
+export const PublicSingleDropdownButton = (
+  props: PublicSingleDropdownButtonPropsI,
+) => {
+  const {
+    dropdownOptions,
+    disabled,
+    dropdownName = 'Actions',
+    hasEndIcon = true,
+    btnVariant = 'outlined',
+    Variant = hasEndIcon ? Button : IconButton,
+    menuSxProps,
+    ...buttonProps
+  } = props;
+
+  const { anchorEl, open, theme, handleClick, handleClose } =
+    usePublicSingleDropdownButton();
+
+  const buttonPropsWithIcon =
+    Variant === Button ? { endIcon: hasEndIcon && <ArrowDropDownIcon /> } : {};
+
+  return (
+    <>
+      <Variant
+        variant={btnVariant}
+        id="demo-positioned-button"
+        aria-controls={open ? 'demo-positioned-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        color="secondary"
+        disabled={disabled}
+        sx={{ textTransform: 'capitalize' }}
+        {...buttonPropsWithIcon}
+        {...buttonProps}
+      >
+        {dropdownName}
+      </Variant>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        sx={{ padding: 2, ...menuSxProps }}
+      >
+        {dropdownOptions?.map((singleOption: PublicSingleDropdownOptionI) => (
+          <MenuItem
+            key={singleOption?.id}
+            disabled={singleOption?.disabled}
+            onClick={() => singleOption?.handleClick?.(handleClose)}
+            sx={{
+              '&.MuiMenuItem-root': {
+                marginBottom: { md: 0.5 },
+                marginX: { md: 0.5 },
+              },
+            }}
+          >
+            <Typography
+              variant="body2"
+              color={theme?.palette?.grey?.[600]}
+              fontWeight={500}
+              sx={singleOption?.titleSx}
+            >
+              {singleOption?.title}{' '}
+            </Typography>
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+};
---
+++ src/components/PublicSingleDropdownButton/usePublicSingleDropdownButton.ts
@@ -0,0 +1,25 @@
+import { Theme, useTheme } from '@mui/material';
+import { useState } from 'react';
+import { PublicSingleDropdownButtonCloseMenuI } from './PublicSingleDropdownButton .interface';
+
+export const usePublicSingleDropdownButton = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open: boolean = Boolean(anchorEl);
+  const theme: Theme = useTheme();
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event?.currentTarget);
+  };
+
+  const handleClose: PublicSingleDropdownButtonCloseMenuI = () => {
+    setAnchorEl(null);
+  };
+
+  return {
+    anchorEl,
+    open,
+    theme,
+    handleClick,
+    handleClose,
+  };
+};
