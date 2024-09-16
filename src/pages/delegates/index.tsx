c2047a945625120854e1c32c9130db9095e6b7d3
nabeel-consultancyoutfit <nabeel.ahmed@consultancyoutfit.co.uk>
2024-09-16 16:06:04 +0500

    Aacsa 708 (#1716)
    * feat(hat): Task -> added fields is not updating on edit task  (#AIR-2165)
    
    * feat(hat):  in right Top user dropdown delegate page not found FIXED  (#AACSA-708)
    
    * feat(hat): Delegate page not found issue  (#AACSA-708)

--- src/pages/delegates/index.tsx
+++ src/pages/delegates/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import SuperAdminLayout from '../../layout';
-
 import Delegates from '@/modules/orgAdmin/Delegates';
+import Layout from '@/layout';
+import { Permissions } from '@/constants/permissions';
 
 const DelegatesPage = () => {
   return <Delegates />;
 };
 export default DelegatesPage;
 DelegatesPage.getLayout = function getLayout(page: any) {
-  return <SuperAdminLayout>{page}</SuperAdminLayout>;
+  return (
+    <Layout guardRoute permissions={Permissions?.ORG_ADMIN_DASHBOARD}>
+      {page}
+    </Layout>
+  );
 };
